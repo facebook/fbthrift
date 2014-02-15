a2d6dbdaac0e41d09f752c62b5169a16d29f6cb3
Tom Jackson <tjackson@fb.com>
2014-02-14 16:52:53 -0800

    Adding overflow checking for Frozen
    Summary:
    As it is, freezing silently corrupts output files if they get too
    large. Also, the limits are 2GB where they could be 4GB easily.
    
    Longer term, it would be good to template parameterize the pointer size.
    
    Very long term, it might be worth giving up fixed layout (like **cap'n'proto**) to allow for flexible pointer sizes.
    
    Test Plan: Unit tests
    @override-unit-failures
    because hphp doesn't use Thrift/Frozen.
    
    Reviewed By: maxime@fb.com
    
    FB internal diff: D1176891

--- thrift/lib/cpp/RelativePtr.h
+++ thrift/lib/cpp/RelativePtr.h
@@ -1,21 +1,19 @@
 /*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements. See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership. The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License. You may obtain a copy of the License at
+ * Copyright 2014 Facebook, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
  *   http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied. See the License for the
- * specific language governing permissions and limitations
- * under the License.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
+
 #ifndef THRIFT_RELATIVEPTR_H_
 #define THRIFT_RELATIVEPTR_H_
 
@@ -28,7 +26,7 @@ typedef uint8_t byte;
 // Relative Ptr - The key to relocatable object graphs
 // TODO: expose 'OffsetType' as a type parameter in freeze()
 template<class T,
-         class OffsetType = int32_t>
+         class OffsetType = uint32_t>
 class RelativePtr : private boost::noncopyable {
   OffsetType offset_;
  public:
@@ -47,17 +45,18 @@ class RelativePtr : private boost::noncopyable {
     }
     const byte* target = reinterpret_cast<const byte*>(ptr);
     const byte* origin = reinterpret_cast<const byte*>(this);
-    offset_ = target - origin;
+    auto offset = target - origin;
+    offset_ = offset;
+    CHECK_EQ(offset_, offset) << "Structure too large to address using "
+                              << (sizeof(OffsetType) * 8) << "-bit pointers";
   }
 
   T* get() const {
     if (!offset_) {
       return nullptr;
     }
-    const byte* origin =
-      reinterpret_cast<const byte*>(this);
-    const byte* target =
-      reinterpret_cast<const byte*>(origin + offset_);
+    byte* origin = const_cast<byte*>(reinterpret_cast<const byte*>(this));
+    byte* target = origin + offset_;
     return reinterpret_cast<T*>(target);
   }
 
--- thrift/lib/cpp/test/FrozenTest.cpp
+++ thrift/lib/cpp/test/FrozenTest.cpp
@@ -1,25 +1,22 @@
 /*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements. See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership. The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License. You may obtain a copy of the License at
+ * Copyright 2014 Facebook, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
  *   http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied. See the License for the
- * specific language governing permissions and limitations
- * under the License.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
-#define BOOST_TEST_MODULE FrozenTest
 
-#include <boost/test/unit_test.hpp>
+#include <gtest/gtest.h>
 #include <random>
+
 #include "folly/Conv.h"
 #include "folly/Hash.h"
 #include "folly/MapUtil.h"
@@ -59,17 +56,15 @@ Team testValue() {
   return team;
 }
 
-BOOST_AUTO_TEST_SUITE( FrozenTest )
-
-BOOST_AUTO_TEST_CASE( TestFrozen ) {
+TEST(Frozen, Basic) {
   Team team = testValue();
-  BOOST_CHECK_EQUAL(team.peopleById.at(hasher(3)).name, "Person 3");
-  BOOST_CHECK_EQUAL(team.peopleById.at(hasher(4)).name, "Person 4");
-  BOOST_CHECK_EQUAL(team.peopleById.at(hasher(5)).name, "Person 5");
-  BOOST_CHECK_EQUAL(team.peopleByName.at("Person 3").id, 3);
-  BOOST_CHECK_EQUAL(team.peopleByName.begin()->second.nums.count(-1), 1);
-  BOOST_CHECK_EQUAL(team.projects.count("alpha"), 1);
-  BOOST_CHECK_EQUAL(team.projects.count("beta"), 1);
+  EXPECT_EQ(team.peopleById.at(hasher(3)).name, "Person 3");
+  EXPECT_EQ(team.peopleById.at(hasher(4)).name, "Person 4");
+  EXPECT_EQ(team.peopleById.at(hasher(5)).name, "Person 5");
+  EXPECT_EQ(team.peopleByName.at("Person 3").id, 3);
+  EXPECT_EQ(team.peopleByName.begin()->second.nums.count(-1), 1);
+  EXPECT_EQ(team.projects.count("alpha"), 1);
+  EXPECT_EQ(team.projects.count("beta"), 1);
 
   size_t size = frozenSize(team);
   for (int misalign = 0; misalign < 16; ++misalign) {
@@ -80,7 +75,7 @@ BOOST_AUTO_TEST_CASE( TestFrozen ) {
     const byte* const frozenLocation =
         static_cast<const byte*>(static_cast<const void*>(freezeResult));
     // verify that freeze didn't yeild a different address.
-    BOOST_CHECK_EQUAL(freezeLocation - &bytes[0],
+    EXPECT_EQ(freezeLocation - &bytes[0],
                       frozenLocation - &bytes[0]);
 
     std::vector<byte> copy(bytes);
@@ -88,47 +83,47 @@ BOOST_AUTO_TEST_CASE( TestFrozen ) {
     auto& frozen = *(Frozen<Team>*)copyBuffer;
 
     auto thawedTeam = thaw(frozen);
-    BOOST_CHECK_EQUAL(frozen.peopleById.at(hasher(3)).name.range(), "Person 3");
-    BOOST_CHECK_EQUAL(frozen.peopleById.at(hasher(4)).name, "Person 4");
-    BOOST_CHECK_EQUAL(frozen.peopleById.at(hasher(5)).name, "Person 5");
-    BOOST_CHECK_EQUAL(frozen.peopleById.at(hasher(3)).dob,
+    EXPECT_EQ(frozen.peopleById.at(hasher(3)).name.range(), "Person 3");
+    EXPECT_EQ(frozen.peopleById.at(hasher(4)).name, "Person 4");
+    EXPECT_EQ(frozen.peopleById.at(hasher(5)).name, "Person 5");
+    EXPECT_EQ(frozen.peopleById.at(hasher(3)).dob,
                       team.peopleById.at(hasher(3)).dob);
-    BOOST_CHECK_EQUAL(frozen.peopleByName.at("Person 3").id, 3);
-    BOOST_CHECK_EQUAL(frozen.peopleByName.at(string("Person 4")).id, 4);
-    BOOST_CHECK_EQUAL(frozen.peopleByName.at(fbstring("Person 5")).id, 5);
-    BOOST_CHECK_EQUAL(frozen.peopleByName.at(StringPiece("Person 6")).id, 6);
-    BOOST_CHECK_EQUAL(frozen.peopleByName.begin()->second.nums.count(-1), 1);
-    BOOST_CHECK_EQUAL(frozen.projects.count("alpha"), 1);
-    BOOST_CHECK_EQUAL(frozen.projects.count("beta"), 1);
+    EXPECT_EQ(frozen.peopleByName.at("Person 3").id, 3);
+    EXPECT_EQ(frozen.peopleByName.at(string("Person 4")).id, 4);
+    EXPECT_EQ(frozen.peopleByName.at(fbstring("Person 5")).id, 5);
+    EXPECT_EQ(frozen.peopleByName.at(StringPiece("Person 6")).id, 6);
+    EXPECT_EQ(frozen.peopleByName.begin()->second.nums.count(-1), 1);
+    EXPECT_EQ(frozen.projects.count("alpha"), 1);
+    EXPECT_EQ(frozen.projects.count("beta"), 1);
 
-    BOOST_CHECK_THROW(frozen.peopleById.at(hasher(50)).name, std::out_of_range);
+    EXPECT_THROW(frozen.peopleById.at(hasher(50)).name, std::out_of_range);
   }
 }
 
-BOOST_AUTO_TEST_CASE( FieldOrdering ) {
+TEST(Frozen, FieldOrdering) {
   Pod p;
   p.a = 0x012345;
   p.b = 0x0678;
   p.c = 0x09;
-  BOOST_CHECK_LT(static_cast<void*>(&p.a),
+  EXPECT_LT(static_cast<void*>(&p.a),
                  static_cast<void*>(&p.b));
-  BOOST_CHECK_LT(static_cast<void*>(&p.b),
+  EXPECT_LT(static_cast<void*>(&p.b),
                  static_cast<void*>(&p.c));
   auto pf = freeze(p);
   auto& f = *pf;
-  BOOST_CHECK_EQUAL(sizeof(f.__isset), 1);
-  BOOST_CHECK_EQUAL(sizeof(f),
+  EXPECT_EQ(sizeof(f.__isset), 1);
+  EXPECT_EQ(sizeof(f),
                     sizeof(int32_t) +
                     sizeof(int16_t) +
                     sizeof(uint8_t) +
                     1);
-  BOOST_CHECK_LT(static_cast<const void*>(&f.a),
+  EXPECT_LT(static_cast<const void*>(&f.a),
                  static_cast<const void*>(&f.b));
-  BOOST_CHECK_LT(static_cast<const void*>(&f.b),
+  EXPECT_LT(static_cast<const void*>(&f.b),
                  static_cast<const void*>(&f.c));
 }
 
-BOOST_AUTO_TEST_CASE( IntMap ) {
+TEST(Frozen, IntMap) {
   std::map<string, int> tmap {
     { "1", 2 },
     { "3", 4 },
@@ -137,100 +132,113 @@ BOOST_AUTO_TEST_CASE( IntMap ) {
   };
   auto pfmap = freeze(tmap);
   auto& fmap = *pfmap;
-  BOOST_CHECK_EQUAL(fmap.at("3"), 4);
+  EXPECT_EQ(fmap.at("3"), 4);
   auto b = fmap.begin();
   auto e = fmap.end();
   using std::make_pair;
-  BOOST_CHECK(fmap.find("0") == e);
-  BOOST_CHECK(fmap.find("3") == b + 1);
-  BOOST_CHECK(fmap.find("4") == e);
-  BOOST_CHECK(fmap.find("9") == e);
-  BOOST_CHECK(fmap.lower_bound("0") == b);
-  BOOST_CHECK(fmap.lower_bound("1") == b);
-  BOOST_CHECK(fmap.lower_bound("3") == b + 1);
-  BOOST_CHECK(fmap.lower_bound("4") == b + 2);
-  BOOST_CHECK(fmap.lower_bound("9") == e);
-  BOOST_CHECK(fmap.upper_bound("0") == b);
-  BOOST_CHECK(fmap.upper_bound("1") == b + 1);
-  BOOST_CHECK(fmap.upper_bound("3") == b + 2);
-  BOOST_CHECK(fmap.upper_bound("4") == b + 2);
-  BOOST_CHECK(fmap.upper_bound("9") == e);
-  BOOST_CHECK(fmap.equal_range("0") == make_pair(b, b));
-  BOOST_CHECK(fmap.equal_range("1") == make_pair(b, b + 1));
-  BOOST_CHECK(fmap.equal_range("3") == make_pair(b + 1, b + 2));
-  BOOST_CHECK(fmap.equal_range("4") == make_pair(b + 2, b + 2));
-  BOOST_CHECK(fmap.equal_range("9") == make_pair(e, e));
-  BOOST_CHECK(fmap.count("0") == 0);
-  BOOST_CHECK(fmap.count("1") == 1);
-  BOOST_CHECK(fmap.count("3") == 1);
-  BOOST_CHECK(fmap.count("4") == 0);
-  BOOST_CHECK(fmap.count("9") == 0);
+  EXPECT_TRUE(fmap.find("0") == e);
+  EXPECT_TRUE(fmap.find("3") == b + 1);
+  EXPECT_TRUE(fmap.find("4") == e);
+  EXPECT_TRUE(fmap.find("9") == e);
+  EXPECT_TRUE(fmap.lower_bound("0") == b);
+  EXPECT_TRUE(fmap.lower_bound("1") == b);
+  EXPECT_TRUE(fmap.lower_bound("3") == b + 1);
+  EXPECT_TRUE(fmap.lower_bound("4") == b + 2);
+  EXPECT_TRUE(fmap.lower_bound("9") == e);
+  EXPECT_TRUE(fmap.upper_bound("0") == b);
+  EXPECT_TRUE(fmap.upper_bound("1") == b + 1);
+  EXPECT_TRUE(fmap.upper_bound("3") == b + 2);
+  EXPECT_TRUE(fmap.upper_bound("4") == b + 2);
+  EXPECT_TRUE(fmap.upper_bound("9") == e);
+  EXPECT_TRUE(fmap.equal_range("0") == make_pair(b, b));
+  EXPECT_TRUE(fmap.equal_range("1") == make_pair(b, b + 1));
+  EXPECT_TRUE(fmap.equal_range("3") == make_pair(b + 1, b + 2));
+  EXPECT_TRUE(fmap.equal_range("4") == make_pair(b + 2, b + 2));
+  EXPECT_TRUE(fmap.equal_range("9") == make_pair(e, e));
+  EXPECT_TRUE(fmap.count("0") == 0);
+  EXPECT_TRUE(fmap.count("1") == 1);
+  EXPECT_TRUE(fmap.count("3") == 1);
+  EXPECT_TRUE(fmap.count("4") == 0);
+  EXPECT_TRUE(fmap.count("9") == 0);
 
-  BOOST_CHECK_EQUAL(fmap.at("1"), 2);
-  BOOST_CHECK_EQUAL(fmap.at("7"), 8);
-  BOOST_CHECK_THROW(fmap.at("9"), std::out_of_range);
+  EXPECT_EQ(fmap.at("1"), 2);
+  EXPECT_EQ(fmap.at("7"), 8);
+  EXPECT_THROW(fmap.at("9"), std::out_of_range);
 
-  BOOST_CHECK(folly::get_ptr(fmap, *freezeStr("1")));
-  BOOST_CHECK_EQUAL(2, *folly::get_ptr(fmap, *freezeStr("1")));
+  EXPECT_TRUE(folly::get_ptr(fmap, *freezeStr("1")));
+  EXPECT_EQ(2, *folly::get_ptr(fmap, *freezeStr("1")));
 
-  BOOST_CHECK(!folly::get_ptr(fmap, *freezeStr("2")));
+  EXPECT_TRUE(!folly::get_ptr(fmap, *freezeStr("2")));
 
-  BOOST_CHECK(folly::get_ptr(fmap, *freezeStr("3")));
-  BOOST_CHECK_EQUAL(4, *folly::get_ptr(fmap, *freezeStr("3")));
+  EXPECT_TRUE(folly::get_ptr(fmap, *freezeStr("3")));
+  EXPECT_EQ(4, *folly::get_ptr(fmap, *freezeStr("3")));
 }
 
-BOOST_AUTO_TEST_CASE( Set ) {
+TEST(Frozen, SetString) {
   std::set<string> tset { "1", "3", "7", "5" };
   auto pfset = freeze(tset);
   auto& fset = *pfset;
   auto b = fset.begin();
   auto e = fset.end();
   using std::make_pair;
-  BOOST_CHECK(fset.find("3") == b + 1);
-  BOOST_CHECK(fset.find(folly::fbstring("3")) == b + 1);
-  BOOST_CHECK(fset.find(std::string("3")) == b + 1);
-  BOOST_CHECK(fset.find(folly::StringPiece("3")) == b + 1);
-  BOOST_CHECK(fset.find("0") == e);
-  BOOST_CHECK(fset.find("3") == b + 1);
-  BOOST_CHECK(fset.find("4") == e);
-  BOOST_CHECK(fset.find("9") == e);
-  BOOST_CHECK(fset.lower_bound("0") == b);
-  BOOST_CHECK(fset.lower_bound("1") == b);
-  BOOST_CHECK(fset.lower_bound("3") == b + 1);
-  BOOST_CHECK(fset.lower_bound("4") == b + 2);
-  BOOST_CHECK(fset.lower_bound("9") == e);
-  BOOST_CHECK(fset.upper_bound("0") == b);
-  BOOST_CHECK(fset.upper_bound("1") == b + 1);
-  BOOST_CHECK(fset.upper_bound("3") == b + 2);
-  BOOST_CHECK(fset.upper_bound("4") == b + 2);
-  BOOST_CHECK(fset.upper_bound("9") == e);
-  BOOST_CHECK(fset.equal_range("0") == make_pair(b, b));
-  BOOST_CHECK(fset.equal_range("1") == make_pair(b, b + 1));
-  BOOST_CHECK(fset.equal_range("3") == make_pair(b + 1, b + 2));
-  BOOST_CHECK(fset.equal_range("4") == make_pair(b + 2, b + 2));
-  BOOST_CHECK(fset.equal_range("9") == make_pair(e, e));
-  BOOST_CHECK(fset.count("0") == 0);
-  BOOST_CHECK(fset.count("1") == 1);
-  BOOST_CHECK(fset.count("3") == 1);
-  BOOST_CHECK(fset.count("4") == 0);
-  BOOST_CHECK(fset.count("9") == 0);
+  EXPECT_TRUE(fset.find("3") == b + 1);
+  EXPECT_TRUE(fset.find(folly::fbstring("3")) == b + 1);
+  EXPECT_TRUE(fset.find(std::string("3")) == b + 1);
+  EXPECT_TRUE(fset.find(folly::StringPiece("3")) == b + 1);
+  EXPECT_TRUE(fset.find("0") == e);
+  EXPECT_TRUE(fset.find("3") == b + 1);
+  EXPECT_TRUE(fset.find("4") == e);
+  EXPECT_TRUE(fset.find("9") == e);
+  EXPECT_TRUE(fset.lower_bound("0") == b);
+  EXPECT_TRUE(fset.lower_bound("1") == b);
+  EXPECT_TRUE(fset.lower_bound("3") == b + 1);
+  EXPECT_TRUE(fset.lower_bound("4") == b + 2);
+  EXPECT_TRUE(fset.lower_bound("9") == e);
+  EXPECT_TRUE(fset.upper_bound("0") == b);
+  EXPECT_TRUE(fset.upper_bound("1") == b + 1);
+  EXPECT_TRUE(fset.upper_bound("3") == b + 2);
+  EXPECT_TRUE(fset.upper_bound("4") == b + 2);
+  EXPECT_TRUE(fset.upper_bound("9") == e);
+  EXPECT_TRUE(fset.equal_range("0") == make_pair(b, b));
+  EXPECT_TRUE(fset.equal_range("1") == make_pair(b, b + 1));
+  EXPECT_TRUE(fset.equal_range("3") == make_pair(b + 1, b + 2));
+  EXPECT_TRUE(fset.equal_range("4") == make_pair(b + 2, b + 2));
+  EXPECT_TRUE(fset.equal_range("9") == make_pair(e, e));
+  EXPECT_TRUE(fset.count("0") == 0);
+  EXPECT_TRUE(fset.count("1") == 1);
+  EXPECT_TRUE(fset.count("3") == 1);
+  EXPECT_TRUE(fset.count("4") == 0);
+  EXPECT_TRUE(fset.count("9") == 0);
 }
 
-BOOST_AUTO_TEST_CASE( Vector ) {
+TEST(Frozen, VectorInt) {
   std::vector<int> tvect { 1, 3, 7, 5 };
   auto pfvect = freeze(tvect);
   auto& fvect = *pfvect;
   auto b = fvect.begin();
   auto e = fvect.end();
   using std::make_pair;
-  BOOST_CHECK_EQUAL(fvect.front(), 1);
-  BOOST_CHECK_EQUAL(fvect.back(), 5);
-  BOOST_CHECK_EQUAL(fvect.size(), 4);
-  BOOST_CHECK_EQUAL(fvect[1], 3);
-  BOOST_CHECK_EQUAL(b[0], 1);
-  BOOST_CHECK_EQUAL(b[1], 3);
-  BOOST_CHECK_EQUAL(e[-1], 5);
-  BOOST_CHECK_EQUAL(e[-2], 7);
+  EXPECT_EQ(fvect.front(), 1);
+  EXPECT_EQ(fvect.back(), 5);
+  EXPECT_EQ(fvect.size(), 4);
+  EXPECT_EQ(fvect[1], 3);
+  EXPECT_EQ(b[0], 1);
+  EXPECT_EQ(b[1], 3);
+  EXPECT_EQ(e[-1], 5);
+  EXPECT_EQ(e[-2], 7);
 }
 
-BOOST_AUTO_TEST_SUITE_END()
+TEST(Frozen, RelativePtr) {
+  int before;
+  RelativePtr<int> rptr;
+  int after;
+
+  rptr.reset(&after);
+  EXPECT_EQ(rptr.get(), &after);   // basics
+  rptr.reset(&before + (1 << 30)); // within 4GB = okay
+
+  // pointing to lower addresses = underflow
+  EXPECT_DEATH(rptr.reset(&before), "address");
+  // pointing to addresses more than 4GB away = overflow
+  EXPECT_DEATH(rptr.reset(&after + (1 << 30)), "address");
+}
