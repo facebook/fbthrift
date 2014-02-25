dc8466e92b4d48c21c3ff7d1cbc2318499b7e427
Cullen Walsh <ckwalsh@fb.com>
2014-02-25 12:44:10 -0800

    Guarantee structural id is only 63 bits at most
    Summary: We ran into structural ids that were too large for hhvm (which is 64 bits signed).
    
    Test Plan: fbmake, regenerate thrift, confirm ids change
    
    Reviewed By: alandau@fb.com
    
    FB internal diff: D1190098

--- thrift/compiler/generate/t_generator.cc
+++ thrift/compiler/generate/t_generator.cc
@@ -1,20 +1,17 @@
 /*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements. See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership. The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License. You may obtain a copy of the License at
+ * Copyright 2014 Facebook, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
  *   http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied. See the License for the
- * specific language governing permissions and limitations
- * under the License.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 #include "thrift/compiler/generate/t_generator.h"
@@ -210,7 +207,7 @@ std::string t_generator::generate_structural_id(
   SHA1(reinterpret_cast<const unsigned char*>(hashable_keys_list.data()),
        hashable_keys_list.size(),
        u.buf);
-  uint64_t h = folly::Endian::little(u.val);
+  uint64_t h = folly::Endian::little(u.val) & 0x7FFFFFFFFFFFFFFFull; // 63 bits
 
   // Generate a readable number.
   char structural_id[21];
