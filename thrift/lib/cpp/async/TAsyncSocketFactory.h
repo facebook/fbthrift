5693ad76fd7c2ff915ea8b9413a9d3b5934122b6
Peter Griess <pgriess@fb.com>
2014-02-28 16:13:19 -0800

    Move TAsyncSocketFactory and Mock* to Thrift
    Summary:
    - Create TAsyncSocketFactory and mocks
    
    Facebook:
    - Proxygen had a useful TAsyncSocketFactory (including mocks); move this
    to Thrift so that others can benefit from it
    - Change TAsyncSocketFactory to return a TAsyncSocket::UniquePtr since
    that's the idiom that we use elsewhere to indicate transfer of
    ownership
    
    Test Plan: - Unit tests
    
    Reviewed By: rajat@fb.com
    
    FB internal diff: D1197579

---
+++ thrift/lib/cpp/async/TAsyncSocketFactory.h
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2014 Facebook, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef THRIFT_ASYNC_TASYNCSOCKETFACTORY_H_
+#define THRIFT_ASYNC_TASYNCSOCKETFACTORY_H_ 1
+
+#include "thrift/lib/cpp/async/TAsyncSocket.h"
+
+namespace apache { namespace thrift { namespace async {
+
+class TEventBase;
+
+/**
+ * Factory class for producing TAsyncSocket instances.
+ *
+ * This implementation produces instances of TAsyncSocket, but one may want to
+ * subclass this to produce MockTAsyncSocket, TAsyncSSLSocket, etc.
+ */
+class TAsyncSocketFactory {
+ public:
+  explicit TAsyncSocketFactory(TEventBase* eventBase);
+  virtual ~TAsyncSocketFactory();
+  virtual TAsyncSocket::UniquePtr make() const;
+
+ protected:
+  TEventBase* eventBase_;
+};
+
+}}}
+
+#endif
--- thrift/lib/cpp/test/MockTAsyncSocket.h
+++ thrift/lib/cpp/test/MockTAsyncSocket.h
@@ -1,21 +1,19 @@
 /*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements. See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership. The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License. You may obtain a copy of the License at
+ * Copyright 2014 Facebook, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
  *   http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied. See the License for the
- * specific language governing permissions and limitations
- * under the License.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
+
 #ifndef THRIFT_TEST_MOCKTASYNCSOCKET_H_
 #define THRIFT_TEST_MOCKTASYNCSOCKET_H_ 1
 
@@ -26,15 +24,18 @@
 
 namespace apache {
 namespace thrift {
+
+namespace async {
+class TEventBase;
+}
+
 namespace test {
 
 class MockTAsyncSocket : public apache::thrift::async::TAsyncSocket {
  public:
   typedef std::unique_ptr<MockTAsyncSocket, Destructor> UniquePtr;
 
-  explicit MockTAsyncSocket(apache::thrift::async::TEventBase* evb)
-    : TAsyncSocket(evb) {
-  }
+  explicit MockTAsyncSocket(async::TEventBase* base) : TAsyncSocket(base) {}
 
   GMOCK_METHOD5_(, noexcept, , connect,
       void(TAsyncSocket::ConnectCallback*,
