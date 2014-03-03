2f8b76a864431d202d4dd00dc29ebfc60de91faf
Andrei Bajenov <andreib@fb.com>
2014-03-03 13:12:34 -0800

    Cancel timeout on HeaderClientChannel destruction
    Summary:
    This cancels a timeout if a channel is being destroyed. This fixes a set of errors that happen in the following scenario:
    - timeout is scheduled
    - channel is destroyed
    - abort is thrown because we try to destroy an HHWheelTimer while it still has a timeout
    
    Note that this doesn't fix the server-side crashes I was seeing.
    
    Test Plan: loadgen
    
    Reviewed By: mhorowitz@fb.com
    
    FB internal diff: D1199543

--- thrift/lib/cpp2/async/HeaderClientChannel.cpp
+++ thrift/lib/cpp2/async/HeaderClientChannel.cpp
@@ -1,20 +1,17 @@
 /*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements. See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership. The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License. You may obtain a copy of the License at
+ * Copyright 2014 Facebook, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
  *   http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied. See the License for the
- * specific language governing permissions and limitations
- * under the License.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 #include "thrift/lib/cpp2/async/HeaderClientChannel.h"
@@ -64,6 +61,7 @@ void HeaderClientChannel::setSaslTimeout(uint32_t ms) {
 }
 
 void HeaderClientChannel::destroy() {
+  saslClientCallback_.cancelTimeout();
   saslClient_->markChannelCallbackUnavailable();
   Cpp2Channel::destroy();
 }
@@ -75,8 +73,8 @@ void HeaderClientChannel::attachEventBase(
 }
 
 void HeaderClientChannel::detachEventBase() {
-  saslClient_->markChannelCallbackUnavailable();
   saslClientCallback_.cancelTimeout();
+  saslClient_->markChannelCallbackUnavailable();
 
   Cpp2Channel::detachEventBase();
   timer_->detachEventBase();
