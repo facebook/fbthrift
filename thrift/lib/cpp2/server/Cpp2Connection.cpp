4bbb1167637120b899e5f0b15b2f4b0247ac63a6
Lars Backstrom <lars@fb.com>
2014-03-04 19:32:41 -0800

    revert D1200110
    Summary: clean revert. Details in t3855556
    
    Test Plan: none
    
    Reviewed By: qiao@fb.com
    
    FB internal diff: D1203200
    
    @override-unit-failures

--- thrift/lib/cpp2/server/Cpp2Connection.cpp
+++ thrift/lib/cpp2/server/Cpp2Connection.cpp
@@ -108,7 +108,6 @@ Cpp2Connection::~Cpp2Connection() {
 
 void Cpp2Connection::stop() {
   cancelTimeout();
-
   for (auto req : activeRequests_) {
     VLOG(1) << "Task killed due to channel close: " <<
       context_.getPeerAddress()->describe();
@@ -127,10 +126,6 @@ void Cpp2Connection::stop() {
   if (handler) {
     handler->connectionDestroyed(&context_);
   }
-
-  // Release the socket to avoid long CLOSE_WAIT times
-  channel_.reset();
-  socket_.reset();
 }
 
 void Cpp2Connection::timeoutExpired() noexcept {
@@ -163,7 +158,6 @@ void Cpp2Connection::requestTimeoutExpired() {
   if (observer) {
     observer->taskTimeout();
   }
-
 }
 
 bool Cpp2Connection::pending() {
