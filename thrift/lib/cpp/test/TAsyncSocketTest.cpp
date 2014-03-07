412885f600ecb93c315229036244a2f245be764f
Peter Griess <pgriess@fb.com>
2014-03-07 08:21:03 -0800

    Fix TAsyncSocketTest.ConnectTimeout
    Summary:
    - It appears that dns.any is now outright rejecting connection attempts
    to port 65535. Instead hit 8.8.8.8, one of the GoogleDNS anycast
    addresses. This should be inaccessible from all L3 prod clusters, as
    they require use of fwdproxy to get out to the internet. I believe all
    devservers are in such clusters these days.
    
    Test Plan: - Ran unit tests
    
    Reviewed By: simpkins@fb.com
    
    FB internal diff: D1209595

--- thrift/lib/cpp/test/TAsyncSocketTest.cpp
+++ thrift/lib/cpp/test/TAsyncSocketTest.cpp
@@ -1,21 +1,19 @@
 /*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements. See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership. The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License. You may obtain a copy of the License at
+ * Copyright 2014 Facebook, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
  *   http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied. See the License for the
- * specific language governing permissions and limitations
- * under the License.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
+
 #include "thrift/lib/cpp/async/TAsyncServerSocket.h"
 #include "thrift/lib/cpp/async/TAsyncSocket.h"
 #include "thrift/lib/cpp/async/TAsyncTimeout.h"
@@ -383,7 +381,7 @@ BOOST_AUTO_TEST_CASE(ConnectTimeout) {
   // Hopefully this IP will be routable but unresponsive.
   // (Alternatively, we could try listening on a local raw socket, but that
   // normally requires root privileges.)
-  TSocketAddress addr("172.31.255.53", 65535);
+  TSocketAddress addr("8.8.8.8", 65535);
   ConnCallback cb;
   socket->connect(&cb, addr, 1); // also set a ridiculously small timeout
 
