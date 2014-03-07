508f34ca02187bfae549d488fa1b28a2becd887c
Alex Bakhturin <abakhturin@fb.com>
2014-03-07 11:36:35 -0800

    Added code for importing '_types' modules for included '.thrift' files
    Summary: This is required for services like Rapido whose '.thrift' files inlude other '.thrift' files.
    
    Test Plan:
    Successfully built the Rapido CLI target :) Here go the details:
    
    **Before**
    module MyCustomNamespace.Rapido;
    
    import thrift.base;
    import thrift.codegen.base;
    import thrift.util.hashset;
    
    import MyCustomNamespace.Rapido_types;
    import FacebookService;
    
    interface Rapido : FacebookService {
    . . .
    
    **After**
    module MyCustomNamespace.Rapido;
    
    import thrift.base;
    import thrift.codegen.base;
    import thrift.util.hashset;
    
    import FacebookService;
    import MyCustomNamespace.RapidoSource_types;
    import MyCustomNamespace.Rapido_types;
    import fb303_types;
    
    interface Rapido : FacebookService {
    
    Reviewed By: davejwatson@fb.com
    
    FB internal diff: D1208880

--- thrift/compiler/generate/t_d_generator.cc
+++ thrift/compiler/generate/t_d_generator.cc
@@ -1,24 +1,17 @@
 /*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements. See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership. The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License. You may obtain a copy of the License at
+ * Copyright 2014 Facebook, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
  *   http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied. See the License for the
- * specific language governing permissions and limitations
- * under the License.
- *
- * Contains some contributions under the Thrift Software License.
- * Please see doc/old-thrift-license.txt in the Thrift distribution for
- * details.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 #include <cassert>
@@ -222,6 +215,15 @@ class t_d_generator : public t_oop_generator {
 
     f_service << "import " << render_package(*get_program()) << program_name_ <<
       "_types;" << endl;
+
+    // Include type modules from other imported programs.
+    const vector<t_program*>& includes = program_->get_includes();
+
+    for (size_t i = 0; i < includes.size(); ++i) {
+      f_service <<
+        "import " << render_package(*(includes[i])) <<
+        includes[i]->get_name() << "_types;" << endl;
+    }
 
     t_service* extends_service = tservice->get_extends();
     if (extends_service != NULL) {
