7bf196c652912d889148d12491765f5ea308d37d
Cullen Walsh <ckwalsh@fb.com>
2014-04-08 09:42:01 -0700

    Add typehint to setAsyncHandler
    Summary: This matches the hack definition, so we can add set/getAsyncHandler to IThriftClient without an hack/runtime errors.
    
    Test Plan: Regenerate all thrift in www, hh_client and unit tests
    
    Reviewed By: alandau@fb.com
    
    FB internal diff: D1264555

--- thrift/compiler/generate/t_php_generator.cc
+++ thrift/compiler/generate/t_php_generator.cc
@@ -1,20 +1,17 @@
 /*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements. See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership. The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License. You may obtain a copy of the License at
+ * Copyright 2014 Facebook, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
  *   http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied. See the License for the
- * specific language governing permissions and limitations
- * under the License.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 #include <string>
@@ -2213,7 +2210,7 @@ void t_php_generator::_generate_service_client(
   generate_client_event_handler_functions(out);
 
   out <<
-    indent() << "public function setAsyncHandler($async_handler) {" << endl <<
+    indent() << "public function setAsyncHandler(TClientAsyncHandler $async_handler) {" << endl <<
     indent() << "  $this->asyncHandler_ = $async_handler;" << endl <<
     indent() << "  return $this;" << endl <<
     indent() << "}" << endl <<
