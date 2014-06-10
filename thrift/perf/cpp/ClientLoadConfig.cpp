944e02c2b864400af2483dd1c8b7dd2b3978b45c
Andrei Bajenov <andreib@fb.com>
2014-06-10 15:47:51 -0700

    Default connections to sys.<prefix> principals, as opposed to tier names
    Summary:
    This is a better default overall. We deploy keys to all machines of the form sys.<hostnameprefix>. Before this change, the connections defaulted to the tier's name as opposed to this principal name. This isn't good in most cases.
    
    For custom keys, you're still able to override with the thrift_security_service_tier property.
    
    Test Plan: loadgen
    
    Reviewed By: mhorowitz@fb.com
    
    Subscribers: mcduff, marccelani, mshneer, alandau, hitesh, rodrigos, wstefancik
    
    FB internal diff: D1375675

--- thrift/perf/cpp/ClientLoadConfig.cpp
+++ thrift/perf/cpp/ClientLoadConfig.cpp
@@ -1,21 +1,19 @@
 /*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements. See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership. The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License. You may obtain a copy of the License at
+ * Copyright 2014 Facebook, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
  *   http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied. See the License for the
- * specific language governing permissions and limitations
- * under the License.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
+
 #define __STDC_FORMAT_MACROS
 
 #include "thrift/perf/cpp/ClientLoadConfig.h"
@@ -39,7 +37,7 @@ DEFINE_string(srtier, "thrift.loadgen",
               "Tier to grab Service Router config from");
 DEFINE_string(sasl_policy_test, "disabled",
               "SASL policy required / permitted / disabled");
-DEFINE_string(sasl_service_tier, "host",
+DEFINE_string(sasl_service_tier, "",
               "Tier name encoded in service keytab");
 
 DEFINE_int32(num_threads, 5, "number of threads");
