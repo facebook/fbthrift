b094fa3be895d4f67d2846df07fae99fd28367f4
Stepan Palamarchuk <stepan@fb.com>
2014-06-23 20:19:26 -0700

    Prevent DestructorGuard from deleting objects multiple times
    Summary:
    Currently DestructorGuard can attempt to delete object multiple times if it's used by methods that are called from the destructor.
    
    Simple example:
    
    std::shared_ptr<A> a;
    
    void someRandomUserCallback() {
    // do something
    a.reset();
    }
    
    class A : public TDelayedDestruction {
    public:
    std::shared_ptr<A> create() { return std::shared_ptr<A>(new A(), Destructor()); }
    void callSomeUserCallbacks() {
    DestructionGuard dd(this); // 1
    someRandomUserCallback(); // 2
    } // 3
    private:
    A() {}
    ~A() { callSomeUserCallbacks(); }
    };
    
    void test() {
    a = A::create();
    a->callSomeUserCallbacks();
    }
    
    What basically happens:
    1) // 1: aquire guard (object is alive)
    2) // 2: set destroyPending_ flag
    3) // 3: call ~A()
    4) // 1: aquire guard (object is already being destroyed)
    5) // 2: do nothing
    6) // 3: call ~A() again -> double delete
    
    Test Plan: fbconfig thrift/lib/cpp/ -r && fbmake runtests
    
    Reviewed By: simpkins@fb.com
    
    Subscribers: alandau, bmatheny
    
    FB internal diff: D1399609

--- thrift/lib/cpp/async/TDelayedDestruction.h
+++ thrift/lib/cpp/async/TDelayedDestruction.h
@@ -1,21 +1,19 @@
 /*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements. See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership. The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License. You may obtain a copy of the License at
+ * Copyright 2014 Facebook, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
  *   http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied. See the License for the
- * specific language governing permissions and limitations
- * under the License.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
+
 #ifndef THRIFT_ASYNC_TDELAYEDDESTRUCTION_H_
 #define THRIFT_ASYNC_TDELAYEDDESTRUCTION_H_ 1
 
@@ -102,6 +100,7 @@ class TDelayedDestruction : private boost::noncopyable {
       assert(dd_->guardCount_ > 0);
       --dd_->guardCount_;
       if (dd_->guardCount_ == 0 && dd_->destroyPending_) {
+        dd_->destroyPending_ = false;
         dd_->destroyNow(true);
       }
     }
