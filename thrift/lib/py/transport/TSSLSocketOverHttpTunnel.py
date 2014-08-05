a24d1e01909c6e9f6d49b7db2d91089f5a368097
Huapeng Zhou <hzhou@fb.com>
2014-08-05 10:14:31 -0700

    Added HTTP tunnel thrift clients
    Summary: For offnet FNAs, we need a way to communicate to the thrift ports via fwdproxy. We can start with a HTTP tunnel via CONNECT and go from there. This is essentially what the ProxyCommand to ssh into FNAs does.
    
    Test Plan: tested via connecting to fna001.01.fna2914 and the thrift clients are working.
    
    Reviewed By: brianp@fb.com
    
    Subscribers: alandau, bmatheny, shuff, ruibalp
    
    FB internal diff: D1467536
    
    Tasks: 4746093

---
+++ thrift/lib/py/transport/TSSLSocketOverHttpTunnel.py
@@ -0,0 +1,41 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+import ssl
+
+from .TSocketOverHttpTunnel import TSocketOverHttpTunnel
+from .TTransport import TTransportException
+
+class TSSLSocketOverHttpTunnel(TSocketOverHttpTunnel):
+    def __init__(self, host, port, proxy_host, proxy_port,
+                 ssl_version=ssl.PROTOCOL_TLSv1,
+                 cert_reqs=ssl.CERT_NONE,
+                 ca_certs=None,
+                 keyfile=None,
+                 certfile=None):
+        TSocketOverHttpTunnel.__init__(self, host, port, proxy_host, proxy_port)
+        self.ssl_version = ssl_version
+        self.cert_reqs = cert_reqs
+        self.keyfile, self.certfile, self.ca_certs = \
+                keyfile, certfile, ca_certs
+
+    def open(self):
+        TSocketOverHttpTunnel.open(self)
+        try:
+            sslh = ssl.SSLSocket(self.handle,
+                                 ssl_version=self.ssl_version,
+                                 cert_reqs=self.cert_reqs,
+                                 keyfile=self.keyfile,
+                                 certfile=self.certfile,
+                                 ca_certs=self.ca_certs)
+            self.handle = sslh
+        except ssl.SSLError as e:
+            self.close()
+            raise TTransportException(TTransportException.NOT_OPEN,
+                    "SSL error during handshake: " + str(e))
+        except socket.error as e:
+            self.close()
+            raise TTransportException(TTransportException.NOT_OPEN,
+                    "socket error during SSL handshake: " + str(e))
---
+++ thrift/lib/py/transport/TSocketOverHttpTunnel.py
@@ -0,0 +1,37 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+from .TSocket import TSocket
+from .TTransport import TTransportException
+import socket
+
+
+class TSocketOverHttpTunnel(TSocket):
+    def __init__(self, host, port, proxy_host, proxy_port):
+        TSocket.__init__(self, proxy_host, proxy_port)
+        try:
+            # Use IP address since sometimes proxy_host cannot resolve
+            # external hostnames using unbound
+            self.remote_host = socket.gethostbyname(host)
+        except socket.error as e:
+            raise TTransportException(TTransportException.NOT_OPEN, str(e))
+        self.remote_port = port
+
+    def open(self):
+        TSocket.open(self)
+        self.write("CONNECT %s:%d HTTP/1.1\r\nHost: %s:%d\r\n\r\n" % (
+            self.remote_host, self.remote_port,
+            self.remote_host, self.remote_port))
+        res = self.read(4096)
+        try:
+            status = res.split()[1]
+            if status != '200':
+                self.close()
+                raise TTransportException(TTransportException.NOT_OPEN,
+                        "Error response from proxy server: %s" % res)
+        except IndexError:
+            self.close()
+            raise TTransportException(TTransportException.NOT_OPEN,
+                    "Error response from proxy server: %s" % res)
