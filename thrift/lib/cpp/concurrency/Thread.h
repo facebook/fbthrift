2e36dce5a6392de89d924c47c868bb2b5e071ac7
James Sedgwick <jsedgwick@fb.com>
2014-08-29 11:40:48 -0700

    kill ThreadManagerPriority.h
    Summary:
    not included anywhere and duplicates the definition in Thread.h
    also fix indent and move definition down with other priority stuff in Thread.h
    
    there's probably some reason for this duplication but putting up a diff is easier than taking off my headphones and asking :)
    
    Test Plan:
    fbconfig -r thrift/lib/cpp/concurrency && fbmake runtests
    grep for header and see it's included nowhere
    
    Reviewed By: davejwatson@fb.com
    
    Subscribers: alandau, bmatheny
    
    FB internal diff: D1520701

--- thrift/lib/cpp/concurrency/Thread.h
+++ thrift/lib/cpp/concurrency/Thread.h
@@ -1,20 +1,17 @@
 /*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements. See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership. The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License. You may obtain a copy of the License at
+ * Copyright 2014 Facebook, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
  *   http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied. See the License for the
- * specific language governing permissions and limitations
- * under the License.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 #ifndef _THRIFT_CONCURRENCY_THREAD_H_
@@ -26,15 +23,6 @@
 
 
 namespace apache { namespace thrift { namespace concurrency {
-
-  enum PRIORITY {
-    HIGH_IMPORTANT,
-    HIGH,
-    IMPORTANT,
-    NORMAL,
-    BEST_EFFORT,
-    N_PRIORITIES
-  };
 
 class Thread;
 
@@ -63,6 +51,15 @@ class Runnable {
 
  private:
   std::weak_ptr<Thread> thread_;
+};
+
+enum PRIORITY {
+  HIGH_IMPORTANT,
+  HIGH,
+  IMPORTANT,
+  NORMAL,
+  BEST_EFFORT,
+  N_PRIORITIES
 };
 
 class PriorityRunnable : public virtual Runnable {
