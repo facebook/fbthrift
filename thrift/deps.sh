34ad64c2217ae6127aee19b904c5d0ab061a1254
Dave Watson <davejwatson@fb.com>
2014-09-17 09:27:45 -0700

    Update build script
    Summary: update build script from 12.10 to 14.04, and update README.md
    
    Test Plan: build on ubuntu
    
    Reviewed By: haijunz@fb.com
    
    Subscribers: doug, alandau
    
    FB internal diff: D1560800

--- thrift/deps.sh
+++ thrift/deps.sh
@@ -1,26 +1,13 @@
-sudo apt-get install -yq git autoconf libtool g++ libboost-all-dev libevent-dev flex bison libgoogle-glog-dev scons libkrb5-dev libsnappy-dev libsasl2-dev
+sudo apt-get install -yq libdouble-conversion-dev libssl-dev make zip git autoconf libtool g++ libboost-all-dev libevent-dev flex bison libgoogle-glog-dev scons libkrb5-dev libsnappy-dev libsasl2-dev
 
 git clone https://github.com/facebook/folly
 cd folly/folly
 
-wget --no-check-certificate https://double-conversion.googlecode.com/files/double-conversion-1.1.1.tar.gz
-tar zxvf double-conversion-1.1.1.tar.gz
-mv SConstruct.double-conversion double-conversion
-cd double-conversion
-scons -f SConstruct.double-conversion
-cd ..
-
-cd test
-wget http://googletest.googlecode.com/files/gtest-1.6.0.zip
-unzip gtest-1.6.0.zip
-cd ..
-
 autoreconf --install
-LDFLAGS=-L`pwd`/double-conversion/ CPPFLAGS=-I`pwd`/double-conversion/src/ ./configure
-make
+./configure
+make -j8
 cd ../..
 
 autoreconf --install
-CPPFLAGS="-I`pwd`/folly/folly/test/gtest-1.6.0/include/ -I`pwd`/folly/ -I`pwd`/folly/folly/double-conversion/src/" LDFLAGS="-L`pwd`/folly/folly/test/.libs/ -L`pwd`/folly/folly/.libs/ -L`pwd`/folly/folly/double-conversion/" ./configure
-make
-make check
+CPPFLAGS=" -I`pwd`/folly/" LDFLAGS="-L`pwd`/folly/folly/.libs/" ./configure
+make -j8
