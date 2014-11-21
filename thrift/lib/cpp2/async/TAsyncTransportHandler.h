2dd57875dd504388700dc9a6fce6c8d8ee805d63
James Sedgwick <jsedgwick@fb.com>
2014-11-21 08:51:52 -0800

    AsyncTransportHandler
    Summary: pulled this out of D1618704. It's one implementation of the handler that sits at the bottom of a channel pipeline and actually does the talking to the transport
    
    Test Plan: will try to test in isolation. using in thrift diff
    
    Reviewed By: davejwatson@fb.com
    
    Subscribers: njormrod, folly-diffs@, trunkagent, fugalh, alandau, bmatheny, mshneer
    
    FB internal diff: D1628202
    
    Signature: t1:1628202:1416529657:8211a62225d68a7958214c483e06636c039c2831

---
+++ thrift/lib/cpp2/async/TAsyncTransportHandler.h
@@ -0,0 +1,155 @@
+/*
+ * Copyright 2014 Facebook, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#include <folly/experimental/wangle/channel/ChannelHandler.h>
+#include <folly/io/async/EventBase.h>
+#include <folly/io/async/EventBaseManager.h>
+#include <folly/io/IOBuf.h>
+#include <folly/io/IOBufQueue.h>
+#include <thrift/lib/cpp/async/TAsyncTransport.h>
+#include <thrift/lib/cpp/transport/TTransportException.h>
+
+namespace apache { namespace thrift {
+
+class TAsyncTransportHandler
+  : public folly::wangle::BytesToBytesHandler,
+    public async::TAsyncTransport::ReadCallback {
+ public:
+  explicit TAsyncTransportHandler(
+      std::shared_ptr<async::TAsyncTransport> transport)
+    : transport_(std::move(transport)) {}
+
+  TAsyncTransportHandler(TAsyncTransportHandler&&) = default;
+
+  ~TAsyncTransportHandler() {
+    if (transport_) {
+      detachReadCallback();
+    }
+  }
+
+  void attachReadCallback() {
+    transport_->setReadCallback(transport_->good() ? this : nullptr);
+  }
+
+  void detachReadCallback() {
+    if (transport_->getReadCallback() == this) {
+      transport_->setReadCallback(nullptr);
+    }
+  }
+
+  void attachEventBase(folly::EventBase* eventBase) {
+    if (eventBase && !transport_->getEventBase()) {
+      transport_->attachEventBase(eventBase);
+    }
+  }
+
+  void detachEventBase() {
+    detachReadCallback();
+    if (transport_->getEventBase()) {
+      transport_->detachEventBase();
+    }
+  }
+
+  void attachPipeline(Context* ctx) override {
+    CHECK(!ctx_);
+    ctx_ = ctx;
+  }
+
+  folly::wangle::Future<void> write(
+      Context* ctx,
+      std::unique_ptr<folly::IOBuf> buf) override {
+    if (UNLIKELY(!buf)) {
+      return folly::wangle::makeFuture();
+    }
+
+    if (!transport_->good()) {
+      VLOG(5) << "transport is closed in write()";
+      return folly::wangle::makeFuture<void>(
+          transport::TTransportException("transport is closed in write()"));
+    }
+
+    auto cb = new WriteCallback();
+    auto future = cb->promise_.getFuture();
+    transport_->writeChain(cb, std::move(buf), ctx->getWriteFlags());
+    return future;
+  };
+
+  folly::wangle::Future<void> close(Context* ctx) {
+    if (transport_) {
+      detachReadCallback();
+      transport_->closeNow();
+    }
+    return folly::wangle::makeFuture();
+  }
+
+  // Must override to avoid warnings about hidden overloaded virtual due to
+  // TAsyncTransport::ReadCallback::readEOF()
+  void readEOF(Context* ctx) override {
+    ctx->fireReadEOF();
+  }
+
+  void getReadBuffer(void** bufReturn, size_t* lenReturn) override {
+    const auto readBufferSettings = ctx_->getReadBufferSettings();
+    const auto ret = bufQueue_.preallocate(
+        readBufferSettings.first,
+        readBufferSettings.second);
+    *bufReturn = ret.first;
+    *lenReturn = ret.second;
+  }
+
+  void readDataAvailable(size_t len) noexcept override {
+    bufQueue_.postallocate(len);
+    ctx_->fireRead(bufQueue_);
+  }
+
+  void readEOF() noexcept override {
+    ctx_->fireReadEOF();
+  }
+
+  void readError(const transport::TTransportException& ex)
+    noexcept override {
+    ctx_->fireReadException(
+        folly::make_exception_wrapper<transport::TTransportException>(
+            std::move(ex)));
+  }
+
+ private:
+  class WriteCallback : private async::TAsyncTransport::WriteCallback {
+    void writeSuccess() noexcept override {
+      promise_.setValue();
+      delete this;
+    }
+
+    void writeError(size_t bytesWritten,
+                    const transport::TTransportException& ex)
+      noexcept override {
+      promise_.setException(std::make_exception_ptr(ex));
+      delete this;
+    }
+
+   private:
+    friend class TAsyncTransportHandler;
+    folly::wangle::Promise<void> promise_;
+  };
+
+  Context* ctx_{nullptr};
+  folly::IOBufQueue bufQueue_;
+  std::shared_ptr<async::TAsyncTransport> transport_;
+};
+
+}}
