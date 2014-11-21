ffb03fc8aba28eea885bc3453caaf0d970b38714
Alex Landau <alandau@fb.com>
2014-11-21 14:17:04 -0800

    CompactProtocol: serialization perf - use inline stack
    Summary:
    Use inline stack if possible instead of allocating on the heap.
    Similar to D1672045 but for writes.
    
    Test Plan:
    ```
    ------ old ------   ------ new ------
    Benchmark                                time/iter  iters/s  time/iter  iters/s        win
    ==========================================================================================
    CompactProtocol_write_Empty               123.10ns    8.12M    92.19ns   10.85M     33.62%
    CompactProtocol_write_SmallInt            140.60ns    7.11M   109.05ns    9.17M     28.97%
    CompactProtocol_write_BigInt              172.44ns    5.80M   144.68ns    6.91M     19.14%
    CompactProtocol_write_SmallString         154.23ns    6.48M   125.45ns    7.97M     22.99%
    CompactProtocol_write_BigString           405.69ns    2.46M   368.60ns    2.71M     10.16%
    CompactProtocol_write_BigBinary           303.58ns    3.29M   276.57ns    3.62M     10.03%
    CompactProtocol_write_LargeBinary         312.93ns    3.20M   290.44ns    3.44M      7.50%
    CompactProtocol_write_Mixed               206.38ns    4.85M   176.04ns    5.68M     17.11%
    CompactProtocol_write_SmallListInt        235.18ns    4.25M   208.11ns    4.81M     13.18%
    CompactProtocol_write_BigListInt           99.76us   10.02K    99.08us   10.09K      0.70%
    CompactProtocol_write_BigListMixed          1.03ms   974.08   997.80us    1.00K      2.66%
    CompactProtocol_write_LargeListMixed      113.49ms     8.81   111.85ms     8.94      1.48%
    ```
    
    Reviewed By: davejwatson@fb.com
    
    Subscribers: trunkagent, alandau, bmatheny, mshneer
    
    FB internal diff: D1698243
    
    Signature: t1:1698243:1416614287:d3ed533057a85aff121989857e7f0ec61ebb5e8f

--- thrift/lib/cpp2/protocol/CompactProtocol.h
+++ thrift/lib/cpp2/protocol/CompactProtocol.h
@@ -41,6 +41,45 @@ static const int32_t VERSION_2 = 0x82020000;
 static const int8_t  PROTOCOL_ID = static_cast<int8_t>(0x82);
 static const int8_t  TYPE_MASK = static_cast<int8_t>(0xE0);
 static const int32_t TYPE_SHIFT_AMOUNT = 5;
+
+// Simple stack with an inline buffer for built-in types
+// Emulates the interface of std::stack
+template <typename T, size_t n>
+class SimpleStack {
+public:
+  SimpleStack() : top_(0) {}
+
+  void push(T v) {
+    if (LIKELY(top_ < n)) {
+      a_[top_++] = v;
+    } else {
+      heapStack_.push(v);
+      top_++;
+    }
+  }
+
+  T top() {
+    DCHECK(top_ > 0);
+    if (LIKELY(top_ <= n)) {
+      return a_[top_-1];
+    } else {
+      return heapStack_.top();
+    }
+  }
+
+  void pop() {
+    DCHECK(top_ > 0);
+    if (UNLIKELY(top_ > n)) {
+      heapStack_.pop();
+    }
+    --top_;
+  }
+private:
+  T a_[n];
+  size_t top_;
+  std::stack<int16_t, folly::fbvector<int16_t>> heapStack_;
+};
+
 }}
 
 /**
@@ -181,7 +220,7 @@ class CompactProtocolWriter {
     int16_t fieldId;
   } booleanField_;
 
-  std::stack<int16_t, folly::fbvector<int16_t>> lastField_;
+  detail::compact::SimpleStack<int16_t, 10> lastField_;
   int16_t lastFieldId_;
 
 };
@@ -300,44 +339,7 @@ class CompactProtocolReader {
 
   int32_t seqid_;
 
-  // Simple stack with an inline buffer for built-in types
-  // Emulates the interface of std::stack
-  template <typename T, size_t n>
-  class SimpleStack {
-  public:
-    SimpleStack() : top_(0) {}
-
-    void push(T v) {
-      if (LIKELY(top_ < n)) {
-        a_[top_++] = v;
-      } else {
-        heapStack_.push(v);
-        top_++;
-      }
-    }
-
-    T top() {
-      DCHECK(top_ > 0);
-      if (LIKELY(top_ <= n)) {
-        return a_[top_-1];
-      } else {
-        return heapStack_.top();
-      }
-    }
-
-    void pop() {
-      DCHECK(top_ > 0);
-      if (UNLIKELY(top_ > n)) {
-        heapStack_.pop();
-      }
-      --top_;
-    }
-  private:
-    T a_[n];
-    size_t top_;
-    std::stack<int16_t, folly::fbvector<int16_t>> heapStack_;
-  };
-  SimpleStack<int16_t, 10> lastField_;
+  detail::compact::SimpleStack<int16_t, 10> lastField_;
   int16_t lastFieldId_;
 
   struct {
