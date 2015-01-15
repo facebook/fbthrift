30eb67aa3a82a3d6daf7ffaa89bb2a98ad080bb3
Andrew Cox <andrewcox@fb.com>
2015-01-14 16:46:49 -0800

    Use specific revision of folly when building fbthrift
    Summary:
    Add code in deps.sh to sync to a specific revision of folly, so in case folly
    releases an update, it won't break the external fbthrift build.
    
    This is just a patch to holdover until folly is a git submodule.
    
    Test Plan: Built using autoconf build on ubuntu
    
    Reviewed By: alandau@fb.com
    
    Subscribers: doug, alandau, mshneer
    
    FB internal diff: D1807082
    
    Signature: t1:1807082:1422400958:6634a4953fc100efd24714a5476baf3896b52827

--- thrift/deps.sh
+++ thrift/deps.sh
@@ -1,7 +1,13 @@
+#!/usr/bin/env bash
+
 sudo apt-get install -yq libdouble-conversion-dev libssl-dev make zip git autoconf libtool g++ libboost-all-dev libevent-dev flex bison libgoogle-glog-dev scons libkrb5-dev libsnappy-dev libsasl2-dev libnuma-dev
 
+# Change directory to location of this script
+cd "$(dirname ${0})"
 git clone https://github.com/facebook/folly
 cd folly/folly
+git fetch
+git checkout v0.24.0
 
 autoreconf --install
 ./configure
