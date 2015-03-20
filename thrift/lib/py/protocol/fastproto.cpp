7e76d697d2c35abcb6287c8b04a937576c798f04
Gordon Huang <zhuohuang@fb.com>
2015-03-19 17:31:58 -0700

    Fix a clang issue caused by fastproto.cpp
    Summary:
    I got complains as
    thrift/lib/py/protocol/fastproto.cpp:234:21: error: use of undeclared identifier 'INT32_MAX'
    if (!( ((len) <= (INT32_MAX)) && ((len) >= (0)) )) {
    
    Google told me to try
    #define __STDC_LIMIT_MACROS
    #include <stdint.h>
    
    and it works.
    
    Test Plan:
    build with clang
    
    Reviewed By: haijunz@fb.com
    
    Subscribers: mathieubaudet, alandau, bmatheny, mshneer, yfeldblum
    
    FB internal diff: D1928996
    
    Tasks: 6435799
    
    Signature: t1:1928996:1426808695:53a8130bfab986d98858f2e0f36479c3400869f4

--- thrift/lib/py/protocol/fastproto.cpp
+++ thrift/lib/py/protocol/fastproto.cpp
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+#define __STDC_LIMIT_MACROS
 #include <Python.h>
 
 #include <stdint.h>
