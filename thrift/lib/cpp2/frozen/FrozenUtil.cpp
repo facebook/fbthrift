615020837da47d17bada5d1a6581b9478363ab72
Lucian Grijincu <lucian@fb.com>
2015-05-14 12:30:14 -0700

    thrift: fix static initialization fiasco
    Test Plan: fbmake
    
    Reviewed By: tjackson@fb.com
    
    Subscribers: trunkagent, alandau, bmatheny, yfeldblum, tudort
    
    FB internal diff: D2070531
    
    Signature: t1:2070531:1431628972:71c7360ae0d150b2f523151b0ac115eca35c0a82
    
    Blame Revision: D2063160

--- thrift/lib/cpp2/frozen/FrozenUtil.cpp
+++ thrift/lib/cpp2/frozen/FrozenUtil.cpp
@@ -24,7 +24,7 @@ FrozenFileForwardIncompatible::FrozenFileForwardIncompatible(int fileVersion)
           "Frozen File version ",
           fileVersion,
           " cannot be read, only versions up to ",
-          schema::g_frozen_constants.kCurrentFrozenFileVersion,
+          schema::frozen_constants::kCurrentFrozenFileVersion,
           " are supported.")),
       fileVersion_(fileVersion) {}
 
