d9d251240083fec104c85cfa0cce28f67754a278
Jake Lengyel <jlengyel@fb.com>
2015-06-16 15:34:50 -0700

    Add Haskell examples to Thrift test fixtures
    Summary: Add Thrift test fixtures for easier maintenance of Haskell code generation features
    
    Test Plan: Generate the fixtures to ensure non-breaking code changes
    
    Reviewed By: yfeldblum@fb.com
    
    Subscribers: alandau, yfeldblum
    
    FB internal diff: D2157148
    
    Tasks: 7420925
    
    Signature: t1:2157148:1434493857:f9ab9440d3851b958ebc805d4cccd96261bcb5d4

---
+++ thrift/compiler/test/fixtures/list/src/module.thrift
@@ -0,0 +1,9 @@
+const map<i64, list<string>> TEST_MAP = {
+  0: [
+    "foo",
+    "bar",
+  ],
+  1: [
+    "baz",
+  ],
+}
---
+++ thrift/compiler/test/fixtures/service-fuzzer/src/module.thrift
@@ -0,0 +1,15 @@
+service TestService {
+  i64 init(
+    1: i64 int1,
+    2: i64 int2,
+    3: i64 int3,
+    4: i64 int4,
+    5: i64 int5,
+    6: i64 int6,
+    7: i64 int7,
+    8: i64 int8,
+    9: i64 int9,
+    10: i64 int10,
+    11: i64 int11,
+  )
+}
