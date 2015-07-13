b2e0fbf5c5714fdb095566b5bab4f0a1daf21421
Yedidya Feldblum <yfeldblum@fb.com>
2015-07-13 11:13:48 -0700

    A compiler fixture for cpp2 and exceptions.
    Summary: [Thrift] A compiler fixture for cpp2 and exceptions.
    
    Test Plan: Unit tests: `thrift/compiler/test/compiler_test.py`.
    
    Reviewers: alandau, mcduff, davejwatson, meyering, njormrod, haijunz
    
    Reviewed By: haijunz
    
    Subscribers: alandau, yfeldblum, dougw, brettp
    
    Differential Revision: https://phabricator.fb.com/D2236083
    
    Signature: t1:2236083:1436809367:b1cc3193a807ef7f8162cde66ddd21cfa8f9c014

---
+++ thrift/compiler/test/fixtures/exceptions/src/module.thrift
@@ -0,0 +1,11 @@
+exception Banal { }
+exception Fiery {
+  1: required string message,
+}
+
+service Raiser {
+  void doBland(),
+  void doRaise() throws (1: Banal b, 2: Fiery f),
+  string get200(),
+  string get500() throws (1: Fiery f, 2: Banal b),
+}
