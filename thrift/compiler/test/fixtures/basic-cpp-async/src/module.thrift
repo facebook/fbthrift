359f73999cafa442c92c64a0ee84290deab0ad5e
Yedidya Feldblum <yfeldblum@fb.com>
2015-07-21 17:30:25 -0700

    A compiler fixture for cpp:cob_client,templates.
    Summary: [Thrift] A compiler fixture for cpp:cob_client,templates.
    
    Test Plan: CI.
    
    Reviewers: haijunz, davejwatson, mcduff, alandau
    
    Reviewed By: alandau
    
    Subscribers: alandau, yfeldblum, dougw, brettp
    
    Differential Revision: https://phabricator.fb.com/D2262647
    
    Signature: t1:2262647:1437508639:712e04f80050238e4b67301bc493da906e13bf0c

---
+++ thrift/compiler/test/fixtures/basic-cpp-async/src/module.thrift
@@ -0,0 +1,19 @@
+enum MyEnum {
+  MyValue1,
+  MyValue2,
+}
+
+struct MyStruct {
+  1: i64 MyIntField,
+  2: string MyStringField,
+}
+
+service MyService {
+  void ping()
+  string getRandomData()
+  bool hasDataById(1: i64 id)
+  string getDataById(1: i64 id)
+  void putDataById(1: i64 id, 2: string data)
+  oneway void lobDataById(1: i64 id, 2: string data)
+  void putStructById(1: i64 id, 2: MyStruct data)
+}
