b8c1dfe295611a887a753f45e6c0b200aa887e96
Noam Zilberstein <noamz@fb.com>
2015-09-08 00:21:14 -0700

    add versioning test case
    Summary: Versioning in hs thrift was supposedly broken, so I added a test case to test the functionality. interestingly, it seems to work, so I'm not sure what's going on
    
    Test Plan: Run test suite
    
    Reviewed By: smarlow@fb.com
    
    Subscribers: alandau, bmatheny, yfeldblum, smarlow, watashi, akr
    
    FB internal diff: D2392467
    
    Tasks: 8018306
    
    Signature: t1:2392467:1441617130:9b6def37f24f8caf9d5dcc595a9e81f1231a92bd

---
+++ thrift/lib/hs/tests/VersionTest.hs
@@ -0,0 +1,26 @@
+module VersionTest where
+
+import Data.Int
+import Data.Functor
+import Test.QuickCheck
+import Thrift.Protocol.Binary
+import Thrift.Protocol.Compact
+import Thrift.Protocol.JSON
+import Thrift.Protocol.SimpleJSON
+import Thrift.Transport
+import Thrift.Transport.Empty
+
+import Version_Types
+
+import Util
+
+prop_roundtrip :: (Transport t, Protocol p) => p t -> Int32 -> Int32 -> Bool
+prop_roundtrip p x y = Foo1 x == decode_Foo1 p (encode_Foo2 p $ Foo2 x y)
+
+main :: IO ()
+main = aggregateResults $ quickCheckResult <$>
+       [ prop_roundtrip $ BinaryProtocol EmptyTransport
+       , prop_roundtrip $ SimpleJSONProtocol EmptyTransport
+       , prop_roundtrip $ JSONProtocol EmptyTransport
+       , prop_roundtrip $ CompactProtocol EmptyTransport
+       ]
---
+++ thrift/lib/hs/tests/if/version.thrift
@@ -0,0 +1,8 @@
+struct Foo1 {
+  1: i32 bar,
+}
+
+struct Foo2 {
+  1: i32 bar,
+  2: i32 baz,
+}
