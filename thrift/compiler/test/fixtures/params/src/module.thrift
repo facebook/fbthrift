2062245f22cd10f2514f4c970fd6c0ea720d01b3
Cullen Walsh <ckwalsh@fb.com>
2015-11-03 10:29:15 -0800

    Thrift fixtures for params using containers
    Test Plan: Generated code
    
    Reviewed By: yfeldblum@fb.com
    
    Subscribers: alandau, yfeldblum
    
    FB internal diff: D2606084
    
    Signature: t1:2606084:1446495892:2a12ec65de38a4a28e15b2c0e5921b053d7428d0

---
+++ thrift/compiler/test/fixtures/params/src/module.thrift
@@ -0,0 +1,8 @@
+service NestedContainers {
+  void mapList(1: map<i32, list<i32>> foo)
+  void mapSet(1: map<i32, set<i32>> foo)
+  void listMap(1: list<map<i32, i32>> foo)
+  void listSet(1: list<set<i32>> foo)
+
+ void turtles(1: list<list<map<i32, map<i32, set<i32>>>>> foo)
+}
