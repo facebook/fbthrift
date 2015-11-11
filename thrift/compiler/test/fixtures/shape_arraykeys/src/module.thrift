22bd8e7a87d25019401ee94f2952cbd5f0a2d99d
Jim Hunt <jhunt@fb.com>
2015-11-10 22:21:25 -0800

    part 1: add fixtures
    Summary:
    We want the to-php thrift compilation to add a
    
    class A {
    const type TShape = shape (...);
    
    to each PHP file (when certain options are enabled).
    
    This diff is the first in a series of diffs. Here, I add a bunch of
    'fixtures' (example compiled php files) so that subsequent diffs will
    be eaiser to understand.
    
    Note: I'm later going to add a
    
    shape_arraykeys
    
    thrift option and modify the 'cmd' file in the added
    
    test/fixtures/shape_arraykeys/cmd
    
    Yeah, at the moment that directory looks duplicative with
    
    test/fixtures/shapes/cmd
    
    It will be more clear when I stack diffs on top of this one.
    
    Test Plan:
    Generate the fixtures; observe that the generated code looks
    broken. Rebase later diffs; make sure they look good.
    
    Reviewed By: yfeldblum@fb.com
    
    Subscribers: alandau
    
    FB internal diff: D2630288
    
    Tasks: 8982701
    
    Signature: t1:2630288:1446948245:4914b5bc1fe376eed2acbfddfbe56dad28bf0d52

---
+++ thrift/compiler/test/fixtures/shape_arraykeys/src/module.thrift
@@ -0,0 +1,33 @@
+struct A {
+  1: string a;
+}
+
+struct B {
+  1: A just_an_A;
+  2: set<i32> set_of_i32;
+  3: list<i32> list_of_i32;
+  4: list<string> list_of_i32;
+  5: map<string, i32> map_of_string_to_i32;
+  6: map<string, A> map_of_string_to_A;
+  7: map<string, list<i32>> map_of_string_to_list_of_i32;
+  8: map<string, list<A>> map_of_string_to_list_of_A;
+  9: map<string, set<i32>> map_of_string_to_set_of_i32;
+  10: map<string, map<string, i32>> map_of_string_to_map_of_string_to_i32;
+  11: map<string, map<string, A>> map_of_string_to_map_of_string_to_A;
+  12: list<set<i32>> list_of_set_of_i32;
+  13: list<map<string, list<A>>> list_of_map_of_string_to_list_of_A;
+  14: list<map<string, A>> list_of_map_of_string_to_A;
+  15: list<B> list_of_self;
+  16: map<string, B> map_of_string_to_self;
+
+  51: optional A optional_just_an_A;
+  52: optional set<i32>  optional_set_of_i32;
+  53: optional list<i32>  optional_list_of_i32;
+  54: optional list<string>  optional_list_of_i32;
+  55: optional map<string, i32>  optional_map_of_string_to_i32;
+  56: optional map<string, A>  optional_map_of_string_to_A;
+  57: optional map<string, list<i32>>  optional_map_of_string_to_list_of_i32;
+  58: optional map<string, list<A>>  optional_map_of_string_to_list_of_A;
+  59: optional map<string, set<i32>>  optional_map_of_string_to_set_of_i32;
+
+}
