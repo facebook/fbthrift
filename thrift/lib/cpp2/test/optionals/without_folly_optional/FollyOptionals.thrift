36c2605886766a73d3b98435a9398d61151fbc5d
Marcelo Juchem <marcelo@fb.com>
2015-12-30 17:04:21 -0800

    properly organizing thrift files
    Summary:
    our guidelines tell us that a source file should not be referenced by more than one build target
    that makes it easier to work with different build systems
    this diff properly organizes thrift files in order to avoid such duplicate reference
    
    Reviewed By: yfeldblum
    
    Differential Revision: D2796767
    
    fb-gh-sync-id: 40143486891406d9dc70d850773b52c9ee18ba35

---
+++ thrift/lib/cpp2/test/optionals/without_folly_optional/FollyOptionals.thrift
@@ -0,0 +1,36 @@
+enum HasOptionalsTestEnum {
+  FOO = 1,
+  BAR
+}
+
+struct HasOptionalsExtra {
+  1: i64 extraInt64Req;
+  2: optional i64 extraInt64Opt;
+  3: string extraStringReq;
+  4: optional string extraStringOpt;
+  5: set<i64> extraSetReq;
+  6: optional set<i64> extraSetOpt;
+  7: list<i64> extraListReq;
+  8: optional list<i64> extraListOpt;
+  9: map<i64, i64> extraMapReq;
+  10: optional map<i64, i64> extraMapOpt;
+  11: HasOptionalsTestEnum extraEnumReq;
+  12: optional HasOptionalsTestEnum extraEnumOpt;
+}
+
+struct HasOptionals {
+  1: i64 int64Req;
+  2: optional i64 int64Opt;
+  3: string stringReq;
+  4: optional string stringOpt;
+  5: set<i64> setReq;
+  6: optional set<i64> setOpt;
+  7: list<i64> listReq;
+  8: optional list<i64> listOpt;
+  9: map<i64, i64> mapReq;
+  10: optional map<i64, i64> mapOpt;
+  11: HasOptionalsTestEnum enumReq;
+  12: optional HasOptionalsTestEnum enumOpt;
+  13: HasOptionalsExtra structReq;
+  14: optional HasOptionalsExtra structOpt;
+}
