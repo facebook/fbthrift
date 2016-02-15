652ae94ec5bec1b8bed00cba971f02ec470d6c6c
Marcelo Juchem <marcelo@fb.com>
2016-02-15 09:25:19 -0800

    A debug helper for comparing objects
    Summary:this helper makes it waaaay easier to figure out differences between
    deeply nested objects. Think a big JSON decoded into a Thrift structure, for
    instance.
    
    Reviewed By: yfeldblum
    
    Differential Revision: D2924309
    
    fb-gh-sync-id: 2b724fdcc8733b1fbc892914a733e2f6c1f12c5a
    shipit-source-id: 2b724fdcc8733b1fbc892914a733e2f6c1f12c5a

---
+++ thrift/lib/cpp2/fatal/debug-inl-post.h
@@ -0,0 +1,349 @@
+/*
+ * Copyright 2016 Facebook, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#ifndef THRIFT_FATAL_DEBUG_POST_INL_H_
+#define THRIFT_FATAL_DEBUG_POST_INL_H_ 1
+
+#include <thrift/lib/cpp2/fatal/reflect_category.h>
+
+#include <folly/Conv.h>
+
+#include <fatal/type/enum.h>
+#include <fatal/type/transform.h>
+#include <fatal/type/variant_traits.h>
+
+#include <stdexcept>
+#include <type_traits>
+#include <utility>
+
+#include <cassert>
+
+namespace apache { namespace thrift { namespace detail {
+
+template <thrift_category> struct debug_equals_impl;
+
+template <typename T, typename Callback>
+bool debug_equals(
+  std::string &path,
+  T const &lhs,
+  T const &rhs,
+  Callback &&callback
+) {
+  using impl = detail::debug_equals_impl<
+    reflect_category<typename std::decay<T>::type>::value
+  >;
+
+  static_assert(
+    fatal::is_complete<impl>::value,
+    "debug_equals: unsupported type"
+  );
+
+  return impl::equals(path, lhs, rhs, callback);
+}
+
+struct scoped_path {
+  template <typename... Args>
+  explicit scoped_path(std::string &path, Args &&...args):
+    size_(path.size()),
+    path_(path)
+  {
+    if (!path.empty()) {
+      path.push_back('.');
+    }
+
+    folly::toAppend(std::forward<Args>(args)..., &path);
+  }
+
+  ~scoped_path() {
+    assert(path_.size() >=  size_);
+    path_.resize(size_);
+  }
+
+private:
+  std::size_t const size_;
+  std::string &path_;
+};
+
+template <> struct debug_equals_impl<thrift_category::enumeration> {
+  template <typename T, typename Callback>
+  static bool equals(
+    std::string &path,
+    T const &lhs,
+    T const &rhs,
+    Callback &&callback
+  ) {
+    if (lhs != rhs) {
+      callback(
+        fatal::enum_to_string(lhs, "<unknown>"),
+        fatal::enum_to_string(rhs, "<unknown>"),
+        path,
+        "value mismatch"
+      );
+      return false;
+    }
+    return true;
+  }
+};
+
+template <> struct debug_equals_impl<thrift_category::list> {
+  template <typename T, typename Callback>
+  static bool equals(
+    std::string &path,
+    T const &lhs,
+    T const &rhs,
+    Callback &&callback
+  ) {
+    using traits = thrift_list_traits<T>;
+
+    if (traits::size(lhs) != traits::size(rhs)) {
+      callback(traits::size(lhs), traits::size(rhs), path, "size mismatch");
+      return false;
+    }
+
+    auto l = traits::begin(lhs);
+    auto const le = traits::end(lhs);
+    auto r = traits::begin(rhs);
+    auto const re = traits::end(rhs);
+    for (std::size_t index = 0; l != le && r != re; ++l, ++r, ++index) {
+      scoped_path guard(path, index);
+
+      if (!debug_equals(path, *l, *r, callback)) {
+        return false;
+      }
+    }
+    assert(l == le);
+    assert(r == re);
+    return true;
+  }
+};
+
+template <> struct debug_equals_impl<thrift_category::map> {
+  template <typename T, typename Callback>
+  static bool equals(
+    std::string &path,
+    T const &lhs,
+    T const &rhs,
+    Callback &&callback
+  ) {
+    using traits = thrift_map_traits<T>;
+
+    if (traits::size(lhs) != traits::size(rhs)) {
+      callback(traits::size(lhs), traits::size(rhs), path, "size mismatch");
+      return false;
+    }
+    auto l = traits::begin(lhs);
+    auto const le = traits::end(lhs);
+    auto r = traits::begin(rhs);
+    auto const re = traits::end(rhs);
+    for (std::size_t index = 0; l != le && r != re; ++l, ++r, ++index) {
+      scoped_path guard(path, index);
+
+      if (!debug_equals(path, traits::key(l), traits::key(r), callback)
+        || !debug_equals(path, traits::mapped(l), traits::mapped(r), callback)
+      ) {
+        return false;
+      }
+    }
+    assert(l == le);
+    assert(r == re);
+    return true;
+  }
+};
+
+template <> struct debug_equals_impl<thrift_category::set> {
+  template <typename T, typename Callback>
+  static bool equals(
+    std::string &path,
+    T const &lhs,
+    T const &rhs,
+    Callback &&callback
+  ) {
+    using traits = thrift_set_traits<T>;
+
+    if (traits::size(lhs) != traits::size(rhs)) {
+      callback(traits::size(lhs), traits::size(rhs), path, "size mismatch");
+      return false;
+    }
+
+    auto l = traits::begin(lhs);
+    auto const le = traits::end(lhs);
+    auto r = traits::begin(rhs);
+    auto const re = traits::end(rhs);
+    for (std::size_t index = 0; l != le && r != re; ++l, ++r, ++index) {
+      scoped_path guard(path, index);
+
+      if (!debug_equals(path, *l, *r, callback)) {
+        return false;
+      }
+    }
+    assert(l == le);
+    assert(r == re);
+    return true;
+  }
+};
+
+template <typename VariantTraits>
+struct debug_equals_variant_visitor {
+  template <
+    typename Id, typename Descriptor, std::size_t Index,
+    typename Needle, typename T, typename Callback
+  >
+  void operator ()(
+    fatal::indexed_type_pair_tag<Id, Descriptor, Index>,
+    Needle,
+    std::string &path,
+    T const &lhs,
+    T const &rhs,
+    Callback &&callback,
+    bool &result
+  ) const {
+    assert(Id::value == VariantTraits::get_id(lhs));
+    assert(Id::value == VariantTraits::get_id(rhs));
+
+    using name = typename fatal::enum_traits<typename VariantTraits::id>
+      ::value_to_name::template get<Id>;
+    scoped_path guard(path, name::z_data());
+
+    typename Descriptor::getter getter;
+    result = debug_equals(path, getter(lhs), getter(rhs), callback);
+  }
+};
+
+template <> struct debug_equals_impl<thrift_category::variant> {
+  template <typename T, typename Callback>
+  static bool equals(
+    std::string &path,
+    T const &lhs,
+    T const &rhs,
+    Callback &&callback
+  ) {
+    using traits = fatal::variant_traits<T>;
+
+    if (traits::get_id(lhs) != traits::get_id(rhs)) {
+      callback(
+        fatal::enum_to_string(traits::get_id(lhs), "<unknown>"),
+        fatal::enum_to_string(traits::get_id(rhs), "<unknown>"),
+        path,
+        "variant stored type mismatch"
+      );
+      return false;
+    }
+
+    bool result = true;
+    traits::by_id::map::template binary_search<>::exact(
+      lhs.getType(),
+      debug_equals_variant_visitor<traits>(),
+      path,
+      lhs,
+      rhs,
+      callback,
+      result
+    );
+
+    return result;
+  }
+};
+
+struct debug_equals_struct_visitor {
+  template <typename Member, std::size_t Index, typename T, typename Callback>
+  void operator ()(
+    fatal::indexed_type_tag<Member, Index>,
+    std::string &path,
+    T const &lhs,
+    T const &rhs,
+    Callback &&callback,
+    bool &result
+  ) const {
+    if (!result) {
+      return;
+    }
+
+    scoped_path guard(path, Member::name::z_data());
+
+    using getter = typename Member::getter;
+    result = debug_equals(path, getter::ref(lhs), getter::ref(rhs), callback);
+  }
+};
+
+template <> struct debug_equals_impl<thrift_category::structure> {
+  template <typename T, typename Callback>
+  static bool equals(
+    std::string &path,
+    T const &lhs,
+    T const &rhs,
+    Callback &&callback
+  ) {
+    bool result = true;
+
+    reflect_struct<T>::members::mapped::foreach(
+      debug_equals_struct_visitor(),
+      path, lhs, rhs, callback, result
+    );
+
+    return result;
+  }
+};
+
+template <> struct debug_equals_impl<thrift_category::string> {
+  template <typename T, typename Callback>
+  static bool equals(
+    std::string &path,
+    T const &lhs,
+    T const &rhs,
+    Callback &&callback
+  ) {
+    if (lhs != rhs) {
+      callback(lhs, rhs, path, "string mismatch");
+      return false;
+    }
+    return true;
+  }
+};
+
+template <> struct debug_equals_impl<thrift_category::floating_point> {
+  template <typename T, typename Callback>
+  static bool equals(
+    std::string &path,
+    T const &lhs,
+    T const &rhs,
+    Callback &&callback
+  ) {
+    if (lhs != rhs) {
+      callback(lhs, rhs, path, "floating point value mismatch");
+      return false;
+    }
+    return true;
+  }
+};
+
+template <> struct debug_equals_impl<thrift_category::integral> {
+  template <typename T, typename Callback>
+  static bool equals(
+    std::string &path,
+    T const &lhs,
+    T const &rhs,
+    Callback &&callback
+  ) {
+    if (lhs != rhs) {
+      callback(lhs, rhs, path, "integral value mismatch");
+      return false;
+    }
+    return true;
+  }
+};
+
+}}} // apache::thrift::detail
+
+#endif // THRIFT_FATAL_DEBUG_POST_INL_H_
---
+++ thrift/test/fatal_debug_test.cpp
@@ -0,0 +1,262 @@
+/*
+ * Copyright 2016 Facebook, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <thrift/lib/cpp2/fatal/debug.h>
+
+#include <thrift/test/gen-cpp2/reflection_fatal_enum.h>
+#include <thrift/test/gen-cpp2/reflection_fatal_struct.h>
+#include <thrift/test/gen-cpp2/reflection_fatal_union.h>
+
+#include <gtest/gtest.h>
+
+#include <string>
+#include <vector>
+
+namespace test_cpp2 {
+namespace cpp_reflection {
+
+struct3 test_data() {
+  structA a1;
+  a1.a = 99;
+  a1.b = "abc";
+  structA a2;
+  a2.a = 1001;
+  a2.b = "foo";
+  structA a3;
+  a3.a = 654;
+  a3.b = "bar";
+  structA a4;
+  a4.a = 9791;
+  a4.b = "baz";
+  structA a5;
+  a5.a = 111;
+  a5.b = "gaz";
+
+  structB b1;
+  b1.c = 1.23;
+  b1.d = true;
+  structB b2;
+  b2.c = 9.8;
+  b2.d = false;
+  structB b3;
+  b3.c = 10.01;
+  b3.d = true;
+  structB b4;
+  b4.c = 159.73;
+  b4.d = false;
+  structB b5;
+  b5.c = 468.02;
+  b5.d = true;
+
+  struct3 pod;
+
+  pod.fieldA = 141;
+  pod.fieldB = "this is a test";
+  pod.fieldC = enum1::field0;
+  pod.fieldD = enum2::field1_2;
+  pod.fieldE.set_ud(5.6);
+  pod.fieldF.set_us_2("this is a variant");
+  pod.fieldG.field0 = 98;
+  pod.fieldG.field1 = "hello, world";
+  pod.fieldG.field2 = enum1::field2;
+  pod.fieldG.field3 = enum2::field0_2;
+  pod.fieldG.field4.set_ui(19937);
+  pod.fieldG.field5.set_ue_2(enum1::field1);
+  // fieldH intentionally left empty
+  pod.fieldI = {3, 5, 7, 9};
+  pod.fieldJ = {"a", "b", "c", "d"};
+  pod.fieldK = {};
+  pod.fieldL.push_back(a1);
+  pod.fieldL.push_back(a2);
+  pod.fieldL.push_back(a3);
+  pod.fieldL.push_back(a4);
+  pod.fieldL.push_back(a5);
+  pod.fieldM = {2, 4, 6, 8};
+  pod.fieldN = {"w", "x", "y", "z"};
+  pod.fieldO = {};
+  pod.fieldP = {b1, b2, b3, b4, b5};
+  pod.fieldQ = {{"a1", a1}, {"a2", a2}, {"a3", a3}};
+  pod.fieldR = {};
+
+  return pod;
+}
+
+struct test_callback {
+  explicit test_callback(std::vector<std::string> &out): out_(out) {}
+
+  template <typename  T>
+  void operator ()(
+    T const &,
+    T const &,
+    folly::StringPiece path,
+    folly::StringPiece
+  ) const {
+    out_.emplace_back(path.data(), path.size());
+  }
+
+private:
+  std::vector<std::string> &out_;
+};
+
+#define TEST_IMPL(LHS, ...) \
+  do { \
+    auto const rhs = test_data(); \
+    std::vector<std::string> const expected{__VA_ARGS__}; \
+    \
+    std::vector<std::string> actual; \
+    actual.reserve(expected.size()); \
+    \
+    EXPECT_EQ( \
+      expected.empty(), \
+      (apache::thrift::debug_equals(LHS, rhs, test_callback(actual))) \
+    ); \
+    \
+    EXPECT_EQ(expected, actual); \
+  } while (false)
+
+TEST(fatal_debug, equal) {
+  TEST_IMPL(test_data());
+}
+
+TEST(fatal_debug, fieldA) {
+  auto pod = test_data();
+  pod.fieldA = 90;
+  TEST_IMPL(pod, "<root>.fieldA");
+  pod.fieldA = 141;
+  TEST_IMPL(pod);
+}
+
+TEST(fatal_debug, fieldB) {
+  auto pod = test_data();
+  pod.fieldB = "should mismatch";
+  TEST_IMPL(pod, "<root>.fieldB");
+  pod.fieldB = "this is a test";
+  TEST_IMPL(pod);
+}
+
+TEST(fatal_debug, fieldC) {
+  auto pod = test_data();
+  pod.fieldC = enum1::field2;
+  TEST_IMPL(pod, "<root>.fieldC");
+  pod.fieldC = enum1::field0;
+  TEST_IMPL(pod);
+}
+
+TEST(fatal_debug, fieldE) {
+  auto pod = test_data();
+  pod.fieldE.set_ui(5);
+  TEST_IMPL(pod, "<root>.fieldE");
+  pod.fieldE.__clear();
+  TEST_IMPL(pod, "<root>.fieldE");
+  pod.fieldE.set_ud(4);
+  TEST_IMPL(pod, "<root>.fieldE.ud");
+  pod.fieldE.set_ud(5.6);
+  TEST_IMPL(pod);
+}
+
+TEST(fatal_debug, fieldH) {
+  auto pod = test_data();
+  pod.fieldH.set_ui_2(3);
+  TEST_IMPL(pod, "<root>.fieldH");
+  pod.fieldH.__clear();
+  TEST_IMPL(pod);
+}
+
+TEST(fatal_debug, fieldI) {
+  auto pod = test_data();
+  pod.fieldI[0] = 4;
+  TEST_IMPL(pod, "<root>.fieldI.0");
+  pod.fieldI[0] = 3;
+  TEST_IMPL(pod);
+  pod.fieldI[2] = 10;
+  TEST_IMPL(pod, "<root>.fieldI.2");
+  pod.fieldI.push_back(11);
+  TEST_IMPL(pod, "<root>.fieldI");
+  pod.fieldI.clear();
+  TEST_IMPL(pod, "<root>.fieldI");
+}
+
+TEST(fatal_debug, fieldM) {
+  auto pod = test_data();
+  pod.fieldM.clear();
+  TEST_IMPL(pod, "<root>.fieldM");
+  pod.fieldM.insert(11);
+  pod.fieldM.insert(12);
+  pod.fieldM.insert(13);
+  pod.fieldM.insert(14);
+  TEST_IMPL(pod, "<root>.fieldM.0");
+  pod.fieldM = test_data().fieldM;
+  TEST_IMPL(pod);
+}
+
+TEST(fatal_debug, fieldQ) {
+  auto pod = test_data();
+  pod.fieldQ.clear();
+  TEST_IMPL(pod, "<root>.fieldQ");
+  structA a1;
+  a1.a = 1;
+  a1.b = "1";
+  structA a2;
+  a2.a = 2;
+  a2.b = "2";
+  structA a3;
+  a3.a = 3;
+  a3.b = "3";
+  pod.fieldQ["A1"] = a1;
+  pod.fieldQ["A2"] = a2;
+  pod.fieldQ["A3"] = a3;
+  TEST_IMPL(pod, "<root>.fieldQ.0");
+  pod.fieldQ = test_data().fieldQ;
+  TEST_IMPL(pod);
+}
+
+TEST(fatal_debug, fieldG_field0) {
+  auto pod = test_data();
+  pod.fieldG.field0 = 12;
+  TEST_IMPL(pod, "<root>.fieldG.field0");
+  pod.fieldG.field0 = 98;
+  TEST_IMPL(pod);
+}
+
+TEST(fatal_debug, fieldG_field1) {
+  auto pod = test_data();
+  pod.fieldG.field1 = "should mismatch";
+  TEST_IMPL(pod, "<root>.fieldG.field1");
+  pod.fieldG.field1 = "hello, world";
+  TEST_IMPL(pod);
+}
+
+TEST(fatal_debug, fieldG_field2) {
+  auto pod = test_data();
+  pod.fieldG.field2 = enum1::field1;
+  TEST_IMPL(pod, "<root>.fieldG.field2");
+  pod.fieldG.field2 = enum1::field2;
+  TEST_IMPL(pod);
+}
+
+TEST(fatal_debug, fieldG_field5) {
+  auto pod = test_data();
+  pod.fieldG.field5.set_ui_2(5);
+  TEST_IMPL(pod, "<root>.fieldG.field5");
+  pod.fieldG.field5.__clear();
+  TEST_IMPL(pod, "<root>.fieldG.field5");
+  pod.fieldG.field5.set_ue_2(enum1::field0);
+  TEST_IMPL(pod, "<root>.fieldG.field5.ue_2");
+  pod.fieldG.field5.set_ue_2(enum1::field1);
+  TEST_IMPL(pod);
+}
+} // namespace cpp_compat {
+} // namespace test_cpp2 {
