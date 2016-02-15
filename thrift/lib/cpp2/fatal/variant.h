30e2f9c2df05354522502168003e1d38b226d9bd
Marcelo Juchem <marcelo@fb.com>
2016-02-15 09:51:58 -0800

    Implementing variant-like accessor helpers for Thrift unions
    Summary:Andrew Cox and I had a discussion on whether having variant-like setters is desired for Thrift unions.
    
    The outcome of that was:
    
    Thrift unions diverged from actual variants since the very beginning, since they're modeled after C unions. Most of the differences can be mitigated, except for one: variants are governed by types, while unions are governed by fields.
    
    It doesn't make sense for a variant to have more than one field with the same type, differing only by name, because fields don't really have names:
    
      variant<int, double, string> v;
    
    Whereas in a Thrift union, field names matter and they can actually represent data:
    
      union my_union {
        string result,
        string logical_error,
        int system_error
      };
    
    Whether this is good practice or not can be argued with strong arguments for both sides.
    There are definitely better examples, but this is an illustration of what might already happen out there.
    Backward compatibility matters, so we can't simply drop support for multiple fields with the same type.
    
    The critical question is: what do we generate for overloaded setters when two fields have the same name?
    Do we default to one particular field? That definitely doesn't fit all cases.
    Do we simply skip generating overloaded setters for this one case? Do we warn? What about users who don't care about them, should they be warned too?
    Just like backwards compatibility, uniformity matters too.
    We could make those setters optional, with an additional flag to the thrift compiler to gate their generation, but anecdotal evidence and experience with other internal infrastructure at Facebook showed us that the combinatorics of multiple options greatly complicate stability and testability of the software, due to unexpected corner cases for different option combinations.
    
    For this reason, we're not changing the compiler to generate overloaded setters for unions.
    
    That being said, given that we already have support for compile-time reflection, I'm providing helper functions to achieve the same goal.
    
    Just to illustrate, variants usually provide a member function as the setter, like this:
    
      variant <int, double, string> v;
      v.set(10); // sets an int
      v.set(5.6); // sets a double
    
    The helper is obviously not a member function, but a free function. Therefore, syntax is slightly different, yet with equal functionality:
    
      // foo.thrift
      union my_union {
        int a;
        double b;
        string c;
      };
    
      // foo.cpp
      my_union v;
      variant_set(v, 10); // sets a
      variant_set(v, 5.6); // sets b
    
    Reviewed By: ericniebler
    
    Differential Revision: D2893885
    
    fb-gh-sync-id: e18bd70ab9349913e5b2e9f05947a18e6aa336e4
    shipit-source-id: e18bd70ab9349913e5b2e9f05947a18e6aa336e4

---
+++ thrift/lib/cpp2/fatal/variant.h
@@ -0,0 +1,283 @@
+/*
+ * Copyright 2016 Facebook, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#ifndef THRIFT_FATAL_VARIANT_H_
+#define THRIFT_FATAL_VARIANT_H_ 1
+
+#include <thrift/lib/cpp2/fatal/reflection.h>
+
+#include <fatal/type/transform.h>
+
+#include <memory>
+#include <stdexcept>
+#include <type_traits>
+#include <utility>
+
+#include <cassert>
+
+namespace apache { namespace thrift {
+namespace detail {
+template <typename, typename> struct variant_helper;
+} // detail
+
+/**
+ * READ ME FIRST: this header enhances Thrift unions with variant-style
+ * functionality.
+ *
+ * Please refer to the top of `thrift/lib/cpp2/fatal/reflection.h` on how to
+ * enable compile-time reflection for unions. The present header relies on it
+ * for its functionality.
+ *
+ * TROUBLESHOOTING:
+ *  - make sure you've followed the instructions on `reflection.h` to enable
+ *    generation of compile-time reflection;
+ *  - make sure you've included the union metadata for your Thrift union, as
+ *    specified in `reflection.h`;
+ *  - make sure exactly one of the Thrift union's field map to a specific type.
+ *
+ * @author: Marcelo Juchem <marcelo@fb.com>
+ */
+
+/**
+ * Gets a reference to the Thrift union's member for the given type.
+ *
+ * NOTE: this is an unchecked version of the variant getter. If the desired
+ * field is not the one currently set in the union, the behavior of this call
+ * is undefined. For a safer alternative, use either `variant_checked_get` or
+ * `variant_try_get`.
+ *
+ * Example:
+ *
+ *  // Foo.thrift
+ *  union MyUnion {
+ *    1: i32 a
+ *    2: string b
+ *    3: double c
+ *  }
+ *
+ *  // foo.cpp
+ *  #include "project_dir/gen-cpp2/Foo_fatal_union.h"
+ *  #include <thrift/lib/cpp2/fatal/variant.h>
+ *
+ *  MyUnion u;
+ *  u.set_a(10);
+ *
+ *  // yields a reference to field `a`
+ *  std::cout << variant_get<std::int32_t>(u);
+ *
+ *  // undefined behavior
+ *  std::cout << variant_get<double>(u);
+ *
+ * @author: Marcelo Juchem <marcelo@fb.com>
+ */
+template <typename T, typename V>
+auto variant_get(V &&variant) -> decltype(
+  detail::variant_helper<T, V>::traits::by_type
+    ::template get<T>(std::forward<V>(variant))
+) {
+  using traits = typename detail::variant_helper<T, V>::traits;
+  assert(traits::get_id(variant) == traits::by_type::template id<T>::value);
+  return traits::by_type::template get<T>(std::forward<V>(variant));
+}
+
+/**
+ * Gets a reference to the Thrift union's member for the given type.
+ *
+ * If the desired field is not the one currently set in the union, throws an
+ * `std::invalid_argument` exception.
+ *
+ * Example:
+ *
+ *  // Foo.thrift
+ *  union MyUnion {
+ *    1: i32 a
+ *    2: string b
+ *    3: double c
+ *  }
+ *
+ *  // foo.cpp
+ *  #include "project_dir/gen-cpp2/Foo_fatal_union.h"
+ *  #include <thrift/lib/cpp2/fatal/variant.h>
+ *
+ *  MyUnion u;
+ *  u.set_a(10);
+ *
+ *  // yields a reference to field `a`
+ *  std::cout << variant_checked_get<std::int32_t>(u);
+ *
+ *  // throws `std::invalid_argument`
+ *  std::cout << variant_checked_get<double>(u);
+ *
+ * @author: Marcelo Juchem <marcelo@fb.com>
+ */
+template <typename T, typename V>
+auto variant_checked_get(V &&variant) -> decltype(
+  detail::variant_helper<T, V>::traits::by_type
+    ::template get<T>(std::forward<V>(variant))
+) {
+  using traits = typename detail::variant_helper<T, V>::traits;
+
+  if (traits::get_id(variant) != traits::by_type::template id<T>::value) {
+    throw std::invalid_argument(
+      "type requested to variant_checked_get() is not the one stored in the"
+      " variant"
+    );
+  }
+
+  return traits::by_type::template get<T>(std::forward<V>(variant));
+}
+
+/**
+ * Gets a pointer to the Thrift union's member for the given type.
+ *
+ * If the desired field is not the one currently set in the union, returns
+ * `nullptr`.
+ *
+ * Example:
+ *
+ *  // Foo.thrift
+ *  union MyUnion {
+ *    1: i32 a
+ *    2: string b
+ *    3: double c
+ *  }
+ *
+ *  // foo.cpp
+ *  #include "project_dir/gen-cpp2/Foo_fatal_union.h"
+ *  #include <thrift/lib/cpp2/fatal/variant.h>
+ *
+ *  MyUnion u;
+ *  u.set_a(10);
+ *
+ *  // yields a pointer to field `a`
+ *  std::cout << variant_try_get<std::int32_t>(u);
+ *
+ *  // yields `nullptr`
+ *  std::cout << variant_try_get<double>(u);
+ *
+ * @author: Marcelo Juchem <marcelo@fb.com>
+ */
+template <typename T, typename V>
+auto variant_try_get(V &variant) -> decltype(
+  std::addressof(
+    detail::variant_helper<T, V>::traits::by_type::template get<T>(variant)
+  )
+) {
+  using traits = typename detail::variant_helper<T, V>::traits;
+
+  if (traits::get_id(variant) != traits::by_type::template id<T>::value) {
+    return nullptr;
+  }
+
+  return std::addressof(traits::by_type::template get<T>(variant));
+}
+
+/**
+ * Sets the Thrift union's member for the given type with the provided value.
+ *
+ * Example:
+ *
+ *  // Foo.thrift
+ *  union MyUnion {
+ *    1: i32 a
+ *    2: string b
+ *    3: double c
+ *  }
+ *
+ *  // foo.cpp
+ *  #include "project_dir/gen-cpp2/Foo_fatal_union.h"
+ *  #include <thrift/lib/cpp2/fatal/variant.h>
+ *
+ *  MyUnion u;
+ *
+ *  // sets the field `a` to the value `10`.
+ *  variant_set(u, 10);
+ *
+ *  std::cout << u.get_a();
+ *
+ * @author: Marcelo Juchem <marcelo@fb.com>
+ */
+template <typename V, typename T>
+typename std::decay<T>::type &variant_set(V &variant, T &&value) {
+  using type = typename std::decay<T>::type;
+  using by_type = typename detail::variant_helper<type, V>::traits::by_type;
+
+  by_type::template set<type>(variant, std::forward<T>(value));
+  return by_type::template get<type>(variant);
+}
+
+/**
+ * Sets the Thrift union's member for the given type by perfectly forwarding the
+ * given arguments to its constructor.
+ *
+ * Example:
+ *
+ *  // Foo.thrift
+ *  union MyUnion {
+ *    1: i32 a
+ *    2: string b
+ *    3: double c
+ *  }
+ *
+ *  // foo.cpp
+ *  #include "project_dir/gen-cpp2/Foo_fatal_union.h"
+ *  #include <thrift/lib/cpp2/fatal/variant.h>
+ *
+ *  MyUnion u;
+ *
+ *  // sets the field `b` to the string "hello, world".
+ *  variant_emplace<std::string>(u, "hello, world");
+ *
+ *  std::cout << u.get_b();
+ *
+ * @author: Marcelo Juchem <marcelo@fb.com>
+ */
+template <typename T, typename V, typename... Args>
+T &variant_emplace(V &variant, Args &&...args) {
+  using by_type = typename detail::variant_helper<T, V>::traits::by_type;
+
+  by_type::template set<T>(variant, std::forward<Args>(args)...);
+  return by_type::template get<T>(variant);
+}
+
+////////////////////////////
+// IMPLEMENTATION DETAILS //
+////////////////////////////
+
+namespace detail {
+
+template <typename T, typename V>
+struct variant_helper {
+  using traits = typename reflect_union<typename std::decay<V>::type>::traits;
+
+private:
+  using count = typename traits::by_type::tags::template filter<
+    fatal::transform_alias<std::is_same, T>::template apply
+  >;
+
+  static_assert(
+    !count::empty,
+    "no fields found for type passed to variant_set"
+  );
+  static_assert(
+    count::size == 1,
+    "multiple fields found for type passed to variant_set"
+  );
+};
+
+} // detail
+}} // apache::thrift
+
+#endif // THRIFT_FATAL_VARIANT_H_
