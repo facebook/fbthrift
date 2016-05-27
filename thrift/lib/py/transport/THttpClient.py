bc8031b0202fd51397ea365b34d1b9bb72051249
David Rothera <drothera@fb.com>
2016-05-27 04:45:46 -0700

    Fix Python3 bug in THttpClient
    Summary: 3 != >3
    
    Differential Revision: D3352815
    
    fbshipit-source-id: 996dcd7ee1f36e0e22afb9f51f05cbf35240ccde

--- thrift/lib/py/transport/THttpClient.py
+++ thrift/lib/py/transport/THttpClient.py
@@ -145,7 +145,7 @@ class THttpClient(TTransportBase):
             self.__http.putheader('User-Agent', user_agent)
 
         if self.__custom_headers:
-            if sys.version_info[0] > 3:
+            if sys.version_info[0] >= 3:
                 custom_headers_iter = self.__custom_headers.items()
             else:
                 custom_headers_iter = self.__custom_headers.iteritems()
