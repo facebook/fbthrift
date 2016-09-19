475fcd20d6de70b2a5440ee2ed4ccb3ff441f1c3
Dusty Phillips <dgp@fb.com>
2016-09-19 15:09:15 -0700

    Create c++ implementation to call into cython
    Summary:
    The whole point of this C++ wrapper is to have a small file that knows that it
    owns a PyObject and to pass that PyObject into a cython handler. I haven't
    actually tested that which I am trying to do, but the goal is to pass a promise
    down to Cython, then have Cython call into a coroutine and set the promise value
    whenever the coroutine finishes.
    
    Reviewed By: cjtenny
    
    Differential Revision: D3852150
    
    fbshipit-source-id: 07e7007c5cc275ce17664af6b9dc3dfb5eff508c

--- thrift/compiler/test/fixtures/basic/gen-py3/module_service_wrapper.cpp
+++ thrift/compiler/test/fixtures/basic/gen-py3/module_service_wrapper.cpp
@@ -1,4 +1,5 @@
-#include <src/gen_py3/module_service_wrapper.h>
+#include <src/gen-py3/module_service_wrapper.h>
+#include <src/gen-py3/cy_module_service_wrapper.h>
 
 
 MyServiceWrapper::MyServiceWrapper(PyObject *obj)
@@ -11,7 +12,79 @@ MyServiceWrapper::~MyServiceWrapper() {
     Py_XDECREF(this->if_object);
 }
 
-std::shared_ptr<ServerInterface> MyServiceInterface(PyObject *if_object);
+folly::Future<folly::Unit> MyServiceWrapper::future_ping() {
+  auto promise = make_shared<folly::Promise<folly::Unit>>();
+  call_cy_MyService_ping(
+    this->if_object,
+    promise
+  );
+  return promise->getFuture();
+}
+
+folly::Future<std::unique_ptr<std::string>> MyServiceWrapper::future_getRandomData() {
+  auto promise = make_shared<folly::Promise<std::unique_ptr<std::string>>>();
+  call_cy_MyService_getRandomData(
+    this->if_object,
+    promise
+  );
+  return promise->getFuture();
+}
+
+folly::Future<bool> MyServiceWrapper::future_hasDataById(
+  int64_t id
+) {
+  auto promise = make_shared<folly::Promise<bool>>();
+  call_cy_MyService_hasDataById(
+    this->if_object,
+    promise,
+    id
+  );
+  return promise->getFuture();
+}
+
+folly::Future<std::unique_ptr<std::string>> MyServiceWrapper::future_getDataById(
+  int64_t id
+) {
+  auto promise = make_shared<folly::Promise<std::unique_ptr<std::string>>>();
+  call_cy_MyService_getDataById(
+    this->if_object,
+    promise,
+    id
+  );
+  return promise->getFuture();
+}
+
+folly::Future<folly::Unit> MyServiceWrapper::future_putDataById(
+  int64_t id,
+  std::unique_ptr<std::string> data
+) {
+  auto promise = make_shared<folly::Promise<folly::Unit>>();
+  call_cy_MyService_putDataById(
+    this->if_object,
+    promise,
+    id,
+    data
+  );
+  return promise->getFuture();
+}
+
+folly::Future<folly::Unit> MyServiceWrapper::future_lobDataById(
+  int64_t id,
+  std::unique_ptr<std::string> data
+) {
+  auto promise = make_shared<folly::Promise<folly::Unit>>();
+  call_cy_MyService_lobDataById(
+    this->if_object,
+    promise,
+    id,
+    data
+  );
+  return promise->getFuture();
+}
+
+std::shared_ptr<ServerInterface> MyServiceInterface(PyObject *if_object) {
+  return std::make_shared<MyServiceWrapper>(if_object);
+}
 
 
 MyServiceFastWrapper::MyServiceFastWrapper(PyObject *obj)
@@ -24,4 +97,76 @@ MyServiceFastWrapper::~MyServiceFastWrapper() {
     Py_XDECREF(this->if_object);
 }
 
-std::shared_ptr<ServerInterface> MyServiceFastInterface(PyObject *if_object);
+folly::Future<folly::Unit> MyServiceFastWrapper::future_ping() {
+  auto promise = make_shared<folly::Promise<folly::Unit>>();
+  call_cy_MyServiceFast_ping(
+    this->if_object,
+    promise
+  );
+  return promise->getFuture();
+}
+
+folly::Future<std::unique_ptr<std::string>> MyServiceFastWrapper::future_getRandomData() {
+  auto promise = make_shared<folly::Promise<std::unique_ptr<std::string>>>();
+  call_cy_MyServiceFast_getRandomData(
+    this->if_object,
+    promise
+  );
+  return promise->getFuture();
+}
+
+folly::Future<bool> MyServiceFastWrapper::future_hasDataById(
+  int64_t id
+) {
+  auto promise = make_shared<folly::Promise<bool>>();
+  call_cy_MyServiceFast_hasDataById(
+    this->if_object,
+    promise,
+    id
+  );
+  return promise->getFuture();
+}
+
+folly::Future<std::unique_ptr<std::string>> MyServiceFastWrapper::future_getDataById(
+  int64_t id
+) {
+  auto promise = make_shared<folly::Promise<std::unique_ptr<std::string>>>();
+  call_cy_MyServiceFast_getDataById(
+    this->if_object,
+    promise,
+    id
+  );
+  return promise->getFuture();
+}
+
+folly::Future<folly::Unit> MyServiceFastWrapper::future_putDataById(
+  int64_t id,
+  std::unique_ptr<std::string> data
+) {
+  auto promise = make_shared<folly::Promise<folly::Unit>>();
+  call_cy_MyServiceFast_putDataById(
+    this->if_object,
+    promise,
+    id,
+    data
+  );
+  return promise->getFuture();
+}
+
+folly::Future<folly::Unit> MyServiceFastWrapper::future_lobDataById(
+  int64_t id,
+  std::unique_ptr<std::string> data
+) {
+  auto promise = make_shared<folly::Promise<folly::Unit>>();
+  call_cy_MyServiceFast_lobDataById(
+    this->if_object,
+    promise,
+    id,
+    data
+  );
+  return promise->getFuture();
+}
+
+std::shared_ptr<ServerInterface> MyServiceFastInterface(PyObject *if_object) {
+  return std::make_shared<MyServiceFastWrapper>(if_object);
+}
--- thrift/compiler/test/fixtures/basic/gen-py3/module_service_wrapper.h
+++ thrift/compiler/test/fixtures/basic/gen-py3/module_service_wrapper.h
@@ -1,6 +1,6 @@
 #pragma once
-#include <src/gen_cpp2/MyService.h>
-#include <src/gen_cpp2/MyServiceFast.h>
+#include <src/gen-cpp2/MyService.h>
+#include <src/gen-cpp2/MyServiceFast.h>
 #include <Python.h>
 #include <memory.h>
 
@@ -11,25 +11,23 @@ class MyServiceWrapper : public MyServiceSvAsyncIf {
   public:
     explicit MyServiceWrapper(PyObject *if_object);
     virtual ~MyServiceWrapper();
-    folly::Future<folly::Unit> future_ping(
-    );
-    folly::Future<std::unique_ptr<std::string>> future_getRandomData(
-    );
-    folly::Future<bool> future_hasDataById(
-      int64_t id
-    );
-    folly::Future<std::unique_ptr<std::string>> future_getDataById(
-      int64_t id
-    );
-    folly::Future<folly::Unit> future_putDataById(
-      int64_t id,
-      std::unique_ptr<std::string> data
-    );
-    folly::Future<folly::Unit> future_lobDataById(
-      int64_t id,
-      std::unique_ptr<std::string> data
-    );
-}
+      folly::Future<folly::Unit> future_ping();
+      folly::Future<std::unique_ptr<std::string>> future_getRandomData();
+      folly::Future<bool> future_hasDataById(
+        int64_t id
+      );
+      folly::Future<std::unique_ptr<std::string>> future_getDataById(
+        int64_t id
+      );
+      folly::Future<folly::Unit> future_putDataById(
+        int64_t id,
+        std::unique_ptr<std::string> data
+      );
+      folly::Future<folly::Unit> future_lobDataById(
+        int64_t id,
+        std::unique_ptr<std::string> data
+      );
+};
 
 std::shared_ptr<ServerInterface> MyServiceInterface(PyObject *if_object);
 
@@ -40,24 +38,22 @@ class MyServiceFastWrapper : public MyServiceFastSvAsyncIf {
   public:
     explicit MyServiceFastWrapper(PyObject *if_object);
     virtual ~MyServiceFastWrapper();
-    folly::Future<folly::Unit> future_ping(
-    );
-    folly::Future<std::unique_ptr<std::string>> future_getRandomData(
-    );
-    folly::Future<bool> future_hasDataById(
-      int64_t id
-    );
-    folly::Future<std::unique_ptr<std::string>> future_getDataById(
-      int64_t id
-    );
-    folly::Future<folly::Unit> future_putDataById(
-      int64_t id,
-      std::unique_ptr<std::string> data
-    );
-    folly::Future<folly::Unit> future_lobDataById(
-      int64_t id,
-      std::unique_ptr<std::string> data
-    );
-}
+      folly::Future<folly::Unit> future_ping();
+      folly::Future<std::unique_ptr<std::string>> future_getRandomData();
+      folly::Future<bool> future_hasDataById(
+        int64_t id
+      );
+      folly::Future<std::unique_ptr<std::string>> future_getDataById(
+        int64_t id
+      );
+      folly::Future<folly::Unit> future_putDataById(
+        int64_t id,
+        std::unique_ptr<std::string> data
+      );
+      folly::Future<folly::Unit> future_lobDataById(
+        int64_t id,
+        std::unique_ptr<std::string> data
+      );
+};
 
 std::shared_ptr<ServerInterface> MyServiceFastInterface(PyObject *if_object);
