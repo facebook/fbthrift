624f8cbeef9becad243aa397054e6e76863b31e0
Eddie Elizondo <eelizondo@fb.com>
2016-11-23 06:51:19 -0800

    t_enum_value - Documented and reorganized
    Summary:
    * Fixed License
    * Added documentation
    
    Reviewed By: yfeldblum
    
    Differential Revision: D4213679
    
    fbshipit-source-id: c2c2b0e371b9a780f4c75c092b1478b798b99f25

--- thrift/compiler/parse/t_enum_value.h
+++ thrift/compiler/parse/t_enum_value.h
@@ -1,29 +1,28 @@
 /*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements. See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership. The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License. You may obtain a copy of the License at
+ * Copyright 2016 Facebook, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
  *   http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied. See the License for the
- * specific language governing permissions and limitations
- * under the License.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
-#ifndef T_ENUM_VALUE_H
-#define T_ENUM_VALUE_H
+#pragma once
 
 #include <string>
+
 #include <thrift/compiler/parse/t_doc.h>
 
 /**
+ * class t_enum_value
+ *
  * A constant. These are used inside of enum definitions. Constants are just
  * symbol identifiers that may or may not have an explicit value associated
  * with them.
@@ -31,29 +30,46 @@
  */
 class t_enum_value : public t_doc {
  public:
+
+  /**
+   * Constructor for t_enum_value
+   *
+   * @param name  - The symbolic name of the enum constant
+   */
+  explicit t_enum_value(std::string name) : name_(std::move(name)) {}
+
+  /**
+   * Constructor for t_enum_value
+   *
+   * @param name  - The symbolic name of the enum constant
+   * @param value - The value of the enum constant. Defaults to 0
+   */
   t_enum_value(std::string name, int32_t value) :
-    name_(name),
-    value_(value) {}
+    name_(std::move(name)),
+    value_(value),
+    has_value_(true) {}
 
   ~t_enum_value() {}
 
-  const std::string& get_name() const {
-    return name_;
+  /**
+   * t_enum_value setters
+   */
+  void set_value(int32_t value) {
+    value_ = value;
+    has_value_ = true;
   }
 
-  // Open source thrift supports enums with auto-incrementing values. This is
-  // a stub, replace it if we ever implement this feature in facebook.
-  bool has_value() {
-    return true;
-  }
+  /**
+   * t_enum_value getters
+   */
+  const std::string& get_name() const { return name_; }
 
-  int32_t get_value() const {
-    return value_;
-  }
+  int32_t get_value() const { return value_; }
+
+  bool has_value() { return has_value_; }
 
  private:
   std::string name_;
-  int32_t value_;
+  int32_t value_{0};
+  bool has_value_{false};
 };
-
-#endif
