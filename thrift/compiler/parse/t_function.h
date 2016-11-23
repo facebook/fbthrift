6fbc60ddff0906d28fbe407fa8138178bd6b97d8
Eddie Elizondo <eelizondo@fb.com>
2016-11-22 18:59:20 -0800

    t_function - Documented and reorganized
    Summary:
    * Fixed License
    * Added documentation
    * Added pragma once
    
    Reviewed By: juchem
    
    Differential Revision: D4220688
    
    fbshipit-source-id: 205eeaff7b3330a800bbb56909e0cc4464ba60d1

--- thrift/compiler/parse/t_function.h
+++ thrift/compiler/parse/t_function.h
@@ -1,33 +1,34 @@
 /*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements. See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership. The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License. You may obtain a copy of the License at
+ * Copyright 2016 Facebook, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
  *   http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied. See the License for the
- * specific language governing permissions and limitations
- * under the License.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
-#ifndef T_FUNCTION_H
-#define T_FUNCTION_H
+#pragma once
 
 #include <string>
+
 #include <thrift/compiler/parse/t_type.h>
 #include <thrift/compiler/parse/t_struct.h>
 #include <thrift/compiler/parse/t_doc.h>
 
+// Internal variable maintained by `lex` representing
+// the current line number of the input.
 extern int yylineno;
 
 /**
+ * class t_function
+ *
  * Representation of a function. Key parts are return type, function name,
  * optional modifiers, and an argument list, which is implemented as a thrift
  * struct.
@@ -35,16 +36,27 @@ extern int yylineno;
  */
 class t_function : public t_doc {
  public:
-  t_function(t_type* returntype,
-             std::string name,
-             t_struct* arglist,
-             t_type* annotations=nullptr,
-             bool oneway=false) :
-    returntype_(returntype),
-    name_(name),
-    arglist_(arglist),
-    annotations_(annotations),
-    oneway_(oneway) {
+
+  /**
+   * Constructor for t_function
+   *
+   * @param returntype  - The type of the value that will be returned
+   * @param name        - The symbolic name of the function
+   * @param arglist     - The parameters that are passed to the functions
+   * @param annotations - Optional args that add more functionality
+   * @param oneway      - Determines if it is a one way function
+   */
+  t_function(
+      t_type* returntype,
+      std::string name,
+      t_struct* arglist,
+      t_type* annotations = nullptr,
+      bool oneway = false) :
+        returntype_(returntype),
+        name_(name),
+        arglist_(arglist),
+        annotations_(annotations),
+        oneway_(oneway) {
     xceptions_ = new t_struct(nullptr);
 
     if (oneway_) {
@@ -54,54 +66,58 @@ class t_function : public t_doc {
     }
   }
 
-  t_function(t_type* returntype,
-             std::string name,
-             t_struct* arglist,
-             t_struct* xceptions,
-             t_type* annotations=nullptr,
-             bool oneway=false) :
-    returntype_(returntype),
-    name_(name),
-    arglist_(arglist),
-    xceptions_(xceptions),
-    annotations_(annotations),
-    oneway_(oneway) {
+  /**
+   * Constructor for t_function
+   *
+   * @param returntype  - The type of the value that will be returned
+   * @param name        - The symbolic name of the function
+   * @param arglist     - The parameters that are passed to the functions
+   * @param xceptions   - Declare the exceptions that function might throw
+   * @param annotations - Optional args that add more functionality
+   * @param oneway      - Determines if it is a one way function
+   */
+  t_function(
+      t_type* returntype,
+      std::string name,
+      t_struct* arglist,
+      t_struct* xceptions,
+      t_type* annotations = nullptr,
+      bool oneway = false) :
+        returntype_(returntype),
+        name_(name),
+        arglist_(arglist),
+        xceptions_(xceptions),
+        annotations_(annotations),
+        oneway_(oneway) {
     if (oneway_) {
       if (!xceptions_->get_members().empty()) {
         throw std::string("Oneway methods can't throw exceptions.");
       }
 
       if (returntype_ == nullptr || !returntype_->is_void()) {
-          throw std::string("Oneway methods must have void return type.");
+        throw std::string("Oneway methods must have void return type.");
       }
     }
   }
 
   ~t_function() {}
 
-  t_type* get_returntype() const {
-    return returntype_;
-  }
+  /**
+   * t_function getters
+   */
+  t_type* get_returntype() const { return returntype_; }
 
-  const std::string& get_name() const {
-    return name_;
-  }
+  const std::string& get_name() const { return name_; }
 
-  t_struct* get_arglist() const {
-    return arglist_;
-  }
+  t_struct* get_arglist() const { return arglist_; }
 
-  t_struct* get_xceptions() const {
-    return xceptions_;
-  }
+  t_struct* get_xceptions() const { return xceptions_; }
 
-  t_type* get_annotations() const {
-      return annotations_;
-  }
+  t_type* get_annotations() const { return annotations_; }
 
-  bool is_oneway() const {
-    return oneway_;
-  }
+  bool is_oneway() const { return oneway_; }
+
+  int get_lineno() const { return lineno_; }
 
  private:
   friend class t_service;
@@ -114,5 +130,3 @@ class t_function : public t_doc {
   bool oneway_;
   int lineno_{yylineno};
 };
-
-#endif
