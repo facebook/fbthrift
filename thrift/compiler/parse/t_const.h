3288e1adcfa7da9b0002dce94cda2cf5c7fbe553
Eddie Elizondo <eelizondo@fb.com>
2016-11-28 12:45:48 -0800

    t_const - Documented and reorganized
    Summary:
    * Fixed License
    * Added documentation
    
    Reviewed By: yfeldblum
    
    Differential Revision: D4213816
    
    fbshipit-source-id: 44a1cb36ad2af44a91155abccf866da0e0780ab3

--- thrift/compiler/parse/t_const.h
+++ thrift/compiler/parse/t_const.h
@@ -1,30 +1,29 @@
 /*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements. See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership. The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License. You may obtain a copy of the License at
+ * Copyright 2016 Facebook, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
  *   http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied. See the License for the
- * specific language governing permissions and limitations
- * under the License.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
-#ifndef T_CONST_H
-#define T_CONST_H
+#pragma once
 
 #include <thrift/compiler/parse/t_type.h>
 #include <thrift/compiler/parse/t_const_value.h>
 
 class t_program;
+
 /**
+ *  class t_const
+ *
  * A const is a constant value defined across languages that has a type and
  * a value. The trick here is that the declared type might not match the type
  * of the value object, since that is not determined until after parsing the
@@ -33,32 +32,36 @@ class t_program;
  */
 class t_const : public t_doc {
  public:
+
+  /**
+   * Constructor for t_const
+   *
+   * @param program - An entire thrift program
+   * @param type    - A thrift type
+   * @param name    - The name of the constant variable
+   * @param value   - The constant value
+   */
   t_const(
       t_program* program,
       t_type* type,
       std::string name,
-      t_const_value* value)
-      : program_(program), type_(type), name_(name), value_(value) {
+      t_const_value* value) :
+        program_(program), type_(type), name_(name), value_(value) {
     if (value) {
       value->set_owner(this);
     }
   }
 
-  t_type* get_type() const {
-    return type_;
-  }
+  /**
+   * t_const getters
+   */
+  t_program* get_program() const { return program_; }
 
-  std::string get_name() const {
-    return name_;
-  }
+  t_type* get_type() const { return type_; }
 
-  t_const_value* get_value() const {
-    return value_;
-  }
+  std::string get_name() const { return name_; }
 
-  t_program* get_program() const {
-    return program_;
-  }
+  t_const_value* get_value() const { return value_; }
 
  private:
   t_program* program_;
@@ -66,5 +69,3 @@ class t_const : public t_doc {
   std::string name_;
   t_const_value* value_;
 };
-
-#endif
