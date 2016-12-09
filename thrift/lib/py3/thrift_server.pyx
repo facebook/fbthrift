f595c6f883233f1bbb4510678d1cfd798fb3c59a
Dusty Phillips <dgp@fb.com>
2016-12-09 10:59:19 -0800

    Python indentation style in thrift-server
    Summary:
    All I really wanted to do was add a new stop method that proxies through to the
    c++ server's stop method. However, I ended up rearranging the code to use
    4-level indentation, as is customary in Python.
    
    Reviewed By: yfeldblum
    
    Differential Revision: D4301610
    
    fbshipit-source-id: 765587a064b3855c0049c071ff12185ef9c5be2b

--- thrift/lib/py3/thrift_server.pyx
+++ thrift/lib/py3/thrift_server.pyx
@@ -6,26 +6,30 @@ import asyncio
 
 
 cdef class ThriftServer:
-  cdef unique_ptr[cThriftServer] server
-  cdef ServiceInterface handler
-  cdef object loop
+    cdef unique_ptr[cThriftServer] server
+    cdef ServiceInterface handler
+    cdef object loop
 
-  def __cinit__(self):
-    self.server = make_unique[cThriftServer]()
+    def __cinit__(self):
+        self.server = make_unique[cThriftServer]()
 
 
-  def __init__(self, ServiceInterface handler, port, loop=None):
-    self.loop = loop or asyncio.get_event_loop()
-    self.handler = handler
-    self.server.get().setInterface(handler.interface_wrapper)
-    self.server.get().setPort(port)
+    def __init__(self, ServiceInterface handler, port, loop=None):
+        self.loop = loop or asyncio.get_event_loop()
+        self.handler = handler
+        self.server.get().setInterface(handler.interface_wrapper)
+        self.server.get().setPort(port)
 
-  async def serve(self):
-    def _serve():
-      with nogil:
-        self.server.get().serve()
-    try:
-        await self.loop.run_in_executor(None, _serve)
-    except Exception:
+    async def serve(self):
+        def _serve():
+            with nogil:
+                self.server.get().serve()
+        try:
+            await self.loop.run_in_executor(None, _serve)
+        except Exception:
+            print("Exception In Server")
+            self.server.get().stop()
+            raise
+
+    def stop(self):
         self.server.get().stop()
-        raise
