9d052a9cae7679f4ada80b49ff0344813c72c098
Sergey Makarenko <smakarenko@fb.com>
2017-01-30 11:16:50 -0800

    Fix oss build
    Summary:
    Making Thrift depend on current versions of Wangle and Folly could help it to
    be in sync with its dependencies.
    
    Reviewed By: eduardo-elizondo
    
    Differential Revision: D4482479
    
    fbshipit-source-id: 4ad21e687ed2768aade66549aa7b8cf5c603555b

--- thrift/build/deps_ubuntu_14.04.sh
+++ thrift/build/deps_ubuntu_14.04.sh
@@ -5,10 +5,9 @@
 sudo apt-get install -y libdouble-conversion-dev libssl-dev make zip git \
   autoconf libtool g++ libboost-all-dev libevent-dev flex bison \
   libgoogle-glog-dev scons libkrb5-dev libsnappy-dev libsasl2-dev \
-  libnuma-dev git cmake
+  libnuma-dev git cmake pkg-config
 
 install_folly ubuntu_14.04  # needs git
 install_mstch ubuntu_14.04  # needs git, cmake
 install_zstd ubuntu_14.04   # needs git
 install_wangle ubuntu_14.04 # needs git, cmake
-
