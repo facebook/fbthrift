ec6478980f64294bc062c1d4eb0282627f58cd5c
Eddie Elizondo <eelizondo@fb.com>
2017-04-10 15:43:43 -0700

    Add protocol_methods to resolve container de/serialization
    Summary:
    This code moves code from thrift/lib/fatal/serializer.h to thrift/lib/serializer.h to be able to reuse without reflection.
    
    This also adds new code (marked inline) to fix corner cases:
    * enum classes
    * fallback to Cpp2Ops for type_class::structure
    
    This is a modification to the fatal implementation of serializer and does not use reflection capabilities so it's unable to read Structs. Instead, this is used to read any type of container and primitive types
    
    Reviewed By: yfeldblum
    
    Differential Revision: D4826672
    
    fbshipit-source-id: f15746e9351bf0e643f320ed993afa9bf4707ea8

---
+++ thrift/lib/cpp2/GeneratedSerializationCodeHelper.h
@@ -0,0 +1,528 @@
+/*
+ * Copyright 2016-present Facebook, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#pragma once
+
+#include <bitset>
+#include <iostream>
+#include <iterator>
+#include <memory>
+#include <type_traits>
+#include <vector>
+
+#include <thrift/lib/cpp2/TypeClass.h>
+
+/**
+ * Specializations of `protocol_methods` encapsulate a collection of
+ * read/write/size/sizeZC methods that can be performed on Thrift
+ * objects and primitives. TypeClass (see apache::thrift::type_class)
+ * refers to the general type of data that Type is, and is passed around for
+ * two reasons:
+ *  - to provide support for generic containers which have a common interface
+ *    for building collections (e.g. a `std::vector<int>` and `std::deque<int>`,
+ *    which can back a Thrift list, and thus have
+ *    `type_class::list<type_class::integral>`, or an
+ *    `std::map<std::string, MyStruct>` would have
+ *    `type_class::map<type_class::string, type_class::structure>``).
+ *  - to differentiate between Thrift types that are represented with the
+ *    same C++ type, e.g. both Thrift binaries and strings are represented
+ *    with an `std::string`, TypeClass is used to decide which protocol
+ *    method to call.
+ *
+ * Example:
+ *
+ * // MyModule.thrift:
+ * struct MyStruct {
+ *   1: list<set<string>> fieldA
+ * }
+ *
+ * // C++
+ *
+ * using methods = protocol_methods<
+ *    type_class::list<type_class::set<type_class::string>>,
+ *    std::vector<std::set<std::string>>>
+ *
+ * MyStruct struct_instance;
+ * CompactProtocolReader reader;
+ * methods::read(struct_instance.fieldA, reader);
+ */
+
+namespace apache {
+namespace thrift {
+namespace detail {
+namespace pm {
+
+inline bool is_unknown_container_size(uint32_t const size) {
+  return size == std::numeric_limits<decltype(size)>::max();
+}
+
+// is_smart_pointer is a helper for determining if a type is a supported
+// pointer type for cpp2.ref fields, while discrimiminating against the
+// pointer corner case in Thrift (e.g., a unqiue_pointer<folly::IOBuf>)
+template <typename>
+struct is_smart_pointer : std::false_type {};
+template <typename D>
+struct is_smart_pointer<std::unique_ptr<folly::IOBuf, D>> : std::false_type {};
+
+// supported smart pointer types for cpp2.ref_type fields
+template <typename T, typename D>
+struct is_smart_pointer<std::unique_ptr<T, D>> : std::true_type {};
+template <typename T>
+struct is_smart_pointer<std::shared_ptr<T>> : std::true_type {};
+
+template <typename T>
+using enable_if_smart_pointer = std::enable_if_t<is_smart_pointer<T>::value>;
+
+template <typename T>
+using disable_if_smart_pointer = std::enable_if_t<!is_smart_pointer<T>::value>;
+
+/*
+ * Primitive Types Specialization
+ */
+template <typename TypeClass, typename Type, typename Enable = void>
+struct protocol_methods;
+
+#define REGISTER_RW_COMMON(Class, Type, Method, TTypeValue)            \
+  constexpr static protocol::TType ttype_value = protocol::TTypeValue; \
+  template <typename Protocol>                                         \
+  static std::size_t read(Protocol& protocol, Type& out) {             \
+    return protocol.read##Method(out);                                 \
+  }                                                                    \
+  template <typename Protocol>                                         \
+  static std::size_t write(Protocol& protocol, Type const& in) {       \
+    return protocol.write##Method(in);                                 \
+  }
+
+#define REGISTER_SS_COMMON(Class, Type, Method, TTypeValue)                \
+  template <bool, typename Protocol>                                       \
+  static std::size_t serialized_size(Protocol& protocol, Type const& in) { \
+    return protocol.serializedSize##Method(in);                            \
+  }
+
+// stamp out specializations for primitive types
+// TODO: Perhaps change ttype_value to a static constexpr member function, as
+// those might instantiate faster than a constexpr objects
+#define REGISTER_OVERLOAD(Class, Type, Method, TTypeValue) \
+  template <>                                              \
+  struct protocol_methods<type_class::Class, Type> {       \
+    REGISTER_RW_COMMON(Class, Type, Method, TTypeValue)    \
+    REGISTER_SS_COMMON(Class, Type, Method, TTypeValue)    \
+  }
+
+#define REGISTER_INTEGRAL(...) REGISTER_OVERLOAD(integral, __VA_ARGS__)
+REGISTER_INTEGRAL(std::int8_t, Byte, T_BYTE);
+REGISTER_INTEGRAL(std::int16_t, I16, T_I16);
+REGISTER_INTEGRAL(std::int32_t, I32, T_I32);
+REGISTER_INTEGRAL(std::int64_t, I64, T_I64);
+#undef REGISTER_INTEGRAL
+
+// std::vector<bool> isn't actually a container, so
+// define a special overload which takes its specialized
+// proxy type
+template <>
+struct protocol_methods<type_class::integral, bool> {
+  REGISTER_RW_COMMON(integral, bool, Bool, T_BOOL)
+  REGISTER_SS_COMMON(integral, bool, Bool, T_BOOL)
+
+  template <typename Protocol>
+  static std::size_t read(
+      Protocol& protocol,
+      std::vector<bool>::reference out) {
+    bool tmp;
+    std::size_t xfer = read(protocol, tmp);
+    out = tmp;
+    return xfer;
+  }
+};
+
+#define REGISTER_FP(...) REGISTER_OVERLOAD(floating_point, __VA_ARGS__)
+REGISTER_FP(double, Double, T_DOUBLE);
+REGISTER_FP(float, Float, T_FLOAT);
+#undef REGISTER_FP
+
+// TODO: might not need to specialize on std::string, but rather
+// just the string typeclass
+REGISTER_OVERLOAD(string, std::string, String, T_STRING);
+REGISTER_OVERLOAD(string, folly::fbstring, String, T_STRING);
+
+#undef REGISTER_OVERLOAD
+
+#define REGISTER_ZC(Class, Type, Method, TTypeValue)             \
+  template <>                                                    \
+  struct protocol_methods<type_class::Class, Type> {             \
+    REGISTER_RW_COMMON(Class, Type, Method, TTypeValue)          \
+    template <bool ZeroCopy, typename Protocol>                  \
+    static typename std::enable_if<ZeroCopy, std::size_t>::type  \
+    serialized_size(Protocol& protocol, Type const& in) {        \
+      return protocol.serializedSizeZC##Method(in);              \
+    }                                                            \
+    template <bool ZeroCopy, typename Protocol>                  \
+    static typename std::enable_if<!ZeroCopy, std::size_t>::type \
+    serialized_size(Protocol& protocol, Type const& in) {        \
+      return protocol.serializedSize##Method(in);                \
+    }                                                            \
+  };
+
+#define REGISTER_BINARY(...) REGISTER_ZC(binary, __VA_ARGS__)
+REGISTER_BINARY(std::string, Binary, T_STRING);
+REGISTER_BINARY(folly::IOBuf, Binary, T_STRING);
+REGISTER_BINARY(std::unique_ptr<folly::IOBuf>, Binary, T_STRING);
+REGISTER_BINARY(folly::fbstring, Binary, T_STRING);
+#undef REGISTER_BINARY
+#undef REGISTER_ZC // this naming sure isn't confusing, no sir
+#undef REGISTER_SS_COMMON
+#undef REGISTER_RW_COMMON
+
+/*
+ * Enum Specialization
+ */
+template <typename Type>
+struct protocol_methods<type_class::enumeration, Type> {
+  constexpr static protocol::TType ttype_value = protocol::T_I32;
+
+  // Enums have an underlying type of int, unless otherwise specified.
+  // Thrift permits customization of the C++ underlying types of enums.
+  // So we cannot simply assume that the underlying type is always int.
+  // Thus, we should always get the underlying type and convert to signed.
+  using enum_type = std::make_signed_t<std::underlying_type_t<Type>>;
+  using int_methods = protocol_methods<type_class::integral, enum_type>;
+
+  template <typename Protocol>
+  static std::size_t read(Protocol& protocol, Type& out) {
+    enum_type tmp;
+    std::size_t xfer = int_methods::read(protocol, tmp);
+    out = static_cast<Type>(tmp);
+    return xfer;
+  }
+
+  template <typename Protocol>
+  static std::size_t write(Protocol& protocol, Type const& in) {
+    enum_type tmp = static_cast<enum_type>(in);
+    return int_methods::template write<Protocol>(protocol, tmp);
+  }
+
+  template <bool ZeroCopy, typename Protocol>
+  static std::size_t serialized_size(Protocol& protocol, Type const& in) {
+    enum_type tmp = static_cast<enum_type>(in);
+    return int_methods::template serialized_size<ZeroCopy>(protocol, tmp);
+  }
+};
+
+/*
+ * List Specialization
+ */
+template <typename ElemClass, typename Type>
+struct protocol_methods<type_class::list<ElemClass>, Type> {
+  constexpr static protocol::TType ttype_value = protocol::T_LIST;
+
+  using elem_type = typename Type::value_type;
+
+  static_assert(
+      !std::is_same<ElemClass, type_class::unknown>(),
+      "Unable to serialize unknown list element");
+
+  using elem_methods = protocol_methods<ElemClass, elem_type>;
+
+  template <typename Protocol>
+  static std::size_t read(Protocol& protocol, Type& out) {
+    std::size_t xfer = 0;
+    std::uint32_t list_size = -1;
+    TType reported_type = protocol::T_STOP;
+
+    xfer += protocol.readListBegin(reported_type, list_size);
+    if (detail::is_unknown_container_size(list_size)) {
+      // list size unknown, SimpleJSON protocol won't know type, either
+      // so let's just hope that it spits out something that makes sense
+      // (if it did set reported_type to something known)
+      if (reported_type != protocol::T_STOP) {
+        assert(reported_type == elem_methods::ttype_value);
+      }
+
+      for (decltype(list_size) i = 0; protocol.peekList(); ++i) {
+        // TODO: this should grow better (e.g., 1.5x each time)
+        // For now, we just bump the container size by 1 each time
+        // because that's what the currently generated Thrift code does.
+        out.resize(i + 1);
+        xfer += elem_methods::read(protocol, out[i]);
+      }
+    } else {
+      assert(reported_type == elem_methods::ttype_value);
+      out.resize(list_size);
+      for (decltype(list_size) i = 0; i < list_size; ++i) {
+        xfer += elem_methods::read(protocol, out[i]);
+      }
+    }
+
+    xfer += protocol.readListEnd();
+    return xfer;
+  }
+
+  template <typename Protocol>
+  static std::size_t write(Protocol& protocol, Type const& out) {
+    std::size_t xfer = 0;
+    xfer += protocol.writeListBegin(elem_methods::ttype_value, out.size());
+
+    for (auto const& elem : out) {
+      xfer += elem_methods::write(protocol, elem);
+    }
+
+    xfer += protocol.writeListEnd();
+    return xfer;
+  }
+
+  template <bool ZeroCopy, typename Protocol>
+  static std::size_t serialized_size(Protocol& protocol, Type const& out) {
+    std::size_t xfer = 0;
+    xfer +=
+        protocol.serializedSizeListBegin(elem_methods::ttype_value, out.size());
+
+    for (auto const& elem : out) {
+      xfer += elem_methods::template serialized_size<ZeroCopy>(protocol, elem);
+    }
+
+    xfer += protocol.serializedSizeListEnd();
+    return xfer;
+  }
+};
+
+/*
+ * Set Specialization
+ */
+template <typename ElemClass, typename Type>
+struct protocol_methods<type_class::set<ElemClass>, Type> {
+  constexpr static protocol::TType ttype_value = protocol::T_SET;
+
+  // TODO: fair amount of shared code bewteen this and specialization for
+  // type_class::list
+  using elem_type = typename Type::value_type;
+
+  static_assert(
+      !std::is_same<ElemClass, type_class::unknown>(),
+      "Unable to serialize unknown type");
+  using elem_methods = protocol_methods<ElemClass, elem_type>;
+
+ private:
+  template <typename Protocol>
+  static std::size_t consume_elem(Protocol& protocol, Type& out) {
+    elem_type tmp;
+    std::size_t xfer = elem_methods::read(protocol, tmp);
+    out.insert(std::move(tmp));
+    return xfer;
+  }
+
+ public:
+  template <typename Protocol>
+  static std::size_t read(Protocol& protocol, Type& out) {
+    std::size_t xfer = 0;
+    // TODO: readSetBegin takes (TType, std::uint32_t)
+    // instead of a (TType, std::size_t)
+    std::uint32_t set_size = -1;
+    TType reported_type = protocol::T_STOP;
+
+    xfer += protocol.readSetBegin(reported_type, set_size);
+    if (detail::is_unknown_container_size(set_size)) {
+      while (protocol.peekSet()) {
+        xfer += consume_elem(protocol, out);
+      }
+    } else {
+      assert(reported_type == elem_methods::ttype_value);
+      for (decltype(set_size) i = 0; i < set_size; ++i) {
+        xfer += consume_elem(protocol, out);
+      }
+    }
+
+    xfer += protocol.readSetEnd();
+    return xfer;
+  }
+
+  template <typename Protocol>
+  static std::size_t write(Protocol& protocol, Type const& in) {
+    std::size_t xfer = 0;
+    xfer += protocol.writeSetBegin(elem_methods::ttype_value, in.size());
+    for (auto const& elem : in) {
+      xfer += elem_methods::write(protocol, elem);
+    }
+    xfer += protocol.writeSetEnd();
+    return xfer;
+  }
+
+  template <bool ZeroCopy, typename Protocol>
+  static std::size_t serialized_size(Protocol& protocol, Type const& out) {
+    std::size_t xfer = 0;
+    xfer +=
+        protocol.serializedSizeSetBegin(elem_methods::ttype_value, out.size());
+
+    for (auto const& elem : out) {
+      xfer += elem_methods::template serialized_size<ZeroCopy>(protocol, elem);
+    }
+
+    xfer += protocol.serializedSizeSetEnd();
+    return xfer;
+  }
+};
+
+/*
+ * Map Specialization
+ */
+template <typename KeyClass, typename MappedClass, typename Type>
+struct protocol_methods<type_class::map<KeyClass, MappedClass>, Type> {
+  constexpr static protocol::TType ttype_value = protocol::T_MAP;
+
+  using key_type = typename Type::key_type;
+  using mapped_type = typename Type::mapped_type;
+
+  static_assert(
+      !std::is_same<KeyClass, type_class::unknown>(),
+      "Unable to serialize unknown key type in map");
+  static_assert(
+      !std::is_same<MappedClass, type_class::unknown>(),
+      "Unable to serialize unknown mapped type in map");
+
+  using key_methods = protocol_methods<KeyClass, key_type>;
+  using mapped_methods = protocol_methods<MappedClass, mapped_type>;
+
+ private:
+  template <typename Protocol>
+  static std::size_t consume_elem(Protocol& protocol, Type& out) {
+    std::size_t xfer = 0;
+    key_type key_tmp;
+    xfer += key_methods::read(protocol, key_tmp);
+    xfer += mapped_methods::read(protocol, out[std::move(key_tmp)]);
+    return xfer;
+  }
+
+ public:
+  template <typename Protocol>
+  static std::size_t read(Protocol& protocol, Type& out) {
+    std::size_t xfer = 0;
+    // TODO: see above re: readMapBegin taking a uint32_t size param
+    std::uint32_t map_size = -1;
+    TType rpt_key_type = protocol::T_STOP, rpt_mapped_type = protocol::T_STOP;
+
+    xfer += protocol.readMapBegin(rpt_key_type, rpt_mapped_type, map_size);
+
+    if (detail::is_unknown_container_size(map_size)) {
+      while (protocol.peekMap()) {
+        xfer += consume_elem(protocol, out);
+      }
+    } else {
+      // CompactProtocol does not transmit key/mapped types if
+      // the map is empty
+      if (map_size > 0) {
+        assert(key_methods::ttype_value == rpt_key_type);
+        assert(mapped_methods::ttype_value == rpt_mapped_type);
+      }
+      for (decltype(map_size) i = 0; i < map_size; ++i) {
+        xfer += consume_elem(protocol, out);
+      }
+    }
+
+    xfer += protocol.readMapEnd();
+    return xfer;
+  }
+
+  template <typename Protocol>
+  static std::size_t write(Protocol& protocol, Type const& out) {
+    std::size_t xfer = 0;
+    xfer += protocol.writeMapBegin(
+        key_methods::ttype_value, mapped_methods::ttype_value, out.size());
+
+    for (auto const& elem_pair : out) {
+      xfer += key_methods::write(protocol, elem_pair.first);
+      xfer += mapped_methods::write(protocol, elem_pair.second);
+    }
+
+    xfer += protocol.writeMapEnd();
+    return xfer;
+  }
+
+  template <bool ZeroCopy, typename Protocol>
+  static std::size_t serialized_size(Protocol& protocol, Type const& out) {
+    std::size_t xfer = 0;
+    xfer += protocol.serializedSizeMapBegin(
+        key_methods::ttype_value, mapped_methods::ttype_value, out.size());
+
+    for (auto const& elem_pair : out) {
+      xfer += key_methods::template serialized_size<ZeroCopy>(
+          protocol, elem_pair.first);
+      xfer += mapped_methods::template serialized_size<ZeroCopy>(
+          protocol, elem_pair.second);
+    }
+
+    xfer += protocol.serializedSizeMapEnd();
+    return xfer;
+  }
+};
+
+/*
+ * Struct Specialization
+ * Forwards to Cpp2Ops wrapper around member read/write/etc.
+ */
+template <typename Type>
+struct protocol_methods<type_class::structure, Type> {
+  constexpr static protocol::TType ttype_value = protocol::T_STRUCT;
+
+  template <typename Protocol>
+  static std::size_t read(Protocol& protocol, Type& out) {
+    return Cpp2Ops<Type>::read(&protocol, &out);
+  }
+  template <typename Protocol>
+  static std::size_t write(Protocol& protocol, Type const& in) {
+    return Cpp2Ops<Type>::write(&protocol, &in);
+  }
+  template <bool ZeroCopy, typename Protocol>
+  static std::size_t serialized_size(Protocol& protocol, Type const& in) {
+    if (ZeroCopy) {
+      return Cpp2Ops<Type>::serializedSizeZC(&protocol, &in);
+    } else {
+      return Cpp2Ops<Type>::serializedSize(&protocol, &in);
+    }
+  }
+};
+
+/*
+ * Smart Pointer Specialization
+ * This resolves all calls that uses pointers and forwards them to
+ * the appropriate method
+ */
+template <typename TypeClass, typename PtrType>
+struct protocol_methods<
+    TypeClass,
+    PtrType,
+    typename detail::enable_if_smart_pointer<PtrType>> {
+  using value_type = typename PtrType::element_type;
+  using type_methods = protocol_methods<TypeClass, value_type>;
+
+  constexpr static protocol::TType ttype_value = type_methods::ttype_value;
+
+  template <typename Protocol>
+  static std::size_t read(Protocol& protocol, PtrType& out) {
+    return type_methods::read(protocol, *out);
+  }
+
+  template <typename Protocol>
+  static std::size_t write(Protocol& protocol, PtrType const& in) {
+    return type_methods::write(protocol, *in);
+  }
+
+  template <bool ZeroCopy, typename Protocol>
+  static std::size_t serialized_size(Protocol& protocol, PtrType const& in) {
+    return type_methods::template serialized_size<ZeroCopy>(protocol, *in);
+  }
+};
+}
+}
+}
+} // namespace apache::thrift::detail::pm
