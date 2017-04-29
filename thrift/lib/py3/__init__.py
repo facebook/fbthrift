b4087f83bab712d41275332747b15634b71edcad
Jason Fried <fried@fb.com>
2017-04-28 17:41:21 -0700

    thrift-py3 used a non-compiled __init__.py for lib/py3/
    Summary:
    compiled __init__.so doesn't seem to work with zipimporter it loads
    the empty __init__.py needed to signal to python that the directory is a package
    
    Reviewed By: cooperlees
    
    Differential Revision: D4973624
    
    fbshipit-source-id: 6e990b5afbd56c04d13568bb9fce3d22696b4047

---
+++ thrift/lib/py3/__init__.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+__all__ = [
+    'get_client', 'Client', 'ThriftServer', 'Struct', 'BadEnum', 'Error',
+    'ApplicationError', 'TransportError'
+]
+
+try:
+    from thrift.py3.client import get_client, Client
+except ImportError:
+    __all__.remove('Client')
+    __all__.remove('get_client')
+
+try:
+    from thrift.py3.server import ThriftServer
+except ImportError:
+    __all__.remove('ThriftServer')
+
+try:
+    from thrift.py3.types import Struct, BadEnum
+except ImportError:
+    __all__.remove('Struct')
+    __all__.remove('BadEnum')
+
+try:
+    from thrift.py3.exceptions import Error, ApplicationError, TransportError
+except ImportError:
+    __all__.remove('Error')
+    __all__.remove('ApplicationError')
+    __all__.remove('TransportError')
