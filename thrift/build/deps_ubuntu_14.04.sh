f2c86bd896860d10308b732e9a62902ae6f6a585
Sergey Makarenko <smakarenko@fb.com>
2017-05-01 12:42:24 -0700

    Use g++-5 to build Thrift project
    Summary:
    Since at least one of the dependencies (folly) that are built with Thrift
    requires gcc version > 4.8 open source build was broken.
    Using g++-5 allows to build all Thrift's dependencies and project itself.
    
    Reviewed By: eduardo-elizondo
    
    Differential Revision: D4979312
    
    fbshipit-source-id: 84874a04cadf04868b80cbc089357fc551be77e2

--- thrift/build/deps_ubuntu_14.04.sh
+++ thrift/build/deps_ubuntu_14.04.sh
@@ -2,8 +2,12 @@
 
 . "$(dirname "$0")/deps_common.sh"
 
+sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
+sudo apt-get update -qq
+sudo apt-get install -qq g++-5
+sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-5 90
 sudo apt-get install -y libdouble-conversion-dev libssl-dev make zip git \
-  autoconf libtool g++ libboost-all-dev libevent-dev flex bison \
+  autoconf libtool libboost-all-dev libevent-dev flex bison \
   libgoogle-glog-dev scons libkrb5-dev libsnappy-dev libsasl2-dev \
   libnuma-dev git cmake pkg-config
 
