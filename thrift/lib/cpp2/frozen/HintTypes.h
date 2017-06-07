b8b5f6b62c06fe1d55151ed1fac654dd9f7a62cd
Tom Jackson <tjackson@fb.com>
2017-06-07 13:40:08 -0700

    Adding support for unpacked ints
    Summary:
    For faster random access. Previously landed as D4960653. Reverted with D5080476.
    
    Depends on D5182492
    
    Reviewed By: yfeldblum
    
    Differential Revision: D5196001
    
    fbshipit-source-id: 7bf4acb80af363d88b098caf4645b1e4854ac87a

---
+++ thrift/lib/cpp2/frozen/HintTypes.h
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2014-present Facebook, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#pragma once
+
+#include <utility>
+#include <vector>
+
+#include <thrift/lib/cpp2/frozen/Traits.h>
+
+namespace apache {
+namespace thrift {
+namespace frozen {
+
+/*
+ * For representing sequences of unpacked integral types.
+ *
+ * Use this in Thrift IDL like:
+ *
+ *   cpp_include "thrift/lib/cpp2/frozen/HintTypes.h"
+ *
+ *   struct MyStruct {
+ *     7: list<i32>
+ *        (cpp.template = "apache::thrift::frozen::VectorUnpacked")
+ *        ids,
+ *   }
+ */
+template <class T>
+class VectorUnpacked : public std::vector<T> {
+  static_assert(
+      std::is_arithmetic<T>::value || std::is_enum<T>::value,
+      "Unpacked storage is only available for simple item types");
+  using std::vector<T>::vector;
+};
+}
+}
+}
+THRIFT_DECLARE_TRAIT_TEMPLATE(IsString, apache::thrift::frozen::VectorUnpacked)
