bdd5881f26f751a10459afdb00a0a88d56608b8b
Xiaohang Yu <xiaohang@fb.com>
2017-07-21 11:50:29 -0700

    Remove use of hash_map in Frozen2ProtocolTest
    Summary: Since we're removing `hash_map`, let's also not use it in tests.
    
    Reviewed By: yfeldblum
    
    Differential Revision: D5463113
    
    fbshipit-source-id: 866f32bcd4ed1acf4d876fa8fa1bc14f055ec376

--- thrift/lib/cpp2/protocol/test/frozen2_protocol_test.thrift
+++ thrift/lib/cpp2/protocol/test/frozen2_protocol_test.thrift
@@ -1,7 +1,9 @@
 namespace cpp2 test.frozen2
 
+typedef map<i32, string> (cpp.template = 'std::unordered_map') HashMapI32String
+
 struct StructA {
   1: i32 i32Field,
   2: string strField,
-  5: list<hash_map<i32, string>> listField
+  5: list<HashMapI32String> listField
 }
