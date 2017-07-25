2ea5f3cc1a7db782837bb798f57710e93c327165
Eddie Elizondo <eelizondo@fb.com>
2017-07-25 14:35:30 -0700

    Protocol Test Fixes
    Summary: Small fixes
    
    Reviewed By: sriramsankar
    
    Differential Revision: D5492472
    
    fbshipit-source-id: 45eff047abe4ad118af39b766991f4040d4ece07

--- thrift/test/ProtocolTest.thrift
+++ thrift/test/ProtocolTest.thrift
@@ -1,15 +1,13 @@
-namespace cpp2 thrift.test
-namespace php thrift.test
-
 struct SimpleStruct {
   1: bool a_bool
   2: byte a_byte
   3: i16 a_i16
   4: i32 a_i32
   5: i64 a_i64
-  6: double a_double
-  7: string a_string
-  8: binary a_binary
+  6: float a_float
+  7: double a_double
+  8: string a_string
+  9: binary a_binary
 }
 
 struct ContainerStruct {
@@ -18,18 +16,20 @@ struct ContainerStruct {
   3: list<i16> i16_list
   4: list<i32> i32_list
   5: list<i64> i64_list
-  6: list<double> double_list
-  7: list<string> string_list
-  8: list<binary> binary_list
+  6: list<float> float_list
+  7: list<double> double_list
+  8: list<string> string_list
+  9: list<binary> binary_list
 
   10: set<bool> boolean_set
   11: set<byte> byte_set
   12: set<i16> i16_set
   13: set<i32> i32_set
   14: set<i64> i64_set
-  15: set<double> double_set
-  16: set<string> string_set
-  17: set<binary> binary_set
+  15: set<float> float_set
+  16: set<double> double_set
+  17: set<string> string_set
+  18: set<binary> binary_set
 
   20: map<byte, bool> byte_bool_map
   21: map<i32, i16> i32_i16_map
@@ -37,7 +37,7 @@ struct ContainerStruct {
   23: map<double, binary> double_binary_map
 
   30: list<map<byte, bool>> list_byte_bool_map
-  31: set<map<i32, i16>> i32_i16_map
+  31: set<map<i32, i16>> i32_i16_map_set
   32: map<string, map<double, binary>> string_double_binary_map_map
 }
 
