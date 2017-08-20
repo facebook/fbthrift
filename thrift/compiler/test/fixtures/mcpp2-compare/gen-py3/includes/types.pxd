18ef315d591c7097e3ec00772dc84a070099a1ba
Yedidya Feldblum <yfeldblum@fb.com>
2017-08-20 11:17:55 -0700

    Rebuild fixtures
    Summary: [Thrift] Rebuild fixtures. Was there a race?
    
    Reviewed By: Orvid
    
    Differential Revision: D5667990
    
    fbshipit-source-id: 1bd5e2a255c398293991c3ba198543437e6c4eea

--- thrift/compiler/test/fixtures/mcpp2-compare/gen-py3/includes/types.pxd
+++ thrift/compiler/test/fixtures/mcpp2-compare/gen-py3/includes/types.pxd
@@ -30,6 +30,8 @@ cdef cAnEnum AnEnum_to_cpp(value)
 cdef extern from "src/gen-cpp2/includes_types_custom_protocol.h" namespace "a::different::ns":
     # Forward Declaration
     cdef cppclass cAStruct "a::different::ns::AStruct"
+    # Forward Declaration
+    cdef cppclass cAStructB "a::different::ns::AStructB"
 
 cdef extern from "src/gen-cpp2/includes_types.h" namespace "a::different::ns":
     cdef cppclass cAStruct__isset "a::different::ns::AStruct::__isset":
@@ -42,14 +44,29 @@ cdef extern from "src/gen-cpp2/includes_types.h" namespace "a::different::ns":
         int32_t FieldA
         cAStruct__isset __isset
 
+    cdef cppclass cAStructB__isset "a::different::ns::AStructB::__isset":
+        bint FieldA
+
+    cdef cppclass cAStructB "a::different::ns::AStructB":
+        cAStructB() except +
+        cAStructB(const cAStructB&) except +
+        bint operator==(cAStructB&)
+        shared_ptr[const cAStruct] FieldA
+        cAStructB__isset __isset
+
+    cdef shared_ptr[cAStruct] aliasing_constructor_FieldA "std::shared_ptr<a::different::ns::AStruct>"(shared_ptr[cAStructB]&, cAStruct*)
 
 cdef extern from "<utility>" namespace "std" nogil:
     cdef shared_ptr[cAStruct] move(unique_ptr[cAStruct])
     cdef shared_ptr[cAStruct] move_shared "std::move"(shared_ptr[cAStruct])
     cdef unique_ptr[cAStruct] move_unique "std::move"(unique_ptr[cAStruct])
+    cdef shared_ptr[cAStructB] move(unique_ptr[cAStructB])
+    cdef shared_ptr[cAStructB] move_shared "std::move"(shared_ptr[cAStructB])
+    cdef unique_ptr[cAStructB] move_unique "std::move"(unique_ptr[cAStructB])
 
 cdef extern from "<memory>" namespace "std" nogil:
     cdef shared_ptr[const cAStruct] const_pointer_cast "std::const_pointer_cast<const a::different::ns::AStruct>"(shared_ptr[cAStruct])
+    cdef shared_ptr[const cAStructB] const_pointer_cast "std::const_pointer_cast<const a::different::ns::AStructB>"(shared_ptr[cAStructB])
 
 # Forward Definition of the cython struct
 cdef class AStruct(thrift.py3.types.Struct)
@@ -67,6 +84,24 @@ cdef class AStruct(thrift.py3.types.Struct):
 
     @staticmethod
     cdef create(shared_ptr[cAStruct])
+
+# Forward Definition of the cython struct
+cdef class AStructB(thrift.py3.types.Struct)
+
+cdef class AStructB(thrift.py3.types.Struct):
+    cdef object __hash
+    cdef object __weakref__
+    cdef shared_ptr[cAStructB] _cpp_obj
+    cdef AStruct __FieldA
+
+    @staticmethod
+    cdef unique_ptr[cAStructB] _make_instance(
+        cAStructB* base_instance,
+        object FieldA
+    ) except *
+
+    @staticmethod
+    cdef create(shared_ptr[cAStructB])
 
 
 
--- thrift/compiler/test/fixtures/mcpp2-compare/gen-py3/includes/types.pyx
+++ thrift/compiler/test/fixtures/mcpp2-compare/gen-py3/includes/types.pyx
@@ -169,5 +169,133 @@ cdef class AStruct(thrift.py3.types.Struct):
         return (deserialize, (AStruct, serialize(self)))
 
 
+cdef cAStructB _AStructB_defaults = cAStructB()
+
+cdef class AStructB(thrift.py3.types.Struct):
+
+    def __init__(
+        AStructB self,
+        FieldA=None
+    ):
+        self._cpp_obj = move(AStructB._make_instance(
+          NULL,
+          FieldA,
+        ))
+
+    def __call__(
+        AStructB self,
+        FieldA=NOTSET
+    ):
+        changes = any((
+            FieldA is not NOTSET,
+        ))
+
+        if not changes:
+            return self
+
+        inst = <AStructB>AStructB.__new__(AStructB)
+        inst._cpp_obj = move(AStructB._make_instance(
+          self._cpp_obj.get(),
+          FieldA,
+        ))
+        return inst
+
+    @staticmethod
+    cdef unique_ptr[cAStructB] _make_instance(
+        cAStructB* base_instance,
+        object FieldA
+    ) except *:
+        cdef unique_ptr[cAStructB] c_inst
+        if base_instance:
+            c_inst = make_unique[cAStructB](deref(base_instance))
+        else:
+            c_inst = make_unique[cAStructB]()
+
+        if base_instance:
+            # Convert None's to default value.
+            if FieldA is None:
+                raise ValueError("Reference-annotated fields may not be initialized to defaults")
+            elif FieldA is NOTSET:
+                FieldA = None
+
+        if FieldA is not None:
+            deref(c_inst).FieldA = const_pointer_cast((<AStruct?>FieldA)._cpp_obj)
+        # in C++ you don't have to call move(), but this doesn't translate
+        # into a C++ return statement, so you do here
+        return move_unique(c_inst)
+
+    def __iter__(self):
+        yield 'FieldA', self.FieldA
+
+    def __bool__(self):
+        return <bint>(deref(self._cpp_obj).FieldA)
+
+    @staticmethod
+    cdef create(shared_ptr[cAStructB] cpp_obj):
+        inst = <AStructB>AStructB.__new__(AStructB)
+        inst._cpp_obj = cpp_obj
+        return inst
+
+    @property
+    def FieldA(self):
+        if self.__FieldA is None:
+            if not deref(self._cpp_obj).FieldA:
+                return None
+            self.__FieldA = AStruct.create(aliasing_constructor_FieldA(self._cpp_obj, <cAStruct*>(deref(self._cpp_obj).FieldA.get())))
+        return self.__FieldA
+
+
+    def __hash__(AStructB self):
+        if not self.__hash:
+            self.__hash = hash((
+            self.FieldA,
+            ))
+        return self.__hash
+
+    def __repr__(AStructB self):
+        return f'AStructB(FieldA={repr(self.FieldA)})'
+    def __richcmp__(self, other, op):
+        cdef int cop = op
+        if cop not in (2, 3):
+            raise TypeError("unorderable types: {}, {}".format(self, other))
+        if not (
+                isinstance(self, AStructB) and
+                isinstance(other, AStructB)):
+            if cop == 2:  # different types are never equal
+                return False
+            else:         # different types are always notequal
+                return True
+
+        cdef cAStructB cself = deref((<AStructB>self)._cpp_obj)
+        cdef cAStructB cother = deref((<AStructB>other)._cpp_obj)
+        cdef cbool cmp = cself == cother
+        if cop == 2:
+            return cmp
+        return not cmp
+
+    cdef bytes _serialize(AStructB self, proto):
+        cdef string c_str
+        if proto is Protocol.COMPACT:
+            serializer.CompactSerialize[cAStructB](deref(self._cpp_obj.get()), &c_str)
+        elif proto is Protocol.BINARY:
+            serializer.BinarySerialize[cAStructB](deref(self._cpp_obj.get()), &c_str)
+        elif proto is Protocol.JSON:
+            serializer.JSONSerialize[cAStructB](deref(self._cpp_obj.get()), &c_str)
+        return <bytes> c_str
+
+    cdef uint32_t _deserialize(AStructB self, const IOBuf* buf, proto):
+        cdef uint32_t needed
+        if proto is Protocol.COMPACT:
+            needed = serializer.CompactDeserialize[cAStructB](buf, deref(self._cpp_obj.get()))
+        elif proto is Protocol.BINARY:
+            needed = serializer.BinaryDeserialize[cAStructB](buf, deref(self._cpp_obj.get()))
+        elif proto is Protocol.JSON:
+            needed = serializer.JSONDeserialize[cAStructB](buf, deref(self._cpp_obj.get()))
+        return needed
+
+    def __reduce__(self):
+        return (deserialize, (AStructB, serialize(self)))
+
+
 IncludedConstant = 42
 IncludedInt64 = int
