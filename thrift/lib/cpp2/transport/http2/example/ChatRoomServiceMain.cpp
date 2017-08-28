2a500b5cac5dbd1c4ff40814532ea7ab1d6a5ccb
Eddie Elizondo <eelizondo@fb.com>
2017-08-28 15:45:26 -0700

    HTTP2: Update example to work with ThriftServer
    Summary: This update the server in the HTTP2 example to work with an HTTP2 client and ThriftServer by adding the HTTP2RoutingHandler.
    
    Reviewed By: phoad
    
    Differential Revision: D5692518
    
    fbshipit-source-id: cba84d0b11bbd9cea356c39fcbaa6bb550b32264

--- thrift/lib/cpp2/transport/http2/example/ChatRoomServiceMain.cpp
+++ thrift/lib/cpp2/transport/http2/example/ChatRoomServiceMain.cpp
@@ -14,27 +14,51 @@
  * limitations under the License.
  */
 
+#include "thrift/lib/cpp2/transport/http2/example/ChatRoomService.h"
+
+#include <folly/init/Init.h>
 #include <gflags/gflags.h>
 #include <glog/logging.h>
-#include <thrift/lib/cpp2/transport/http2/server/H2ThriftServer.h>
-
-#include "thrift/lib/cpp2/transport/http2/example/ChatRoomService.h"
+#include <thrift/lib/cpp2/server/ThriftServer.h>
+#include <thrift/lib/cpp2/transport/core/ThriftProcessor.h>
+#include <thrift/lib/cpp2/transport/http2/common/HTTP2RoutingHandler.h>
+#include <thrift/lib/cpp2/transport/http2/server/ThriftRequestHandlerFactory.h>
+#include <proxygen/httpserver/HTTPServerOptions.h>
 
 DEFINE_int32(port, 7777, "Port for the thrift server");
 
 using namespace apache::thrift;
 using namespace facebook::tutorials::thrift::chatroomservice;
+using proxygen::RequestHandlerChain;
 
-int main(int /*argc*/, char** argv) {
-  google::InitGoogleLogging(argv[0]);
+std::unique_ptr<apache::thrift::HTTP2RoutingHandler> getHTTP2RoutingHandler(
+    std::shared_ptr<ThriftServer> server) {
+  auto h2_options = std::make_unique<proxygen::HTTPServerOptions>();
+  h2_options->threads = static_cast<size_t>(server->getNumIOWorkerThreads());
+  h2_options->idleTimeout = server->getIdleTimeout();
+  h2_options->shutdownOn = {SIGINT, SIGTERM};
+  h2_options->handlerFactories =
+      RequestHandlerChain()
+        .addThen<ThriftRequestHandlerFactory>(server->getThriftProcessor())
+        .build();
+
+  return std::make_unique<apache::thrift::HTTP2RoutingHandler>(
+      std::move(h2_options));
+}
+
+int main(int argc, char** argv) {
+  folly::init(&argc, &argv);
 
   auto handler = std::make_shared<ChatRoomServiceHandler>();
   auto cpp2PFac = std::make_shared<
       ThriftServerAsyncProcessorFactory<ChatRoomServiceHandler>>(handler);
 
-  auto server = std::make_shared<H2ThriftServer>();
+  auto server = std::make_shared<ThriftServer>();
   server->setPort(FLAGS_port);
   server->setProcessorFactory(cpp2PFac);
+
+  auto http2_transport_handler = getHTTP2RoutingHandler(server);
+  server->addRoutingHandler(http2_transport_handler.get());
 
   LOG(INFO) << "ChatRoomService running on port: " << FLAGS_port;
 
