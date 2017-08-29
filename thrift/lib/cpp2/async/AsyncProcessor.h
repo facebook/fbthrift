8687ae719295c3c45c6b1c5680585777ad781c9c
Eddie Elizondo <eelizondo@fb.com>
2017-08-29 12:21:19 -0700

    cpp2: Add missing ExceptionWrapper include
    Summary: The include was missing in AsyncProcessor
    
    Reviewed By: yfeldblum
    
    Differential Revision: D5726630
    
    fbshipit-source-id: ba53798edb83b3fe5f1655487879923420db04c8

--- thrift/lib/cpp2/async/AsyncProcessor.h
+++ thrift/lib/cpp2/async/AsyncProcessor.h
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014-present Facebook, Inc.
+ * Copyright 2004-present Facebook, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,9 +13,10 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-#ifndef THRIFT_ASYNCPROCESSOR2_H
-#define THRIFT_ASYNCPROCESSOR2_H 1
 
+#pragma once
+
+#include <folly/ExceptionWrapper.h>
 #include <folly/String.h>
 #include <folly/futures/Future.h>
 #include <folly/io/async/EventBase.h>
@@ -857,5 +858,3 @@ class ServerInterface : public AsyncProcessorFactory {
 };
 
 }} // apache::thrift
-
-#endif // THRIFT_ASYNCPROCESSOR2_H
