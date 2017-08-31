1d3706cb6d83d4e62047bcffa751b3ee7e4efe9b
Eddie Elizondo <eelizondo@fb.com>
2017-08-31 09:35:41 -0700

    oss: Enable building in external directory
    Summary: Make build independent of source
    
    Reviewed By: yfeldblum
    
    Differential Revision: D5741633
    
    fbshipit-source-id: 7f8d7fbdefd4f13b9cd945dfd83d78d23a20c6ab

--- thrift/cmake/FindFolly.cmake
+++ thrift/cmake/FindFolly.cmake
@@ -2,29 +2,29 @@
 #
 # This will define:
 # FOLLY_FOUND
-# FOLLY_INCLUDE_DIRS
-# FOLLY_LIBRARIES
+# FOLLY_INCLUDE_DIR
+# FOLLY_LIBRARY
 
 find_path(
-    FOLLY_INCLUDE_DIRS folly/String.h
+    FOLLY_INCLUDE_DIR folly/String.h
     HINTS
         $ENV{FOLLY_ROOT}/include
         ${FOLLY_ROOT}/include
 )
 
 find_library(
-    FOLLY_LIBRARIES
+    FOLLY_LIBRARY
     NAMES folly
     HINTS
         $ENV{FOLLY_ROOT}/lib
         ${FOLLY_ROOT}/lib
 )
 
-mark_as_advanced(FOLLY_INCLUDE_DIRS FOLLY_LIBRARIES)
+mark_as_advanced(FOLLY_INCLUDE_DIR FOLLY_LIBRARY)
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(FOLLY FOLLY_INCLUDE_DIRS FOLLY_LIBRARIES)
+find_package_handle_standard_args(FOLLY FOLLY_INCLUDE_DIR FOLLY_LIBRARY)
 
 if(FOLLY_FOUND AND NOT FOLLY_FIND_QUIETLY)
-    message(STATUS "FOLLY: ${FOLLY_INCLUDE_DIRS}")
+    message(STATUS "FOLLY: ${FOLLY_INCLUDE_DIR}")
 endif()
