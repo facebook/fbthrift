598ce9a7d530dea694272d0508f91c00ffcd731b
Steve Gury <stevegury@fb.com>
2018-02-15 15:17:45 -0800

    Refactoring of ContextStack
    Summary:
    Split the EventHandlerBase file which contains multiple classes into smaller files.
    Other than moving code around, I didn't do any structural changes.
    
    Reviewed By: yfeldblum
    
    Differential Revision: D6989190
    
    fbshipit-source-id: b4a80e07cb8e53784685828276cc15583e4b1bfa

---
+++ thrift/lib/cpp/ContextStack.h
@@ -0,0 +1,168 @@
+/*
+ * Copyright 2018-present Facebook, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#include <thrift/lib/cpp/SerializedMessage.h>
+#include <thrift/lib/cpp/TProcessorEventHandler.h>
+#include <thrift/lib/cpp/protocol/TProtocolTypes.h>
+#include <thrift/lib/cpp/transport/THeader.h>
+
+namespace apache {
+namespace thrift {
+
+class ContextStack {
+  friend class EventHandlerBase;
+
+ public:
+  explicit ContextStack(const char* method)
+      : ctxs_(), handlers_(), method_(method) {}
+
+  ContextStack(
+      const std::shared_ptr<
+          std::vector<std::shared_ptr<TProcessorEventHandler>>>& handlers,
+      const char* serviceName,
+      const char* method,
+      TConnectionContext* connectionContext)
+      : ctxs_(), handlers_(handlers), method_(method) {
+    ctxs_.reserve(handlers->size());
+    for (auto handler : *handlers) {
+      ctxs_.push_back(
+          handler->getServiceContext(serviceName, method, connectionContext));
+    }
+  }
+
+  ContextStack(
+      const std::shared_ptr<
+          std::vector<std::shared_ptr<TProcessorEventHandler>>>& handlers,
+      const char* method,
+      TConnectionContext* connectionContext)
+      : ctxs_(), handlers_(handlers), method_(method) {
+    for (auto handler : *handlers) {
+      ctxs_.push_back(handler->getContext(method, connectionContext));
+    }
+  }
+
+  ~ContextStack() {
+    if (handlers_) {
+      for (size_t i = 0; i < handlers_->size(); i++) {
+        (*handlers_)[i]->freeContext(ctxs_[i], getMethod());
+      }
+    }
+  }
+
+  void preWrite() {
+    if (handlers_) {
+      for (size_t i = 0; i < handlers_->size(); i++) {
+        (*handlers_)[i]->preWrite(ctxs_[i], getMethod());
+      }
+    }
+  }
+
+  void onWriteData(const SerializedMessage& msg) {
+    if (handlers_) {
+      for (size_t i = 0; i < handlers_->size(); i++) {
+        (*handlers_)[i]->onWriteData(ctxs_[i], getMethod(), msg);
+      }
+    }
+  }
+
+  void postWrite(uint32_t bytes) {
+    if (handlers_) {
+      for (size_t i = 0; i < handlers_->size(); i++) {
+        (*handlers_)[i]->postWrite(ctxs_[i], getMethod(), bytes);
+      }
+    }
+  }
+
+  void preRead() {
+    if (handlers_) {
+      for (size_t i = 0; i < handlers_->size(); i++) {
+        (*handlers_)[i]->preRead(ctxs_[i], getMethod());
+      }
+    }
+  }
+
+  void onReadData(const SerializedMessage& msg) {
+    if (handlers_) {
+      for (size_t i = 0; i < handlers_->size(); i++) {
+        (*handlers_)[i]->onReadData(ctxs_[i], getMethod(), msg);
+      }
+    }
+  }
+
+  void postRead(apache::thrift::transport::THeader* header, uint32_t bytes) {
+    if (handlers_) {
+      for (size_t i = 0; i < handlers_->size(); i++) {
+        (*handlers_)[i]->postRead(ctxs_[i], getMethod(), header, bytes);
+      }
+    }
+  }
+
+  void handlerError() {
+    if (handlers_) {
+      for (size_t i = 0; i < handlers_->size(); i++) {
+        (*handlers_)[i]->handlerError(ctxs_[i], getMethod());
+      }
+    }
+  }
+
+  void handlerErrorWrapped(const folly::exception_wrapper& ew) {
+    if (handlers_) {
+      for (size_t i = 0; i < handlers_->size(); i++) {
+        (*handlers_)[i]->handlerErrorWrapped(ctxs_[i], getMethod(), ew);
+      }
+    }
+  }
+
+  void userException(const std::string& ex, const std::string& ex_what) {
+    if (handlers_) {
+      for (size_t i = 0; i < handlers_->size(); i++) {
+        (*handlers_)[i]->userException(ctxs_[i], getMethod(), ex, ex_what);
+      }
+    }
+  }
+
+  void userExceptionWrapped(bool declared, const folly::exception_wrapper& ew) {
+    if (handlers_) {
+      for (size_t i = 0; i < handlers_->size(); i++) {
+        (*handlers_)[i]->userExceptionWrapped(
+            ctxs_[i], getMethod(), declared, ew);
+      }
+    }
+  }
+
+  void asyncComplete() {
+    if (handlers_) {
+      for (size_t i = 0; i < handlers_->size(); i++) {
+        (*handlers_)[i]->asyncComplete(ctxs_[i], getMethod());
+      }
+    }
+  }
+
+  const char* getMethod() {
+    return method_.c_str();
+  }
+
+ private:
+  std::vector<void*> ctxs_;
+  std::shared_ptr<std::vector<std::shared_ptr<TProcessorEventHandler>>>
+      handlers_;
+  std::string method_;
+};
+
+} // namespace thrift
+} // namespace apache
---
+++ thrift/lib/cpp/TProcessorEventHandler.h
@@ -0,0 +1,161 @@
+/*
+ * Copyright 2018-present Facebook, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#include <thrift/lib/cpp/server/TConnectionContext.h>
+#include <thrift/lib/cpp/transport/THeader.h>
+
+namespace apache {
+namespace thrift {
+
+using server::TConnectionContext;
+
+/**
+ * Virtual interface class that can handle events from the processor. To
+ * use this you should subclass it and implement the methods that you care
+ * about. Your subclass can also store local data that you may care about,
+ * such as additional "arguments" to these methods (stored in the object
+ * instance's state).
+ */
+class TProcessorEventHandler {
+ public:
+  virtual ~TProcessorEventHandler() {}
+
+  /**
+   * Called before calling other callback methods.
+   * Expected to return some sort of context object.
+   * The return value is passed to all other callbacks
+   * for that function invocation.
+   */
+  virtual void* getServiceContext(
+      const char* /*service_name*/,
+      const char* fn_name,
+      TConnectionContext* connectionContext) {
+    return getContext(fn_name, connectionContext);
+  }
+  virtual void* getContext(
+      const char* /*fn_name*/,
+      TConnectionContext* /*connectionContext*/) {
+    return nullptr;
+  }
+
+  /**
+   * Expected to free resources associated with a context.
+   */
+  virtual void freeContext(void* /*ctx*/, const char* /*fn_name*/) {}
+
+  /**
+   * Called before reading arguments.
+   */
+  virtual void preRead(void* /*ctx*/, const char* /*fn_name*/) {}
+
+  /**
+   * Called before postRead, after reading arguments (server) / after reading
+   * reply (client), with the actual (unparsed, serialized) data.
+   *
+   * The data is framed by message begin / end entries, call readMessageBegin /
+   * readMessageEnd on the protocol.
+   *
+   * Only called for Cpp2.
+   */
+  virtual void onReadData(
+      void* /*ctx*/,
+      const char* /*fn_name*/,
+      const SerializedMessage& /*msg*/) {}
+
+  /**
+   * Called between reading arguments and calling the handler.
+   */
+  virtual void postRead(
+      void* /*ctx*/,
+      const char* /*fn_name*/,
+      apache::thrift::transport::THeader* /*header*/,
+      uint32_t /*bytes*/) {}
+
+  /**
+   * Called between calling the handler and writing the response.
+   */
+  virtual void preWrite(void* /*ctx*/, const char* /*fn_name*/) {}
+
+  /**
+   * Called before postWrite, after serializing response (server) / after
+   * serializing request (client), with the actual (serialized) data.
+   *
+   * The data is framed by message begin / end entries, call readMessageBegin /
+   * readMessageEnd on the protocol.
+   *
+   * Only called for Cpp2.
+   */
+  virtual void onWriteData(
+      void* /*ctx*/,
+      const char* /*fn_name*/,
+      const SerializedMessage& /*msg*/) {}
+
+  /**
+   * Called after writing the response.
+   */
+  virtual void
+  postWrite(void* /*ctx*/, const char* /*fn_name*/, uint32_t /*bytes*/) {}
+
+  /**
+   * Called when an async function call completes successfully.
+   */
+  virtual void asyncComplete(void* /*ctx*/, const char* /*fn_name*/) {}
+
+  /**
+   * Called if the handler throws an undeclared exception.
+   */
+  virtual void handlerError(void* /*ctx*/, const char* /*fn_name*/) {}
+  virtual void handlerErrorWrapped(
+      void* ctx,
+      const char* fn_name,
+      const folly::exception_wrapper& /*ew*/) {
+    handlerError(ctx, fn_name);
+  }
+
+  /**
+   * Called if the handler throws an exception.
+   *
+   * Only called for Cpp2
+   */
+  virtual void userException(
+      void* /*ctx*/,
+      const char* /*fn_name*/,
+      const std::string& /*ex*/,
+      const std::string& /*ex_what*/) {}
+  virtual void userExceptionWrapped(
+      void* ctx,
+      const char* fn_name,
+      bool declared,
+      const folly::exception_wrapper& ew_) {
+    auto ew = ew_; // make a local, mutable copy
+    const auto type = ew.class_name();
+    const auto what = ew.what();
+    folly::StringPiece whatsp(what);
+    if (declared) {
+      CHECK(whatsp.removePrefix(type)) << "weird format: '" << what << "'";
+      CHECK(whatsp.removePrefix(": ")) << "weird format: '" << what << "'";
+    }
+    return userException(ctx, fn_name, type.toStdString(), whatsp.str());
+  }
+
+ protected:
+  TProcessorEventHandler() {}
+};
+
+} // namespace thrift
+} // namespace apache
