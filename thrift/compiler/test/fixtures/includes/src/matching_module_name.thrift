993251ff135643ae404f1637c6d35d83334364e5
Stepan Palamarchuk <stepan@fb.com>
2018-03-01 04:41:43 -0800

    Extend fixtures for includes
    Summary:
    This extends includes fixture with next case: one file includes another file with the same name and same namespace, but in different folder.
    
    As you may notice right now the codegen would produce incorrect `#include` statements (it would actually include itself).
    This is caused by cachekey collision in mstch-cpp2 codegen. Fixed in the next stacked diff.
    
    Reviewed By: yfeldblum
    
    Differential Revision: D7114924
    
    fbshipit-source-id: 112613ab443e95e0232416fc7a28ccf64d98004d

---
+++ thrift/compiler/test/fixtures/includes/src/matching_module_name.thrift
@@ -0,0 +1,7 @@
+include "other/matching_module_name.thrift"
+
+namespace cpp2 matching_module_name
+
+struct MyStruct {
+  1: matching_module_name.OtherStruct OtherStructField;
+}
---
+++ thrift/compiler/test/fixtures/includes/src/other/matching_module_name.thrift
@@ -0,0 +1,5 @@
+namespace cpp2 matching_module_name
+
+struct OtherStruct {
+  1: i64 id;
+}
