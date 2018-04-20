2b18a576c81cf195559cd2d380aff3bee94d21e5
Tom Jackson <tjackson@fb.com>
2018-04-20 16:14:23 -0700

    Benchmarks += strings, more accurate
    Summary:
    - No more `int16_t` maps.
    - Results now measure cost per lookup.
    - Frozen test no longer "cheats" with a smaller key range (accidental)
    - String lookup added
    - Entry count increased to 1e6
    
    ```
    ============================================================================
    thrift/lib/cpp2/frozen/test/FrozenBench.cpp     relative  time/iter  iters/s
    ============================================================================
    ----------------------------------------------------------------------------
    ----------------------------------------------------------------------------
    benchmarkLookup(map_f32)                                   374.64ns    2.67M
    benchmarkLookup(frozenMap_f32)                   159.93%   234.26ns    4.27M
    benchmarkLookup(map_i32)                                   359.70ns    2.78M
    benchmarkLookup(frozenMap_i32)                   103.14%   348.75ns    2.87M
    benchmarkLookup(map_i64)                                   370.04ns    2.70M
    benchmarkLookup(frozenMap_i64)                   111.36%   332.28ns    3.01M
    ----------------------------------------------------------------------------
    benchmarkLookup(hashMap_f32)                                68.05ns   14.70M
    benchmarkLookup(frozenHashMap_f32)                80.42%    84.61ns   11.82M
    benchmarkLookup(hashMap_i32)                                22.17ns   45.11M
    benchmarkLookup(frozenHashMap_i32)                32.25%    68.73ns   14.55M
    benchmarkLookup(hashMap_i64)                                26.56ns   37.65M
    benchmarkLookup(frozenHashMap_i64)                28.19%    94.21ns   10.61M
    benchmarkLookup(hashMap_str)                               129.41ns    7.73M
    benchmarkLookup(frozenHashMap_str)                77.64%   166.67ns    6.00M
    ----------------------------------------------------------------------------
    ============================================================================
    ```
    
    Reviewed By: ot
    
    Differential Revision: D7701375
    
    fbshipit-source-id: 6a1b3b9b24bf48da5ec9f7103b1a99d6e56b1230

--- thrift/lib/cpp2/frozen/test/FrozenBench.cpp
+++ thrift/lib/cpp2/frozen/test/FrozenBench.cpp
@@ -14,6 +14,7 @@
  * limitations under the License.
  */
 #include <folly/Benchmark.h>
+#include <folly/Random.h>
 
 #include <thrift/lib/cpp2/frozen/FrozenUtil.h>
 #include <thrift/lib/cpp2/frozen/HintTypes.h>
@@ -23,8 +24,8 @@
 using namespace apache::thrift;
 using namespace frozen;
 
-const int kRows = 490;
-const int kCols = 51;
+constexpr int kRows = 490;
+constexpr int kCols = 51;
 
 template <class T, class Inner = std::vector<T>>
 std::vector<Inner> makeMatrix() {
@@ -33,28 +34,14 @@ std::vector<Inner> makeMatrix() {
     vals.emplace_back();
     auto& row = vals.back();
     for (int x = 0; x < kCols; ++x) {
-      row.push_back(rand() % 12000 - 6000);
+      row.push_back(folly::Random::rand32(12000) - 6000);
     }
   }
   return vals;
 }
 
-auto vvf32 = makeMatrix<float>();
-auto fvvf32 = freeze(vvf32);
-auto vvi16 = makeMatrix<int16_t>();
-auto vvi32 = makeMatrix<int32_t>();
-auto vvi64 = makeMatrix<int64_t>();
-auto fvvi16 = freeze(vvi16);
-auto fvvi32 = freeze(vvi32);
-auto fvvi64 = freeze(vvi64);
-auto fuvvi16 = freeze(makeMatrix<int16_t, VectorUnpacked<int16_t>>());
-auto fuvvi32 = freeze(makeMatrix<int32_t, VectorUnpacked<int32_t>>());
-auto fuvvi64 = freeze(makeMatrix<int64_t, VectorUnpacked<int64_t>>());
-auto strings = std::vector<std::string>{"one", "two", "three", "four", "five"};
-auto tuple = std::make_pair(std::make_pair(1.3, 2.4), std::make_pair(4.5, 'x'));
-
 template <class F>
-void benchmarkSum(int iters, const F& matrix) {
+void benchmarkSum(size_t iters, const F& matrix) {
   int s = 0;
   while (iters--) {
     for (auto& row : matrix) {
@@ -67,7 +54,7 @@ void benchmarkSum(int iters, const F& matrix) {
 }
 
 template <class F>
-void benchmarkSumCols(int iters, const F& matrix) {
+void benchmarkSumCols(size_t iters, const F& matrix) {
   int s = 0;
   while (iters--) {
     for (size_t x = 0; x < kCols; ++x) {
@@ -80,7 +67,7 @@ void benchmarkSumCols(int iters, const F& matrix) {
 }
 
 template <class F>
-void benchmarkSumSavedCols(int iters, const F& matrix) {
+void benchmarkSumSavedCols(size_t iters, const F& matrix) {
   folly::BenchmarkSuspender setup;
   std::vector<typename F::value_type> rows;
   for (size_t y = 0; y < kRows; ++y) {
@@ -98,6 +85,18 @@ void benchmarkSumSavedCols(int iters, const F& matrix) {
   }
   folly::doNotOptimizeAway(s);
 }
+
+auto vvi16 = makeMatrix<int16_t>();
+auto vvi32 = makeMatrix<int32_t>();
+auto vvi64 = makeMatrix<int64_t>();
+auto fvvi16 = freeze(vvi16);
+auto fvvi32 = freeze(vvi32);
+auto fvvi64 = freeze(vvi64);
+auto fuvvi16 = freeze(makeMatrix<int16_t, VectorUnpacked<int16_t>>());
+auto fuvvi32 = freeze(makeMatrix<int32_t, VectorUnpacked<int32_t>>());
+auto fuvvi64 = freeze(makeMatrix<int64_t, VectorUnpacked<int64_t>>());
+auto vvf32 = makeMatrix<float>();
+auto fvvf32 = freeze(vvf32);
 
 BENCHMARK_PARAM(benchmarkSum, vvi16)
 BENCHMARK_RELATIVE_PARAM(benchmarkSum, fvvi16)
@@ -117,72 +116,57 @@ BENCHMARK_RELATIVE_PARAM(benchmarkSumCols, fuvvi32)
 BENCHMARK_RELATIVE_PARAM(benchmarkSumSavedCols, fvvi32)
 BENCHMARK_RELATIVE_PARAM(benchmarkSumSavedCols, fuvvi32)
 
-const size_t entries = 100000;
+constexpr size_t kEntries = 1000000;
+constexpr size_t kChunkSize = 1000;
+
+template <class K>
+K makeKey() {
+  return folly::to<K>(folly::Random::rand32(kEntries));
+}
+
+template <class K>
+const std::vector<K>& makeKeys() {
+  // static to reuse storage across iterations.
+  static std::vector<K> keys(kChunkSize);
+  for (auto& key : keys) {
+    key = makeKey<K>();
+  }
+  return keys;
+}
+
 template <class Map>
 Map makeMap() {
   Map hist;
-  for (size_t y = 0; y < entries * 3; ++y) {
-    auto k = (rand() * 8192 + rand()) % entries;
-    ++hist[k];
+  using K = typename Map::key_type;
+  for (size_t y = 0; y < kEntries; ++y) {
+    ++hist[makeKey<K>()];
   }
   return hist;
 }
 
-auto hashMap_f32 = makeMap<std::unordered_map<float, int>>();
-auto hashMap_i16 = makeMap<std::unordered_map<int16_t, int>>();
-auto hashMap_i32 = makeMap<std::unordered_map<int32_t, int>>();
-auto hashMap_i64 = makeMap<std::unordered_map<int64_t, int>>();
-auto frozenHashMap_f32 = freeze(hashMap_f32);
-auto frozenHashMap_i16 = freeze(hashMap_i16);
-auto frozenHashMap_i32 = freeze(hashMap_i32);
-auto frozenHashMap_i64 = freeze(hashMap_i64);
-auto map_f32 = std::map<float, int>(hashMap_f32.begin(), hashMap_f32.end());
-auto map_i16 = std::map<int16_t, int>(hashMap_i16.begin(), hashMap_i16.end());
-auto map_i32 = std::map<int32_t, int>(hashMap_i32.begin(), hashMap_i32.end());
-auto map_i64 = std::map<int64_t, int>(hashMap_i64.begin(), hashMap_i64.end());
-auto frozenMap_f32 = freeze(map_f32);
-auto frozenMap_i16 = freeze(map_i16);
-auto frozenMap_i32 = freeze(map_i32);
-auto frozenMap_i64 = freeze(map_i64);
+template <class K>
+using OwnedKey = typename std::conditional<
+    std::is_same<K, folly::StringPiece>::value,
+    std::string,
+    K>::type;
 
 template <class Map>
-void benchmarkLookup(int iters, const Map& hist) {
+void benchmarkLookup(size_t iters, const Map& map) {
+  using K = OwnedKey<typename Map::key_type>;
   int s = 0;
-  while (iters--) {
-    for (int n = 0; n < 1000; ++n) {
-      auto found = hist.find(rand());
-      if (found != hist.end()) {
-        s += found->second();
-      }
-    }
-  }
-  folly::doNotOptimizeAway(s);
-}
+  for (;;) {
+    folly::BenchmarkSuspender setup;
+    auto& keys = makeKeys<K>();
+    setup.dismiss();
 
-template <class T>
-void benchmarkLookup(int iters, const std::map<T, int>& hist) {
-  int s = 0;
-  while (iters--) {
-    for (int n = 0; n < 1000; ++n) {
-      auto k = (rand() * 8192 + rand()) % entries;
-      auto found = hist.find(k);
-      if (found != hist.end()) {
-        s += found->second;
+    for (auto& key : keys) {
+      if (iters-- == 0) {
+        folly::doNotOptimizeAway(s);
+        return;
       }
-    }
-  }
-  folly::doNotOptimizeAway(s);
-}
-
-template <class T>
-void benchmarkLookup(int iters, const std::unordered_map<T, int>& hist) {
-  int s = 0;
-  while (iters--) {
-    for (int n = 0; n < 1000; ++n) {
-      auto k = (rand() * 8192 + rand()) % entries;
-      auto found = hist.find(k);
-      if (found != hist.end()) {
-        s += found->second;
+      auto found = map.find(key);
+      if (found != map.end()) {
+        ++s;
       }
     }
   }
@@ -191,10 +175,35 @@ void benchmarkLookup(int iters, const std::unordered_map<T, int>& hist) {
 
 BENCHMARK_DRAW_LINE();
 
+auto hashMap_f32 = makeMap<std::unordered_map<float, int>>();
+auto hashMap_i32 = makeMap<std::unordered_map<int32_t, int>>();
+auto hashMap_i64 = makeMap<std::unordered_map<int64_t, int>>();
+auto hashMap_str = makeMap<std::unordered_map<std::string, int>>();
+auto frozenHashMap_f32 = freeze(hashMap_f32);
+auto frozenHashMap_i32 = freeze(hashMap_i32);
+auto frozenHashMap_i64 = freeze(hashMap_i64);
+auto frozenHashMap_str = freeze(hashMap_str);
+
+BENCHMARK_PARAM(benchmarkLookup, hashMap_f32)
+BENCHMARK_RELATIVE_PARAM(benchmarkLookup, frozenHashMap_f32)
+BENCHMARK_PARAM(benchmarkLookup, hashMap_i32)
+BENCHMARK_RELATIVE_PARAM(benchmarkLookup, frozenHashMap_i32)
+BENCHMARK_PARAM(benchmarkLookup, hashMap_i64)
+BENCHMARK_RELATIVE_PARAM(benchmarkLookup, frozenHashMap_i64)
+BENCHMARK_PARAM(benchmarkLookup, hashMap_str)
+BENCHMARK_RELATIVE_PARAM(benchmarkLookup, frozenHashMap_str)
+
+BENCHMARK_DRAW_LINE();
+
+auto map_f32 = std::map<float, int>(hashMap_f32.begin(), hashMap_f32.end());
+auto map_i32 = std::map<int32_t, int>(hashMap_i32.begin(), hashMap_i32.end());
+auto map_i64 = std::map<int64_t, int>(hashMap_i64.begin(), hashMap_i64.end());
+auto frozenMap_f32 = freeze(map_f32);
+auto frozenMap_i32 = freeze(map_i32);
+auto frozenMap_i64 = freeze(map_i64);
+
 BENCHMARK_PARAM(benchmarkLookup, map_f32)
 BENCHMARK_RELATIVE_PARAM(benchmarkLookup, frozenMap_f32)
-BENCHMARK_PARAM(benchmarkLookup, map_i16)
-BENCHMARK_RELATIVE_PARAM(benchmarkLookup, frozenMap_i16)
 BENCHMARK_PARAM(benchmarkLookup, map_i32)
 BENCHMARK_RELATIVE_PARAM(benchmarkLookup, frozenMap_i32)
 BENCHMARK_PARAM(benchmarkLookup, map_i64)
@@ -202,19 +211,8 @@ BENCHMARK_RELATIVE_PARAM(benchmarkLookup, frozenMap_i64)
 
 BENCHMARK_DRAW_LINE();
 
-BENCHMARK_PARAM(benchmarkLookup, hashMap_f32)
-BENCHMARK_RELATIVE_PARAM(benchmarkLookup, frozenHashMap_f32)
-BENCHMARK_PARAM(benchmarkLookup, hashMap_i16)
-BENCHMARK_RELATIVE_PARAM(benchmarkLookup, frozenHashMap_i16)
-BENCHMARK_PARAM(benchmarkLookup, hashMap_i32)
-BENCHMARK_RELATIVE_PARAM(benchmarkLookup, frozenHashMap_i32)
-BENCHMARK_PARAM(benchmarkLookup, hashMap_i64)
-BENCHMARK_RELATIVE_PARAM(benchmarkLookup, frozenHashMap_i64)
-
-BENCHMARK_DRAW_LINE();
-
 template <class T>
-void benchmarkOldFreezeDataToString(int iters, const T& data) {
+void benchmarkOldFreezeDataToString(size_t iters, const T& data) {
   const auto layout = maximumLayout<T>();
   size_t s = 0;
   while (iters--) {
@@ -230,7 +228,7 @@ void benchmarkOldFreezeDataToString(int iters, const T& data) {
 }
 
 template <class T>
-void benchmarkFreezeDataToString(int iters, const T& data) {
+void benchmarkFreezeDataToString(size_t iters, const T& data) {
   const auto layout = maximumLayout<T>();
   size_t s = 0;
   while (iters--) {
@@ -238,6 +236,9 @@ void benchmarkFreezeDataToString(int iters, const T& data) {
   }
   folly::doNotOptimizeAway(s);
 }
+
+auto strings = std::vector<std::string>{"one", "two", "three", "four", "five"};
+auto tuple = std::make_pair(std::make_pair(1.3, 2.4), std::make_pair(4.5, 'x'));
 
 BENCHMARK_PARAM(benchmarkFreezeDataToString, vvf32)
 BENCHMARK_RELATIVE_PARAM(benchmarkOldFreezeDataToString, vvf32)
@@ -252,41 +253,48 @@ BENCHMARK_RELATIVE_PARAM(benchmarkOldFreezeDataToString, hashMap_i32)
 ============================================================================
 thrift/lib/cpp2/frozen/test/FrozenBench.cpp     relative  time/iter  iters/s
 ============================================================================
-benchmarkSum(vvi16)                                          4.13us  241.93K
-benchmarkSum(fvvi16)                               3.80%   108.74us    9.20K
-benchmarkSum(fuvvi16)                             48.05%     8.60us  116.24K
-benchmarkSum(vvi32)                                          4.70us  212.83K
-benchmarkSum(fvvi32)                               4.35%   107.97us    9.26K
-benchmarkSum(fuvvi32)                             55.06%     8.53us  117.19K
-benchmarkSum(vvi64)                                         10.83us   92.34K
-benchmarkSum(fvvi64)                              14.84%    72.98us   13.70K
-benchmarkSum(fuvvi64)                             65.67%    16.49us   60.64K
-benchmarkSum(vvf32)                                         69.91us   14.30K
-benchmarkSum(fvvf32)                              99.85%    70.02us   14.28K
+benchmarkSum(vvi16)                                          4.93us  203.01K
+benchmarkSum(fvvi16)                               3.86%   127.70us    7.83K
+benchmarkSum(fuvvi16)                             49.45%     9.96us  100.39K
+benchmarkSum(vvi32)                                          5.76us  173.51K
+benchmarkSum(fvvi32)                               4.51%   127.72us    7.83K
+benchmarkSum(fuvvi32)                             55.70%    10.35us   96.64K
+benchmarkSum(vvi64)                                         13.80us   72.48K
+benchmarkSum(fvvi64)                               7.37%   187.20us    5.34K
+benchmarkSum(fuvvi64)                             75.72%    18.22us   54.88K
+benchmarkSum(vvf32)                                         83.52us   11.97K
+benchmarkSum(fvvf32)                              99.54%    83.90us   11.92K
 ----------------------------------------------------------------------------
-benchmarkSumCols(vvi32)                                     14.90us   67.11K
-benchmarkSumCols(fvvi32)                           6.75%   220.84us    4.53K
-benchmarkSumCols(fuvvi32)                         10.51%   141.85us    7.05K
-benchmarkSumSavedCols(fvvi32)                     15.29%    97.46us   10.26K
-benchmarkSumSavedCols(fuvvi32)                   124.74%    11.95us   83.70K
+benchmarkSumCols(vvi32)                                     35.23us   28.38K
+benchmarkSumCols(fvvi32)                          12.49%   282.07us    3.55K
+benchmarkSumCols(fuvvi32)                         18.26%   192.99us    5.18K
+benchmarkSumSavedCols(fvvi32)                     32.34%   108.94us    9.18K
+benchmarkSumSavedCols(fuvvi32)                   194.45%    18.12us   55.19K
 ----------------------------------------------------------------------------
-benchmarkLookup(map_f32)                                   241.65us    4.14K
-benchmarkLookup(frozenMap_f32)                   423.24%    57.09us   17.51K
-benchmarkLookup(map_i16)                                   212.87us    4.70K
-benchmarkLookup(frozenMap_i16)                    85.52%   248.91us    4.02K
-benchmarkLookup(map_i32)                                   229.78us    4.35K
-benchmarkLookup(frozenMap_i32)                   171.00%   134.37us    7.44K
-benchmarkLookup(map_i64)                                   231.73us    4.32K
-benchmarkLookup(frozenMap_i64)                   205.83%   112.58us    8.88K
+benchmarkLookup(hashMap_f32)                                64.58ns   15.49M
+benchmarkLookup(frozenHashMap_f32)               132.46%    48.75ns   20.51M
+benchmarkLookup(hashMap_i32)                                21.59ns   46.31M
+benchmarkLookup(frozenHashMap_i32)                57.54%    37.52ns   26.65M
+benchmarkLookup(hashMap_i64)                                26.85ns   37.24M
+benchmarkLookup(frozenHashMap_i64)                77.91%    34.47ns   29.01M
+benchmarkLookup(hashMap_str)                               122.18ns    8.18M
+benchmarkLookup(frozenHashMap_str)               128.36%    95.18ns   10.51M
 ----------------------------------------------------------------------------
-benchmarkLookup(hashMap_f32)                                88.28us   11.33K
-benchmarkLookup(frozenHashMap_f32)               220.12%    40.11us   24.93K
-benchmarkLookup(hashMap_i16)                                51.96us   19.24K
-benchmarkLookup(frozenHashMap_i16)               108.97%    47.68us   20.97K
-benchmarkLookup(hashMap_i32)                                48.50us   20.62K
-benchmarkLookup(frozenHashMap_i32)               133.37%    36.37us   27.50K
-benchmarkLookup(hashMap_i64)                                51.68us   19.35K
-benchmarkLookup(frozenHashMap_i64)                61.69%    83.77us   11.94K
+benchmarkLookup(map_f32)                                   352.68ns    2.84M
+benchmarkLookup(frozenMap_f32)                   177.46%   198.74ns    5.03M
+benchmarkLookup(map_i32)                                   347.51ns    2.88M
+benchmarkLookup(frozenMap_i32)                   110.00%   315.92ns    3.17M
+benchmarkLookup(map_i64)                                   344.25ns    2.90M
+benchmarkLookup(frozenMap_i64)                   123.47%   278.80ns    3.59M
+----------------------------------------------------------------------------
+benchmarkFreezeDataToString(vvf32)                         107.88us    9.27K
+benchmarkOldFreezeDataToString(vvf32)             82.78%   130.31us    7.67K
+benchmarkFreezeDataToString(tuple)                         233.81ns    4.28M
+benchmarkOldFreezeDataToString(tuple)            236.93%    98.68ns   10.13M
+benchmarkFreezeDataToString(strings)                       839.63ns    1.19M
+benchmarkOldFreezeDataToString(strings)          297.64%   282.10ns    3.54M
+benchmarkFreezeDataToString(hashMap_i32)                    41.09ms    24.34
+benchmarkOldFreezeDataToString(hashMap_i32)       43.27%    94.95ms    10.53
 ============================================================================
 #endif
 
