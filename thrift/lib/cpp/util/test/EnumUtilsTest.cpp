f6a849edefc6923496099e64a4ae72b70c8c521c
Yedidya Feldblum <yfeldblum@fb.com>
2018-05-01 19:28:08 -0700

    Cut thrift/lib/cpp/util/test/enum_non_strict.thrift
    Summary: [Thrift] Cut `thrift/lib/cpp/util/test/enum_non_strict.thrift`, removing the parts of `thrift/lib/cpp/util/test/EnumUtilsTest.cpp` which use it.
    
    Reviewed By: stevegury
    
    Differential Revision: D7824372
    
    fbshipit-source-id: 914560f4319752e7a64bea261015685ab7391369

--- thrift/lib/cpp/util/test/EnumUtilsTest.cpp
+++ thrift/lib/cpp/util/test/EnumUtilsTest.cpp
@@ -17,12 +17,11 @@
 #include <gtest/gtest.h>
 
 #include <thrift/lib/cpp/util/EnumUtils.h>
-#include <thrift/lib/cpp/util/test/gen-cpp/enum_non_strict_types.h>
 #include <thrift/lib/cpp/util/test/gen-cpp/enum_strict_types.h>
 
 using namespace apache::thrift::util;
 
-TEST(EnumStrict, ShortEnumName) {
+TEST(EnumUtilsTest, ShortEnumName) {
   EXPECT_STREQ(enumName((EnumStrict)0), "UNKNOWN");
   EXPECT_STREQ(enumName((EnumStrict)1), "VALUE");
   EXPECT_STREQ(enumName((EnumStrict)2), "FOO");
@@ -33,33 +32,11 @@ TEST(EnumStrict, ShortEnumName) {
   EXPECT_EQ(enumName((EnumStrict)-1), nullptr);
 }
 
-TEST(EnumNonStrict, ShortEnumName) {
-  EXPECT_STREQ(enumName((EnumNonStrict)0), "UNKNOWN");
-  EXPECT_STREQ(enumName((EnumNonStrict)1), "VALUE");
-  EXPECT_STREQ(enumName((EnumNonStrict)2), "FOO");
-  EXPECT_STREQ(enumName((EnumNonStrict)3), "BAR");
-  EXPECT_STREQ(enumName((EnumNonStrict)6), "NEW_VALUE");
-  EXPECT_STREQ(enumName((EnumStrict)3, "NOTBAR"), "BAR");
-  EXPECT_STREQ(enumName((EnumStrict)42, "Universe"), "Universe");
-  EXPECT_EQ(enumName((EnumNonStrict)4), nullptr);
-  EXPECT_EQ(enumName((EnumNonStrict)5), nullptr);
-}
-
-TEST(EnumStrict, ShortEnumNameSafe) {
+TEST(EnumUtilsTest, ShortEnumNameSafe) {
   EXPECT_EQ(enumNameSafe((EnumStrict)0), "UNKNOWN");
   EXPECT_EQ(enumNameSafe((EnumStrict)1), "VALUE");
   EXPECT_EQ(enumNameSafe((EnumStrict)2), "FOO");
   EXPECT_EQ(enumNameSafe((EnumStrict)3), "BAR");
   EXPECT_EQ(enumNameSafe((EnumStrict)42), "42");
   EXPECT_EQ(enumNameSafe((EnumStrict)-1), "-1");
-}
-
-TEST(EnumNonStrict, ShortEnumNameSafe) {
-  EXPECT_EQ(enumNameSafe((EnumNonStrict)0), "UNKNOWN");
-  EXPECT_EQ(enumNameSafe((EnumNonStrict)1), "VALUE");
-  EXPECT_EQ(enumNameSafe((EnumNonStrict)2), "FOO");
-  EXPECT_EQ(enumNameSafe((EnumNonStrict)3), "BAR");
-  EXPECT_EQ(enumNameSafe((EnumNonStrict)6), "NEW_VALUE");
-  EXPECT_EQ(enumNameSafe((EnumNonStrict)4), "4");
-  EXPECT_EQ(enumNameSafe((EnumNonStrict)5), "5");
 }
--- thrift/lib/cpp/util/test/enum_non_strict.thrift
+++
@@ -1,8 +0,0 @@
-enum EnumNonStrict {
-  UNKNOWN = 0,
-  VALUE = 1,
-  FOO = 2,
-  BAR = 3,
-  // DEPRECATED = 4,
-  NEW_VALUE = 6,
-}
