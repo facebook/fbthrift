0d528435f82c7c0b683108d6b87719167ecb4e37
Yedidya Feldblum <yfeldblum@fb.com>
2018-05-09 00:08:35 -0700

    Dead Code: TRpcTransportContext
    Summary: [Thrift] Dead Code: `TRpcTransportContext`.
    
    Reviewed By: spalamarchuk
    
    Differential Revision: D7893403
    
    fbshipit-source-id: f82a3b36a41ad94f6bdd8ea9735ffba10ec72baf

--- thrift/lib/cpp/server/TConnectionContext.h
+++ thrift/lib/cpp/server/TConnectionContext.h
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014 Facebook, Inc.
+ * Copyright 2014-present Facebook, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -43,7 +43,6 @@ class TConnectionContext {
     cleanupUserData();
   }
 
-  // expose getPeerAddress() defined in TRpcTransportContext
   virtual const folly::SocketAddress* getPeerAddress() const {
     return &peerAddress_;
   }
