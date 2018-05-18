38920676d80ebe11083887bbe8d64a3a1569b13a
Orvid King <cdykes@fb.com>
2018-05-17 21:00:53 -0700

    Use folly's FindGFlags
    Summary: Because it works properly on Windows, and isn't whatever this nonesense is it had previously.
    
    Reviewed By: yfeldblum
    
    Differential Revision: D8053782
    
    fbshipit-source-id: 29b49f9ccc0704c14f879f447c56b4cc84e2a02d

--- thrift/cmake/FindGFlags.cmake
+++ thrift/cmake/FindGFlags.cmake
@@ -1,50 +1,27 @@
-# - Try to find GFLAGS
 #
-# The following variables are optionally searched for defaults
-#  GFLAGS_ROOT_DIR:            Base directory where all GFLAGS components are found
+# Find libgflags
 #
-# The following are set after configuration is done:
-#  GFLAGS_FOUND
-#  GFLAGS_INCLUDE_DIRS
-#  GFLAGS_LIBRARIES
-#  GFLAGS_LIBRARYRARY_DIRS
-
-include(FindPackageHandleStandardArgs)
-
-set(GFLAGS_ROOT_DIR "" CACHE PATH "Folder contains Gflags")
-
-# We are testing only a couple of files in the include directories
-if(WIN32)
-    find_path(GFLAGS_INCLUDE_DIR gflags/gflags.h
-        PATHS ${GFLAGS_ROOT_DIR}/src/windows)
-else()
-    find_path(GFLAGS_INCLUDE_DIR gflags/gflags.h
-        PATHS ${GFLAGS_ROOT_DIR})
-endif()
-
-if(MSVC)
-    find_library(GFLAGS_LIBRARY_RELEASE
-        NAMES libgflags
-        PATHS ${GFLAGS_ROOT_DIR}
-        PATH_SUFFIXES Release)
-
-    find_library(GFLAGS_LIBRARY_DEBUG
-        NAMES libgflags-debug
-        PATHS ${GFLAGS_ROOT_DIR}
-        PATH_SUFFIXES Debug)
-
-    set(GFLAGS_LIBRARY optimized ${GFLAGS_LIBRARY_RELEASE} debug ${GFLAGS_LIBRARY_DEBUG})
-else()
-    find_library(GFLAGS_LIBRARY gflags)
-endif()
-
-find_package_handle_standard_args(GFlags DEFAULT_MSG GFLAGS_INCLUDE_DIR GFLAGS_LIBRARY)
+#  LIBGFLAGS_INCLUDE_DIR - where to find gflags/gflags.h, etc.
+#  LIBGFLAGS_LIBRARY     - List of libraries when using libgflags.
+#  LIBGFLAGS_FOUND       - True if libgflags found.
 
 
-if(GFLAGS_FOUND)
-    set(GFLAGS_INCLUDE_DIRS ${GFLAGS_INCLUDE_DIR})
-    set(GFLAGS_LIBRARIES ${GFLAGS_LIBRARY})
-    message(STATUS "Found gflags  (include: ${GFLAGS_INCLUDE_DIR}, library: ${GFLAGS_LIBRARY})")
-    mark_as_advanced(GFLAGS_LIBRARY_DEBUG GFLAGS_LIBRARY_RELEASE
-                     GFLAGS_LIBRARY GFLAGS_INCLUDE_DIR GFLAGS_ROOT_DIR)
-endif()
+IF (LIBGFLAGS_INCLUDE_DIR)
+  # Already in cache, be silent
+  SET(LIBGFLAGS_FIND_QUIETLY TRUE)
+ENDIF ()
+
+FIND_PATH(LIBGFLAGS_INCLUDE_DIR gflags/gflags.h)
+
+FIND_LIBRARY(LIBGFLAGS_LIBRARY_DEBUG NAMES gflagsd gflags_staticd)
+FIND_LIBRARY(LIBGFLAGS_LIBRARY_RELEASE NAMES gflags gflags_static)
+
+INCLUDE(SelectLibraryConfigurations)
+SELECT_LIBRARY_CONFIGURATIONS(LIBGFLAGS)
+
+# handle the QUIETLY and REQUIRED arguments and set LIBGFLAGS_FOUND to TRUE if
+# all listed variables are TRUE
+INCLUDE(FindPackageHandleStandardArgs)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(LIBGFLAGS DEFAULT_MSG LIBGFLAGS_LIBRARY LIBGFLAGS_INCLUDE_DIR)
+
+MARK_AS_ADVANCED(LIBGFLAGS_LIBRARY LIBGFLAGS_INCLUDE_DIR)
