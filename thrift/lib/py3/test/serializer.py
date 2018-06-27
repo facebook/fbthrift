e673de2dc049691307f17e82ce478b8825e17e6d
Jason Fried <fried@fb.com>
2018-06-26 17:05:52 -0700

    fix serialization tests, also provide fixtures for serialized output
    Summary:
    Fixed test, for some reason we were shadowing the argument list.
    Also added fixtures so we know right away if we broke/changed serialization.
    
    Reviewed By: jreese
    
    Differential Revision: D8626327
    
    fbshipit-source-id: 04620afd801b699781212b29b26efbf215d13869

--- thrift/lib/py3/test/serializer.py
+++ thrift/lib/py3/test/serializer.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 import unittest
 import pickle
+
+from typing import Mapping
 from thrift.py3 import serialize, deserialize, Protocol, Struct, Error
 
 from testing.types import easy, hard, Integers
@@ -10,24 +12,24 @@ class SerializerTests(unittest.TestCase):
 
     def test_bad_deserialize(self) -> None:
         with self.assertRaises(Error):
-            deserialize(easy, b'', protocol=Protocol.JSON)
+            deserialize(easy, b"", protocol=Protocol.JSON)
         with self.assertRaises(Error):
-            deserialize(easy, b'\x05AAAAAAAA')
+            deserialize(easy, b"\x05AAAAAAAA")
         with self.assertRaises(Error):
-            deserialize(easy, b'\x02\xDE\xAD\xBE\xEF', protocol=Protocol.BINARY)
+            deserialize(easy, b"\x02\xDE\xAD\xBE\xEF", protocol=Protocol.BINARY)
 
-    def thrift_serialization_round_robin(self, control: Struct) -> None:
-        control = easy(val=5, val_list=[1, 2, 3, 4])
-
+    def thrift_serialization_round_robin(
+        self, control: Struct, fixtures: Mapping[Protocol, bytes]
+    ) -> None:
         for proto in Protocol:
             encoded = serialize(control, protocol=proto)
             self.assertIsInstance(encoded, bytes)
             decoded = deserialize(type(control), encoded, protocol=proto)
             self.assertIsInstance(decoded, type(control))
             self.assertEqual(control, decoded)
+            self.assertEqual((proto, encoded), (proto, fixtures.get(proto)))
 
     def pickle_round_robin(self, control: Struct) -> None:
-        control = easy(val=0, val_list=[5, 6, 7])
         encoded = pickle.dumps(control, protocol=pickle.HIGHEST_PROTOCOL)
         decoded = pickle.loads(encoded)
         self.assertIsInstance(decoded, type(control))
@@ -35,7 +37,14 @@ class SerializerTests(unittest.TestCase):
 
     def test_serialize_easy_struct(self) -> None:
         control = easy(val=5, val_list=[1, 2, 3, 4])
-        self.thrift_serialization_round_robin(control)
+        fixtures: Mapping[Protocol, bytes] = {
+            Protocol.COMPACT: b"\x15\n\x19E\x02\x04\x06\x08,\x00\x00",
+            Protocol.BINARY: b"\x08\x00\x01\x00\x00\x00\x05\x0f\x00\x02\x08\x00\x00\x00"
+            b"\x04\x00\x00\x00\x01\x00\x00\x00\x02\x00\x00\x00\x03\x00"
+            b"\x00\x00\x04\x0c\x00\x04\x00\x00",
+            Protocol.JSON: b'{"val":5,"val_list":[1,2,3,4],"an_int":{}}',
+        }
+        self.thrift_serialization_round_robin(control, fixtures)
 
     def test_pickle_easy_struct(self) -> None:
         control = easy(val=0, val_list=[5, 6, 7])
@@ -43,19 +52,36 @@ class SerializerTests(unittest.TestCase):
 
     def test_serialize_hard_struct(self) -> None:
         control = hard(
-            val=0, val_list=[1, 2, 3, 4], name='foo', an_int=Integers(tiny=1)
+            val=0, val_list=[1, 2, 3, 4], name="foo", an_int=Integers(tiny=1)
         )
-        self.thrift_serialization_round_robin(control)
+        fixtures: Mapping[Protocol, bytes] = {
+            Protocol.COMPACT: b"\x15\x00\x19E\x02\x04\x06\x08\x18\x03foo\x1c\x13\x01"
+            b"\x00\x18\x0csome default\x00",
+            Protocol.BINARY: b"\x08\x00\x01\x00\x00\x00\x00\x0f\x00\x02\x08\x00\x00\x00"
+            b"\x04\x00\x00\x00\x01\x00\x00\x00\x02\x00\x00\x00\x03\x00"
+            b"\x00\x00\x04\x0b\x00\x03\x00\x00\x00\x03foo\x0c\x00\x04"
+            b"\x03\x00\x01\x01\x00\x0b\x00\x05\x00\x00\x00\x0csome def"
+            b"ault\x00",
+            Protocol.JSON: b'{"val":0,"val_list":[1,2,3,4],"name":"foo","an_int":{"tiny'
+            b'":1},"other":"some default"}',
+        }
+        self.thrift_serialization_round_robin(control, fixtures)
 
     def test_pickle_hard_struct(self) -> None:
         control = hard(
-            val=0, val_list=[1, 2, 3, 4], name='foo', an_int=Integers(tiny=1)
+            val=0, val_list=[1, 2, 3, 4], name="foo", an_int=Integers(tiny=1)
         )
         self.pickle_round_robin(control)
 
     def test_serialize_Integers_union(self) -> None:
         control = Integers(medium=1337)
-        self.thrift_serialization_round_robin(control)
+        fixtures: Mapping[Protocol, bytes] = {
+            Protocol.COMPACT: b"5\xf2\x14\x00",
+            Protocol.BINARY: b"\x08\x00\x03\x00\x00\x059\x00",
+            Protocol.JSON: b'{"medium":1337}',
+        }
+
+        self.thrift_serialization_round_robin(control, fixtures)
 
     def test_pickle_Integers_union(self) -> None:
         control = Integers(large=2 ** 32)
