e6bdbedb6b335462c5a019f7270af2e6fde9a868
Lee Howes <lwh@fb.com>
2018-08-04 15:40:14 -0700

    Future<T>::then 6/n: Future<T>::then() & -> Future<T>::then() &&.
    Summary:
    Overall plan to modify Future<T>::then to be r-value qualified and use Future<T>::thenTry or Future<T>::thenValue.
    
    The goal is to disambiguate folly::Future and to improve type and lifetime safety of Future and its methods.
    
    6/n: Codemod rvalue-future<T>.then(...) to rvalue-future<T>.then(...).
    
    Reviewed By: alandau
    
    Differential Revision: D9152351
    
    fbshipit-source-id: 366d099a0b88f84f9e4786a7eb387b14ed7dd5f0

--- thrift/lib/cpp2/async/Cpp2Channel.cpp
+++ thrift/lib/cpp2/async/Cpp2Channel.cpp
@@ -229,7 +229,7 @@ void Cpp2Channel::sendMessage(
   DestructorGuard dg(this);
 
   auto future = pipeline_->write(std::make_pair(std::move(buf), header));
-  future.then([this, dg](folly::Try<folly::Unit>&& t) {
+  std::move(future).then([this, dg](folly::Try<folly::Unit>&& t) {
     if (t.withException<TTransportException>(
             [&](const TTransportException& ex) { writeError(0, ex); }) ||
         t.withException<std::exception>([&](const std::exception& ex) {
