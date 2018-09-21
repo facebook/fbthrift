5c8acb80ea86ddf4366fbfaff89b7f79940d593a
Victor Zverovich <viz@fb.com>
2018-09-20 20:47:55 -0700

    Add field_ref class that represents a field reference
    Summary:
    This diff adds `field_ref<T>` and `optional_field_ref<T>` types that represent
    references to an unqualified and optional fields of type `T` in a Thrift-
    generated C++ struct respectively. The idea is to use them in the new C++ Thrift
    structs API replacinng direct access to member variables as well as wrapping
    fields in `folly::Optional`. Here's a simplified example:
    
    ```
    class TestStruct {
     public:
      field_ref<std::string> name() {
        return {name_, __isset.name};
      }
    
      field_ref<const std::string> name() const {
        return {name_, __isset.name};
      }
    
     private:
      std::string name_;
    
      struct __isset {
        bool name;
      } __isset = {};
    };
    ```
    
    This will provide the following benefits:
    
    * Preventing unsafe use where an optional field is assigned directly without
      updating the `__isset` flag:
    
      ```
      // Old API:
      TestStruct t;
      t.name = "foo";
      auto name = t.get_name(); // name == nullptr
    
      // New API:
      t.name() = "foo"; // __isset is updated automatically
      ```
    
    * More compact memory representation compared to wrapping fields in
      `folly::Optional` due to reduced padding. This also enables bit packing of
      isset fields in the future.
    
    * Less generated code as there will be no need for setters.
    
    * Unified API: `field_ref` can replace both the default API and the `optionals`
      one.
    
    Reviewed By: spalamarchuk
    
    Differential Revision: D9614416
    
    fbshipit-source-id: 389f534072c59979bb2fbb9605cf96086a22ada1

---
+++ thrift/lib/cpp2/gen/field_ref.h
@@ -0,0 +1,144 @@
+/*
+ * Copyright 2018-present Facebook, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#include <type_traits>
+#include <utility>
+
+#include <folly/CPortability.h>
+#include <folly/Likely.h>
+
+// Let the optimizer remove these artificial functions completely.
+#define THRIFT_NOLINK FOLLY_ALWAYS_INLINE FOLLY_ATTR_VISIBILITY_HIDDEN
+
+namespace apache {
+namespace thrift {
+namespace detail {
+
+template <typename T>
+using is_set_t = std::conditional_t<std::is_const<T>::value, const bool, bool>;
+
+[[noreturn]] void throw_on_bad_field_access();
+
+} // namespace detail
+
+// A reference to an unqualified field of the possibly const-qualified type T in
+// a Thrift-generated struct.
+template <typename T>
+class field_ref {
+ public:
+  using value_type = T;
+
+  THRIFT_NOLINK field_ref(T& value, detail::is_set_t<T>& is_set) noexcept
+      : value_(value), is_set_(is_set) {}
+
+  THRIFT_NOLINK operator field_ref<const T>() noexcept {
+    return {value_, is_set_};
+  }
+
+  template <typename U>
+  THRIFT_NOLINK std::enable_if_t<std::is_assignable<T&, U>::value, field_ref&>
+  operator=(U&& value) noexcept(std::is_nothrow_assignable<T&, U>::value) {
+    value_ = std::forward<U>(value);
+    is_set_ = true;
+    return *this;
+  }
+
+  THRIFT_NOLINK bool is_set() const noexcept {
+    return is_set_;
+  }
+
+  // Returns a reference to the value.
+  THRIFT_NOLINK T& value() const noexcept {
+    return value_;
+  }
+
+  THRIFT_NOLINK T& operator*() const noexcept {
+    return value_;
+  }
+
+  THRIFT_NOLINK T* operator->() const noexcept {
+    return &value_;
+  }
+
+ private:
+  T& value_;
+  detail::is_set_t<T>& is_set_;
+};
+
+// A reference to an optional field of the possibly const-qualified type T in a
+// Thrift-generated struct.
+template <typename T>
+class optional_field_ref {
+ public:
+  using value_type = T;
+
+  THRIFT_NOLINK optional_field_ref(
+      T& value,
+      detail::is_set_t<T>& is_set) noexcept
+      : value_(value), is_set_(is_set) {}
+
+  THRIFT_NOLINK operator optional_field_ref<const T>() noexcept {
+    return {value_, is_set_};
+  }
+
+  template <typename U>
+  THRIFT_NOLINK
+      std::enable_if_t<std::is_assignable<T&, U>::value, optional_field_ref&>
+      operator=(U&& value) noexcept(std::is_nothrow_assignable<T&, U>::value) {
+    value_ = std::forward<U>(value);
+    is_set_ = true;
+    return *this;
+  }
+
+  THRIFT_NOLINK bool has_value() const noexcept {
+    return is_set_;
+  }
+
+  THRIFT_NOLINK explicit operator bool() const noexcept {
+    return is_set_;
+  }
+
+  // Returns a reference to the value if this optional_field_ref has one; throws
+  // bad_field_access otherwise.
+  THRIFT_NOLINK T& value() const {
+    if (FOLLY_UNLIKELY(!is_set_)) {
+      detail::throw_on_bad_field_access();
+    }
+    return value_;
+  }
+
+  // Returns a reference to the value without checking whether it is available.
+  THRIFT_NOLINK T& value_unchecked() const {
+    return value_;
+  }
+
+  THRIFT_NOLINK T& operator*() const {
+    return value();
+  }
+
+  THRIFT_NOLINK T* operator->() const {
+    return &value();
+  }
+
+ private:
+  T& value_;
+  detail::is_set_t<T>& is_set_;
+};
+
+} // namespace thrift
+} // namespace apache
---
+++ thrift/test/field_ref_test.cpp
@@ -0,0 +1,261 @@
+/*
+ * Copyright 2018-present Facebook, Inc.
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+#include <string>
+#include <type_traits>
+
+#include <thrift/lib/cpp2/gen/bad_field_access.h>
+#include <thrift/lib/cpp2/gen/field_ref.h>
+
+#include <gtest/gtest.h>
+
+using apache::thrift::bad_field_access;
+using apache::thrift::field_ref;
+using apache::thrift::optional_field_ref;
+
+// A struct which is assignable but not constructible from int or other types
+// to test forwarding in field_ref::operator=.
+struct IntAssignable {
+  IntAssignable& operator=(int v) noexcept {
+    value = v;
+    return *this;
+  }
+
+  int value = 0;
+};
+
+// A struct assignable from std::string with a non-noexcept assignment operator
+// to test conditional noexcept in field_ref::operator=.
+struct StringAssignable {
+  StringAssignable& operator=(const std::string& v) {
+    value = v;
+    return *this;
+  }
+
+  std::string value;
+};
+
+// A struct constructible from std::string with a non-noexcept ctor
+// to test conditional noexcept in field_ref::emplace.
+struct StringConstructible {
+  explicit StringConstructible(const std::string& v = {}) {
+    value = v;
+  }
+
+  std::string value;
+};
+
+class TestStruct {
+ public:
+  field_ref<std::string> name() {
+    return {name_, __isset.name};
+  }
+
+  field_ref<const std::string> name() const {
+    return {name_, __isset.name};
+  }
+
+  optional_field_ref<std::string> opt_name() {
+    return {name_, __isset.name};
+  }
+
+  field_ref<IntAssignable> int_assign() {
+    return {int_assign_, __isset.int_assign};
+  }
+
+  optional_field_ref<IntAssignable> opt_int_assign() {
+    return {int_assign_, __isset.int_assign};
+  }
+
+  optional_field_ref<StringConstructible> scons() {
+    return {scons_, __isset.scons};
+  }
+
+ private:
+  std::string name_ = "default";
+  IntAssignable int_assign_;
+  StringConstructible scons_;
+
+  struct __isset {
+    bool name;
+    bool int_assign;
+    bool scons;
+  } __isset = {};
+};
+
+TEST(field_ref_test, access_default_value) {
+  auto s = TestStruct();
+  EXPECT_EQ(*s.name(), "default");
+}
+
+TEST(field_ref_test, assign) {
+  auto s = TestStruct();
+  EXPECT_FALSE(s.name().is_set());
+  EXPECT_EQ(*s.name(), "default");
+  s.name() = "foo";
+  EXPECT_TRUE(s.name().is_set());
+  EXPECT_EQ(*s.name(), "foo");
+}
+
+template <template <typename> class FieldRef>
+void check_is_assignable() {
+  using IntAssignableRef = FieldRef<IntAssignable>;
+  static_assert(std::is_assignable<IntAssignableRef, int>::value, "");
+  static_assert(!std::is_assignable<IntAssignableRef, std::string>::value, "");
+  static_assert(std::is_nothrow_assignable<IntAssignableRef, int>::value, "");
+
+  using StringAssignableRef = FieldRef<StringAssignable>;
+  static_assert(
+      !std::is_nothrow_assignable<StringAssignableRef, int>::value, "");
+}
+
+TEST(field_ref_test, is_assignable) {
+  check_is_assignable<field_ref>();
+}
+
+TEST(field_ref_test, assign_forwards) {
+  auto s = TestStruct();
+  s.int_assign() = 42;
+  EXPECT_TRUE(s.int_assign().is_set());
+  EXPECT_EQ(s.int_assign()->value, 42);
+}
+
+TEST(field_ref_test, construct_const_from_mutable) {
+  auto s = TestStruct();
+  s.name() = "foo";
+  field_ref<std::string> name = s.name();
+  field_ref<const std::string> const_name = name;
+  EXPECT_TRUE(const_name.is_set());
+  EXPECT_EQ(*const_name, "foo");
+}
+
+template <typename T>
+constexpr bool is_const_ref() {
+  return std::is_reference<T>::value &&
+      std::is_const<std::remove_reference_t<T>>::value;
+}
+
+TEST(field_ref_test, const_accessors) {
+  TestStruct s;
+  s.name() = "bar";
+  field_ref<const std::string> name = s.name();
+  EXPECT_EQ(*name, "bar");
+  EXPECT_EQ(name.value(), "bar");
+  EXPECT_EQ(name->size(), 3);
+  static_assert(is_const_ref<decltype(*name)>(), "");
+  static_assert(is_const_ref<decltype(name.value())>(), "");
+  static_assert(is_const_ref<decltype(*name.operator->())>(), "");
+}
+
+TEST(field_ref_test, mutable_accessors) {
+  TestStruct s;
+  field_ref<std::string> name = s.name();
+  *name = "foo";
+  EXPECT_EQ(*name, "foo");
+  name.value() = "bar";
+  EXPECT_EQ(*name, "bar");
+  name->assign("baz");
+  EXPECT_EQ(*name, "baz");
+
+  // Field is not marked as set but that's OK for unqualified field.
+  EXPECT_FALSE(name.is_set());
+}
+
+TEST(optional_field_ref_test, access_default_value) {
+  auto s = TestStruct();
+  EXPECT_THROW(*s.opt_name(), bad_field_access);
+  EXPECT_EQ(s.opt_name().value_unchecked(), "default");
+}
+
+TEST(optional_field_ref_test, assign) {
+  auto s = TestStruct();
+  EXPECT_FALSE(s.opt_name().has_value());
+  s.opt_name() = "foo";
+  EXPECT_TRUE(s.opt_name().has_value());
+  EXPECT_EQ(*s.opt_name(), "foo");
+}
+
+TEST(optional_field_ref_test, is_assignable) {
+  check_is_assignable<optional_field_ref>();
+}
+
+TEST(optional_field_ref_test, assign_forwards) {
+  auto s = TestStruct();
+  s.opt_int_assign() = 42;
+  EXPECT_TRUE(s.opt_int_assign().has_value());
+  EXPECT_EQ(s.opt_int_assign()->value, 42);
+}
+
+TEST(optional_field_ref_test, construct_const_from_mutable) {
+  auto s = TestStruct();
+  s.opt_name() = "foo";
+  optional_field_ref<std::string> name = s.opt_name();
+  optional_field_ref<const std::string> const_name = name;
+  EXPECT_TRUE(const_name.has_value());
+  EXPECT_EQ(*const_name, "foo");
+}
+
+TEST(optional_field_ref_test, const_accessors) {
+  TestStruct s;
+  s.opt_name() = "bar";
+  optional_field_ref<const std::string> name = s.opt_name();
+  EXPECT_EQ(*name, "bar");
+  EXPECT_EQ(name.value(), "bar");
+  EXPECT_EQ(name->size(), 3);
+  static_assert(is_const_ref<decltype(*name)>(), "");
+  static_assert(is_const_ref<decltype(name.value())>(), "");
+  static_assert(is_const_ref<decltype(*name.operator->())>(), "");
+}
+
+TEST(optional_field_ref_test, mutable_accessors) {
+  TestStruct s;
+  s.opt_name() = "initial";
+  optional_field_ref<std::string> name = s.opt_name();
+  *name = "foo";
+  EXPECT_EQ(*name, "foo");
+  name.value() = "bar";
+  EXPECT_EQ(*name, "bar");
+  name->assign("baz");
+  EXPECT_EQ(*name, "baz");
+  EXPECT_TRUE(name.has_value());
+}
+
+TEST(optional_field_ref_test, bad_field_access) {
+  TestStruct s;
+  optional_field_ref<std::string> name = s.opt_name();
+  EXPECT_THROW(*name = "foo", bad_field_access);
+  EXPECT_THROW(name.value() = "bar", bad_field_access);
+  EXPECT_THROW(name->assign("baz"), bad_field_access);
+}
+
+TEST(optional_field_ref_test, convert_to_bool) {
+  TestStruct s;
+  if (auto name = s.opt_name()) {
+    EXPECT_TRUE(false);
+  }
+  s.opt_name() = "foo";
+  if (auto name = s.opt_name()) {
+    // Do nothing.
+  } else {
+    EXPECT_TRUE(false);
+  }
+  EXPECT_FALSE((std::is_convertible<decltype(s.opt_name()), bool>::value));
+}
