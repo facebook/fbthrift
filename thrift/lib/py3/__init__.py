c9311399b622f97f8a005106fec1703cff3ff54f
Jason Fried <fried@fb.com>
2018-10-18 17:39:41 -0700

    Fixed up __init__.py not to hide transitive import failures
    Summary:
    These try/except blocks were ment to catch the issue of a particualar
    compiled module not existing at runtime, not to catch transitive import issues
    because some linked shared.so is missing in the environment but the compiled
    python module is pressent.
    
    Reviewed By: ambv
    
    Differential Revision: D10451305
    
    fbshipit-source-id: ffee81827cd4b1fbbd1644fc8b177f88160858f2

--- thrift/lib/py3/__init__.py
+++ thrift/lib/py3/__init__.py
@@ -6,7 +6,7 @@ __all__ = []  # type: List[str]
 try:
     from thrift.py3.client import get_client, Client
     __all__.extend(['Client', 'get_client'])
-except ImportError:
+except ModuleNotFoundError:
     pass
 
 try:
@@ -14,29 +14,29 @@ try:
         ThriftServer, SSLPolicy, pass_context, RequestContext
     )
     __all__.extend(['ThriftServer', 'pass_context', 'SSLPolicy', 'RequestContext'])
-except ImportError:
+except ModuleNotFoundError:
     pass
 
 try:
     from thrift.py3.types import Struct, BadEnum, Union, Enum, Flag
     __all__.extend(['Struct', 'BadEnum', 'Union', 'Enum', 'Flag'])
-except ImportError:
+except ModuleNotFoundError:
     pass
 
 try:
     from thrift.py3.exceptions import Error, ApplicationError, TransportError, ProtocolError
     __all__.extend(['Error', 'ApplicationError', 'TransportError', 'ProtocolError'])
-except ImportError:
+except ModuleNotFoundError:
     pass
 
 try:
     from thrift.py3.serializer import Protocol, serialize, deserialize
     __all__.extend(['Protocol', 'serialize', 'deserialize'])
-except ImportError:
+except ModuleNotFoundError:
     pass
 
 try:
     from thrift.py3.common import Priority, RpcOptions
     __all__.extend(['Priority', 'RpcOptions'])
-except ImportError:
+except ModuleNotFoundError:
     pass
