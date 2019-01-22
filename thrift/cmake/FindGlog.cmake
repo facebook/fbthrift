2c95930176bdd2a528bfce5e474727a5ed2bc798
Wez Furlong <wez@fb.com>
2019-01-22 12:13:18 -0800

    fix detection and dep on glog::glog when building via cmake
    Summary:
    folly exports a dependency on `glog::glog`.  That causes dependent packages to also depend implicitly on glog::glog with no way to resolve what that means, resulting in errors like this:
    
    ```
    CMake Error at CMakeLists.txt:106 (add_library):
      Target "wangle" links to target "glog::glog" but the target was not found.
      Perhaps a find_package() call is missing for an IMPORTED target, or an
      ALIAS target is missing?
    ```
    
    We cannot rely on `glog` having installed its configuration for a simple `find_package(glog CONFIG)`; the version of `glog` provided by some versions of ubuntu doesn't install a `.cmake` file for this.
    
    That means that we need to detect it for ourselves.
    
    This diff enhances the `FindGlog.cmake` script to define a `glog::glog` target so that we can consistently depend upon it throughout the set of related packages.
    
    It is unfortunate that we have duplication of the `FindGlog.cmake` files across these projects; a follow up diff could place these in eg: fbcode_builder and arrange to have shipit to copy them to the right place at shipit time.
    
    Reviewed By: strager
    
    Differential Revision: D13482288
    
    fbshipit-source-id: d19716871edce29019114030e4c82911ac345adb

--- thrift/cmake/FindGlog.cmake
+++ thrift/cmake/FindGlog.cmake
@@ -1,29 +1,31 @@
-# Find Glog
+# - Try to find Glog
+# Once done, this will define
 #
-# This will define:
-#  GLOG_FOUND
-#  GLOG_INCLUDE_DIRS
-#  GLOG_LIBRARIES
-
-find_path(
-  GLOG_INCLUDE_DIRS glog/logging.h
-  HINTS
-      $ENV{GLOG_ROOT}/include
-      ${GLOG_ROOT}/include
-)
-
-find_library(
-  GLOG_LIBRARIES glog
-  HINTS
-      $ENV{GLOG_ROOT}/src
-      ${GLOG_ROOT}/src
-)
-
-mark_as_advanced(GLOG_INCLUDE_DIRS GLOG_LIBRARIES)
+# GLOG_FOUND - system has Glog
+# GLOG_INCLUDE_DIRS - the Glog include directories
+# GLOG_LIBRARIES - link these to use Glog
 
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(Glog GLOG_INCLUDE_DIRS GLOG_LIBRARIES)
 
-if(GLOG_FOUND AND NOT GLOG_FIND_QUIETLY)
-    message(STATUS "GLOG: ${GLOG_INCLUDE_DIRS}")
+find_library(GLOG_LIBRARY glog
+  PATHS ${GLOG_LIBRARYDIR})
+
+find_path(GLOG_INCLUDE_DIR glog/logging.h
+  PATHS ${GLOG_INCLUDEDIR})
+
+find_package_handle_standard_args(glog DEFAULT_MSG
+  GLOG_LIBRARY
+  GLOG_INCLUDE_DIR)
+
+mark_as_advanced(
+  GLOG_LIBRARY
+  GLOG_INCLUDE_DIR)
+
+set(GLOG_LIBRARIES ${GLOG_LIBRARY})
+set(GLOG_INCLUDE_DIRS ${GLOG_INCLUDE_DIR})
+
+if (NOT TARGET glog::glog)
+  add_library(glog::glog UNKNOWN IMPORTED)
+  set_target_properties(glog::glog PROPERTIES INTERFACE_INCLUDE_DIRECTORIES "${GLOG_INCLUDE_DIRS}")
+  set_target_properties(glog::glog PROPERTIES IMPORTED_LINK_INTERFACE_LANGUAGES "C" IMPORTED_LOCATION "${GLOG_LIBRARIES}")
 endif()
