55b2686fe2b3064df8a11e767577e8e40deeaf67
Nate Mueller <nmueller@fb.com>
2019-02-13 11:32:20 -0800

    Add Facebook copyright headers to all files
    Reviewed By: fiorix
    
    Differential Revision: D14068006
    
    fbshipit-source-id: 7adf4e661cc8a58181292aaed2870faba3c43931

--- thrift/lib/go/thrift/header_protocol_test.go
+++ thrift/lib/go/thrift/header_protocol_test.go
@@ -1,4 +1,6 @@
 /*
+ * Copyright 2019-present Facebook, Inc.
+ *
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements. See the NOTICE file
  * distributed with this work for additional information
@@ -16,7 +18,6 @@
  * specific language governing permissions and limitations
  * under the License.
  */
-
 package thrift
 
 import "testing"
