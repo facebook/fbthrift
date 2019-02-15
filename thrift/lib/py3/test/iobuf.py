40b71433e4431da4f6ac4a541c7db78eb019a327
Chad Austin <chadaustin@fb.com>
2019-02-15 13:38:32 -0800

    re-enable typechecking on iobuf tests
    Summary:
    I noticed that `buck test thrift/lib/...` failed because the typecheck
    target on the iobuf tests was missing. Re-enable type checking and
    disable the specific typecheck that failed.
    
    Reviewed By: yfeldblum
    
    Differential Revision: D14096798
    
    fbshipit-source-id: 273a7271854aa9981b273c7bdc930e9c2c0640d2

--- thrift/lib/py3/test/iobuf.py
+++ thrift/lib/py3/test/iobuf.py
@@ -3,31 +3,30 @@ import unittest
 
 from folly.iobuf import IOBuf
 from iobuf.types import Moo
-# pyre-ignore: T32805591
 from .iobuf_helper import get_empty_chain, make_chain
 
 class IOBufTests(unittest.TestCase):
     def test_empty_chain(self) -> None:
-        ebuf = get_empty_chain()  # pyre-ignore: T32805591
+        ebuf = get_empty_chain()
         self.assertFalse(ebuf)
         self.assertTrue(ebuf.is_chained)
         self.assertEqual(len(ebuf), 0)
         self.assertEqual(ebuf.chain_size(), 0)
         self.assertEqual(ebuf.chain_count(), 8)
-        self.assertEqual(b''.join(ebuf), b'')
+        self.assertEqual(b''.join(ebuf), b'')  # pyre-ignore: T32805991
         self.assertEqual(b'', bytes(ebuf))
 
     def test_chain(self) -> None:
         control = [b'facebook', b'thrift', b'python3', b'cython']
-        chain = make_chain([IOBuf(x) for x in control])  # pyre-ignore: T32805591
+        chain = make_chain([IOBuf(x) for x in control])
         self.assertTrue(chain.is_chained)
         self.assertTrue(chain)
         self.assertEqual(bytes(chain), control[0])
         self.assertEqual(len(chain), len(control[0]))
         self.assertEqual(chain.chain_size(), sum(len(x) for x in control))
         self.assertEqual(chain.chain_count(), len(control))
-        self.assertEqual(memoryview(chain.next), control[1])
-        self.assertEqual(b''.join(chain), b''.join(control))
+        self.assertEqual(memoryview(chain.next), control[1])  # type: ignore
+        self.assertEqual(b''.join(chain), b''.join(control))  # pyre-ignore: T32805991
 
     def test_hash(self) -> None:
         x = b"omg"
