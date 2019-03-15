06aaf1fb6873f2cab51edac36977aa033fe3c3d3
Yuhan Hao <yuhanhao@fb.com>
2019-03-15 14:16:45 -0700

    Add test cases for future build options legacy_generate_beans
    Summary: tsia, in this diff, we add a new test fixture for future option: `legacy_generate_beans`.
    
    Reviewed By: stevegury
    
    Differential Revision: D14450968
    
    fbshipit-source-id: fd0ecc460c22db3d20f442209df2678cf67c3038

---
+++ thrift/compiler/test/fixtures/basic-swift-bean/src/module.thrift
@@ -0,0 +1,36 @@
+namespace java.swift test.fixtures.basic_swift_bean
+
+enum MyEnum {
+  MyValue1 = 0,
+  MyValue2 = 1,
+  DOMAIN = 2,
+}
+
+struct MyStruct1 {
+  1: i64 MyIntField,
+  2: string MyStringField,
+  # use the type before it is defined. Thrift should be able to handle this
+  3: MyDataItem MyDataField,
+  # glibc has macros with this name, Thrift should be able to prevent collisions
+  4: i64 major (cpp.name = "majorVer"),
+}
+
+struct MyStruct2 {
+  1: MyStruct1 myStruct1,
+  2: string myString,
+}
+
+struct MyDataItem {
+  1: i32 field1,
+  2: i32 field2,
+}
+
+union MyUnion1 {
+  1: i32 i
+  2: double d
+}
+
+union MyUnion2 {
+  1: MyUnion1 u,
+  2: MyStruct1 s,
+}
