5a538d10e9ac56d84cdb2ea540bc47819185780a
Fred Emmott <fe@fb.com>
2019-03-27 08:44:05 -0700

    Revert D13966702: [fbcode] AsyncServerSocket::AcceptCallback::connectionAccepted to NetworkSocket
    Differential Revision:
    D13966702
    
    Original commit changeset: 415622dc347d
    
    fbshipit-source-id: 11ff9cac08174cfaefe20e7e4c5e08dc005aaa39

--- thrift/lib/cpp/test/TAsyncSocketCorkingTest.cpp
+++ thrift/lib/cpp/test/TAsyncSocketCorkingTest.cpp
@@ -46,10 +46,8 @@ class AcceptCallback : public AsyncServerSocket::AcceptCallback {
   explicit AcceptCallback(const std::function<void(int)>& fn) : fn_(fn) {}
 
   void connectionAccepted(
-      folly::NetworkSocket fdNetworkSocket,
+      int fd,
       const folly::SocketAddress&) noexcept override {
-    int fd = fdNetworkSocket.toFd();
-
     fn_(fd);
   }
   void acceptError(const std::exception& ex) noexcept override {
