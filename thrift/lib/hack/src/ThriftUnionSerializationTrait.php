30b15ee2eb07b36b73eb94314363446dfbe52146
Rodrigo Muñoz <rmk@fb.com>
2019-04-18 15:27:04 -0700

    Sync ThriftUnionSerializationTrait
    Reviewed By: DavidSnider
    
    Differential Revision: D14980934
    
    fbshipit-source-id: 6fe1979214c1711aab0eddbb50849a71549bc41e

--- thrift/lib/hack/src/ThriftUnionSerializationTrait.php
+++ thrift/lib/hack/src/ThriftUnionSerializationTrait.php
@@ -1,24 +1,21 @@
-<?hh // strict
+<?hh
 // Copyright 2004-present Facebook. All Rights Reserved.
 
 /**
  * Trait for Thrift Unions to call into the Serialization Helper
  */
 trait ThriftUnionSerializationTrait implements \IThriftStruct {
-  public function read(\TProtocol $protocol) : int {
+
+  public function read(\TProtocol $protocol): int {
     /* HH_IGNORE_ERROR[4053] every thrift union has a _type field */
     $type = $this->_type;
-    $ret = \ThriftSerializationHelper::readUnion(
-      $protocol,
-      $this,
-      &$type
-    );
+    $ret = \ThriftSerializationHelper::readUnion($protocol, $this, inout $type);
     /* HH_IGNORE_ERROR[4053] every thrift union has a _type field */
     $this->_type = $type;
     return $ret;
   }
 
-  public function write(\TProtocol $protocol) : int {
+  public function write(\TProtocol $protocol): int {
     // Writing Structs and Unions is the same procedure.
     return \ThriftSerializationHelper::writeStruct($protocol, $this);
   }
