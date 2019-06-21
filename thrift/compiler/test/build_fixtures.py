29a686f3f081e461c591500a92788010103ddea4
Steve Gury <stevegury@fb.com>
2019-06-21 08:33:15 -0700

    Fixtures: Apply codestyle on build_fixtures.py
    Summary: Apply codestyle formating on the file (which simplify the next diff).
    
    Reviewed By: yfeldblum
    
    Differential Revision: D15934155
    
    fbshipit-source-id: b048497867549562163cfeb6e09e81441e78f020

--- thrift/compiler/test/build_fixtures.py
+++ thrift/compiler/test/build_fixtures.py
@@ -2,12 +2,13 @@
 
 import argparse
 import asyncio
-import re
 import os
+import re
 import shlex
 import shutil
 import subprocess
 import sys
+
 
 """
 * Invoke using `buck run`:
@@ -85,41 +86,46 @@ def ascend_find_dir(path, target):
 
 def find_templates_rel(build_dir):
     if build_dir == BUCK_OUT:
-        return subprocess.check_output([
-            'buck',
-            'targets',
-            '--show-output',
-            '//thrift/compiler/generate/templates:templates',
-        ]).decode().split()[1]
+        cmd = [
+            "buck",
+            "targets",
+            "--show-output",
+            "//thrift/compiler/generate/templates:templates",
+        ]
+        return subprocess.check_output(cmd).decode().split()[1]
     else:
         return os.path.join(
-            build_dir, 'thrift/compiler/generate/templates/templates/templates')
+            build_dir, "thrift/compiler/generate/templates/templates/templates"
+        )
 
 
 def read_lines(path):
-    with open(path, 'r') as f:
+    with open(path, "r") as f:
         return f.readlines()
 
 
 args = parsed_args()
 build_dir = args.build_dir
 exe = os.path.join(os.getcwd(), sys.argv[0])
-thrift_rel = os.path.join(build_dir, 'thrift/compiler/thrift')
+thrift_rel = os.path.join(build_dir, "thrift/compiler/thrift")
 templates_rel = find_templates_rel(build_dir)
 templates = ascend_find_dir(exe, templates_rel)
 thrift = ascend_find_exe(exe, thrift_rel)
 
 if thrift is None:
-    sys.stderr.write(
-        "error: cannot find the Thrift compiler ({})\n".format(thrift_rel))
-    sys.stderr.write(
-        "(run `buck build thrift/compiler:thrift` to build it yourself)\n")
+    sys.stderr.write("error: cannot find the Thrift compiler ({})\n".format(thrift_rel))
+    sys.stderr.write("(run `buck build thrift/compiler:thrift` to build it yourself)\n")
     sys.exit(1)
-fixture_dir = ascend_find_dir(exe, 'thrift/compiler/test/fixtures')
-fixture_names = args.fixture_names if args.fixture_names is not None else sorted(
-    f
-    for f in os.listdir(fixture_dir)
-    if os.path.isfile(os.path.join(fixture_dir, f, 'cmd'))
+
+fixture_dir = ascend_find_dir(exe, "thrift/compiler/test/fixtures")
+fixture_names = (
+    args.fixture_names
+    if args.fixture_names is not None
+    else sorted(
+        f
+        for f in os.listdir(fixture_dir)
+        if os.path.isfile(os.path.join(fixture_dir, f, "cmd"))
+    )
 )
 
 has_errors = False
@@ -127,11 +133,7 @@ has_errors = False
 
 async def run_subprocess(cmd, *, cwd):
     p = await asyncio.create_subprocess_exec(
-        *cmd,
-        cwd=cwd,
-        close_fds=True,
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE,
+        *cmd, cwd=cwd, close_fds=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE
     )
     out, err = await p.communicate()
     sys.stdout.write(out.decode(sys.stdout.encoding))
@@ -140,19 +142,21 @@ async def run_subprocess(cmd, *, cwd):
         has_errors = True
         sys.stderr.write(err.decode(sys.stderr.encoding))
 
+
 processes = []
 
 msg_format = "Building fixture {{0:>{w}}}/{{1}}: {{2}}".format(
-    w=len(str(len(fixture_names))))
+    w=len(str(len(fixture_names)))
+)
 for name, index in zip(fixture_names, range(len(fixture_names))):
     msg = msg_format.format(index + 1, len(fixture_names), name)
     print(msg, file=sys.stderr)
     cwd = os.path.join(fixture_dir, name)
-    for fn in set(os.listdir(cwd)) - {'cmd', 'src'}:
-        if fn.startswith('.'):
+    for fn in set(os.listdir(cwd)) - {"cmd", "src"}:
+        if fn.startswith("."):
             continue
         shutil.rmtree(os.path.join(cwd, fn))
-    cmds = read_lines(os.path.join(cwd, 'cmd'))
+    cmds = read_lines(os.path.join(cwd, "cmd"))
     for cmd in cmds:
         if re.match(r"^\s*#", cmd):
             continue
@@ -163,13 +167,14 @@ for name, index in zip(fixture_names, range(len(fixture_names))):
             extra = "include_prefix=" + path
             join = "," if ":" in args[0] else ":"
             args[0] = args[0] + join + extra
-        if ("cpp2" in args[0] or "schema" in args[0] or "swift" in args[0]):
+        if "cpp2" in args[0] or "schema" in args[0] or "swift" in args[0]:
             # TODO: (yuhanhao) T41937765 When use generators that use
             # `t_mstch_objects` in recursive mode, if included thrift file
             # contains const structs or const union, generater will attempt to
             # de-reference a nullptr in `mstch_const_value::const_struct()`.
             # This is a hack before this is resolved.
-            base_args.remove('-r')
+            base_args.remove("-r")
+        xargs = base_args + args
         processes.append(run_subprocess(base_args + args, cwd=cwd))
 
 loop = asyncio.get_event_loop()
