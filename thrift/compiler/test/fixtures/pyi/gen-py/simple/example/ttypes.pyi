be5969bea89bc3e121f96a321aa641fc2b29bc2c
Shannon Zhu <szhu@fb.com>
2019-09-26 18:14:25 -0700

    Add pyre-unsafe header to ttypes thrift template
    Summary: Adding pyre-unsafe headers for generated file templates that reference typing.Any
    
    Reviewed By: dkgi
    
    Differential Revision: D17542463
    
    fbshipit-source-id: 6ea6048f5398983172b1c7da87e70adfe77d3e54

--- thrift/compiler/test/fixtures/pyi/gen-py/simple/example/ttypes.pyi
+++ thrift/compiler/test/fixtures/pyi/gen-py/simple/example/ttypes.pyi
@@ -4,6 +4,9 @@
 # DO NOT EDIT UNLESS YOU ARE SURE THAT YOU KNOW WHAT YOU ARE DOING
 #  @generated
 #
+
+# pyre-unsafe
+
 import typing as __T  # sometimes `t` is used as a field name
 
 from thrift import Thrift
