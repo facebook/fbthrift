1a79b09b2bdcccd00802162f846f8b06de967d8b
Andrii Grynenko <andrii@fb.com>
2019-10-18 08:45:29 -0700

    Add fixtures for client_buffered_stream
    Reviewed By: yfeldblum
    
    Differential Revision: D18001475
    
    fbshipit-source-id: e37d88fa09a3254df8b11c776fa679aadd77d820

---
+++ thrift/compiler/test/fixtures/stream-client-buffered-stream/src/module.thrift
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2019-present Facebook, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+/*
+ * Copyright 2019-present Facebook, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+namespace java.swift test.fixtures.stream
+
+exception FooEx { }
+
+service PubSubStreamingService {
+  stream<i32> returnstream(1: i32 i32_from, 2: i32 i32_to);
+  stream<i32> streamthrows(1: i32 foo) stream throws (1: FooEx e);
+  stream<i32> boththrows(1: i32 foo) throws(1: FooEx e)
+    stream throws (1: FooEx e);
+  i32, stream<i32> responseandstreamthrows(1: i32 foo) throws(1: FooEx e)
+    stream throws (1: FooEx e);
+}
