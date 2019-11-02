7cb2e01c52904eaa94d36e13ce35c9e4b74acff9
Nanshu Chen <nanshu@fb.com>
2019-11-01 18:34:50 -0700

    thrift-py3 client streaming integration tests
    Summary: Tests for py3 streaming client
    
    Reviewed By: andriigrynenko
    
    Differential Revision: D17553364
    
    fbshipit-source-id: ba48c4ef70c12856a375d2bad861e32a0a101b79

---
+++ thrift/lib/py3/test/stream/server.h
@@ -0,0 +1,76 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#include <thrift/lib/cpp2/async/StreamPublisher.h>
+#include <thrift/lib/cpp2/server/ThriftServer.h>
+#include <thrift/lib/cpp2/transport/rsocket/server/RSRoutingHandler.h>
+#include <thrift/lib/py3/test/stream/if/gen-cpp2/StreamTestService.tcc>
+
+namespace thrift {
+namespace py3 {
+namespace test {
+
+class StreamTestService : public StreamTestServiceSvIf {
+ public:
+  static std::shared_ptr<StreamTestService> createInstance() {
+    return std::make_shared<StreamTestService>();
+  }
+  apache::thrift::Stream<int32_t> returnstream(int32_t i32_from, int32_t i32_to)
+      override {
+    return createStreamGenerator(
+        [i32_from, i32_to]() -> folly::coro::AsyncGenerator<int32_t> {
+          for (auto i = i32_from; i < i32_to; ++i) {
+            co_yield i;
+          }
+        });
+  }
+  apache::thrift::Stream<int32_t> streamthrows(bool t) override {
+    if (t) {
+      throw FuncEx{};
+    } else {
+      return createStreamGenerator(
+          []() -> folly::coro::AsyncGenerator<int32_t&&> { throw StreamEx{}; });
+    }
+  }
+  apache::thrift::ResponseAndStream<included::Included, included::Included>
+  returnresponseandstream(std::unique_ptr<included::Included> foo) override {
+    included::Included resp;
+    resp.from = 100;
+    resp.to = 200;
+    auto stream = createStreamGenerator(
+        [foo = std::move(foo)]() mutable
+        -> folly::coro::AsyncGenerator<included::Included&&> {
+          for (auto i = foo->from; i < foo->to; ++i) {
+            included::Included p;
+            p.from = foo->from;
+            p.to = i;
+            co_yield std::move(p);
+          }
+        });
+    return {std::move(resp), std::move(stream)};
+  }
+};
+
+std::unique_ptr<apache::thrift::TransportRoutingHandler>
+createRSRoutingHandler() {
+  return std::make_unique<apache::thrift::RSRoutingHandler>();
+}
+
+} // namespace test
+} // namespace py3
+} // namespace thrift
---
+++ thrift/lib/py3/test/stream/test_client.py
@@ -0,0 +1,109 @@
+#!/usr/bin/env python3
+# Copyright (c) Facebook, Inc. and its affiliates.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import asyncio
+import unittest
+
+from thrift.py3 import RpcOptions, get_client
+from thrift.py3.client import ClientType
+from thrift.py3.test.included.included.types import Included
+from thrift.py3.test.server import TestServer
+from thrift.py3.test.stream.clients import StreamTestService
+from thrift.py3.test.stream.types import FuncEx, StreamEx
+
+
+class StreamClientTest(unittest.TestCase):
+    def test_return_stream(self) -> None:
+        loop = asyncio.get_event_loop()
+
+        async def inner_test() -> None:
+            async with TestServer(ip="::1") as sa:
+                assert sa.ip and sa.port
+                async with get_client(
+                    StreamTestService,
+                    host=sa.ip,
+                    port=sa.port,
+                    client_type=ClientType.THRIFT_ROCKET_CLIENT_TYPE,
+                ) as client:
+                    stream = await client.returnstream(10, 1024)
+                    res = [n async for n in stream]
+                    self.assertEqual(res, list(range(10, 1024)))
+
+        loop.run_until_complete(inner_test())
+
+    def test_stream_throws(self) -> None:
+        loop = asyncio.get_event_loop()
+
+        async def inner_test() -> None:
+            async with TestServer(ip="::1") as sa:
+                assert sa.ip and sa.port
+                async with get_client(
+                    StreamTestService,
+                    host=sa.ip,
+                    port=sa.port,
+                    client_type=ClientType.THRIFT_ROCKET_CLIENT_TYPE,
+                ) as client:
+                    with self.assertRaises(FuncEx):
+                        await client.streamthrows(True)
+                    stream = await client.streamthrows(False)
+                    with self.assertRaises(StreamEx):
+                        async for _ in stream:  # noqa: F841 current flake8 version too old to support "async for _ in"
+                            pass
+
+        loop.run_until_complete(inner_test())
+
+    def test_return_response_and_stream(self) -> None:
+        loop = asyncio.get_event_loop()
+
+        async def inner_test() -> None:
+            async with TestServer(ip="::1") as sa:
+                assert sa.ip and sa.port
+                async with get_client(
+                    StreamTestService,
+                    host=sa.ip,
+                    port=sa.port,
+                    client_type=ClientType.THRIFT_ROCKET_CLIENT_TYPE,
+                ) as client:
+                    resp, stream = await client.returnresponseandstream(
+                        Included(from_=39, to=42)
+                    )
+                    self.assertEqual(resp, Included(from_=100, to=200))
+                    expected_to = 39
+                    async for n in stream:
+                        self.assertEqual(n, Included(from_=39, to=expected_to))
+                        expected_to += 1
+                    self.assertEqual(expected_to, 42)
+
+        loop.run_until_complete(inner_test())
+
+    def test_return_stream_set_buffer_size(self) -> None:
+        loop = asyncio.get_event_loop()
+
+        async def inner_test() -> None:
+            options = RpcOptions()
+            options.chunk_buffer_size = 64
+            async with TestServer(ip="::1") as sa:
+                assert sa.ip and sa.port
+                async with get_client(
+                    StreamTestService,
+                    host=sa.ip,
+                    port=sa.port,
+                    client_type=ClientType.THRIFT_ROCKET_CLIENT_TYPE,
+                ) as client:
+                    stream = await client.returnstream(10, 1024, rpc_options=options)
+                    res = [n async for n in stream]
+                    self.assertEqual(res, list(range(10, 1024)))
+
+        loop.run_until_complete(inner_test())
