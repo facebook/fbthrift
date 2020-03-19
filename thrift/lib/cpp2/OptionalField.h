4efc040733321bd661e262e57e94c8dab8efcfdf
TJ Yin <ytj@fb.com>
2020-03-19 16:05:51 -0700

    Reimplement comparison operator
    Summary:
    Currently the comparison operator relies on the ability to convert to folly::Optional from DeprecatedOptionalField. In order to re-implement DeprecatedOptionalField, we can reimplement all comparison operators first.
    
    (Note: this ignores all push blocking failures!)
    
    Reviewed By: yfeldblum
    
    Differential Revision: D20455512
    
    fbshipit-source-id: 4037251e79ddeb5a3af8a46aa7cfb45944ecd2fc

--- thrift/lib/cpp2/OptionalField.h
+++ thrift/lib/cpp2/OptionalField.h
@@ -73,29 +73,87 @@ class DeprecatedOptionalField : public folly::Optional<T> {
     Base::operator=(std::move(other));
     return *this;
   }
-
-#define THRIFT_DETAIL_DEPRECATED_OPTIONAL_FIELD_DEFINE_OP(OP)              \
-  template <class U>                                                       \
-  bool operator OP(const DeprecatedOptionalField<U>& rhs) const {          \
-    return toFolly() OP rhs.toFolly();                                     \
-  }                                                                        \
-  template <class U>                                                       \
-  friend bool operator OP(                                                 \
-      const folly::Optional<U>& lhs, const DeprecatedOptionalField& rhs) { \
-    return lhs OP rhs.toFolly();                                           \
-  }                                                                        \
-  template <class U>                                                       \
-  bool operator OP(const folly::Optional<U>& rhs) const {                  \
-    return toFolly() OP rhs;                                               \
-  }
-  THRIFT_DETAIL_DEPRECATED_OPTIONAL_FIELD_DEFINE_OP(==)
-  THRIFT_DETAIL_DEPRECATED_OPTIONAL_FIELD_DEFINE_OP(!=)
-  THRIFT_DETAIL_DEPRECATED_OPTIONAL_FIELD_DEFINE_OP(<)
-  THRIFT_DETAIL_DEPRECATED_OPTIONAL_FIELD_DEFINE_OP(<=)
-  THRIFT_DETAIL_DEPRECATED_OPTIONAL_FIELD_DEFINE_OP(>)
-  THRIFT_DETAIL_DEPRECATED_OPTIONAL_FIELD_DEFINE_OP(>=)
-#undef THRIFT_DETAIL_DEPRECATED_OPTIONAL_FIELD_DEFINE_OP
 };
+
+namespace detail {
+
+template <class T>
+bool has_value(const DeprecatedOptionalField<T>& a) {
+  return a.has_value();
+}
+template <class T>
+bool has_value(const folly::Optional<T>& a) {
+  return a.has_value();
+}
+template <class T>
+bool has_value(const T&) {
+  return true;
+}
+template <class T>
+const auto& value(const DeprecatedOptionalField<T>& a) {
+  return *a;
+}
+template <class T>
+const auto& value(const folly::Optional<T>& a) {
+  return *a;
+}
+template <class T>
+const auto& value(const T& a) {
+  return a;
+}
+
+template <class U, class V, class Comp>
+bool compare(const U& a, const V& b, Comp comp) {
+  if (has_value(a) != has_value(b)) {
+    return comp(has_value(a), has_value(b));
+  }
+  if (has_value(a)) {
+    return comp(value(a), value(b));
+  }
+  return comp(0, 0);
+}
+} // namespace detail
+
+#define THRIFT_DETAIL_DEPRECATED_OPTIONAL_FIELD_DEFINE_OP(OP)                 \
+  template <class U, class V>                                                 \
+  bool operator OP(                                                           \
+      const DeprecatedOptionalField<U>& a,                                    \
+      const DeprecatedOptionalField<V>& b) {                                  \
+    return detail::compare(a, b, [](auto&& x, auto&& y) { return x OP y; });  \
+  }                                                                           \
+  template <class U, class V>                                                 \
+  bool operator OP(                                                           \
+      const folly::Optional<U>& a, const DeprecatedOptionalField<V>& b) {     \
+    return detail::compare(a, b, [](auto&& x, auto&& y) { return x OP y; });  \
+  }                                                                           \
+  template <class U, class V>                                                 \
+  bool operator OP(                                                           \
+      const DeprecatedOptionalField<U>& a, const folly::Optional<V>& b) {     \
+    return detail::compare(a, b, [](auto&& x, auto&& y) { return x OP y; });  \
+  }                                                                           \
+  template <class U, class V>                                                 \
+  bool operator OP(const U& a, const DeprecatedOptionalField<V>& b) {         \
+    return detail::compare(a, b, [](auto&& x, auto&& y) { return x OP y; });  \
+  }                                                                           \
+  template <class U, class V>                                                 \
+  bool operator OP(const DeprecatedOptionalField<U>& a, const V& b) {         \
+    return detail::compare(a, b, [](auto&& x, auto&& y) { return x OP y; });  \
+  }                                                                           \
+  template <class T>                                                          \
+  bool operator OP(const folly::None&, const DeprecatedOptionalField<T>& t) { \
+    return false OP t.has_value();                                            \
+  }                                                                           \
+  template <class T>                                                          \
+  bool operator OP(const DeprecatedOptionalField<T>& t, const folly::None&) { \
+    return t.has_value() OP false;                                            \
+  }
+THRIFT_DETAIL_DEPRECATED_OPTIONAL_FIELD_DEFINE_OP(==)
+THRIFT_DETAIL_DEPRECATED_OPTIONAL_FIELD_DEFINE_OP(!=)
+THRIFT_DETAIL_DEPRECATED_OPTIONAL_FIELD_DEFINE_OP(<)
+THRIFT_DETAIL_DEPRECATED_OPTIONAL_FIELD_DEFINE_OP(<=)
+THRIFT_DETAIL_DEPRECATED_OPTIONAL_FIELD_DEFINE_OP(>)
+THRIFT_DETAIL_DEPRECATED_OPTIONAL_FIELD_DEFINE_OP(>=)
+#undef THRIFT_DETAIL_DEPRECATED_OPTIONAL_FIELD_DEFINE_OP
 
 template <class T>
 folly::Optional<T> castToFolly(const DeprecatedOptionalField<T>& t) {
