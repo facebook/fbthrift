af418c27c581c6ead4d70f3b554f89cb7fecc3d9
Jun Zhou <junz@fb.com>
2020-04-01 11:03:37 -0700

    converter to thrift-py3 structs
    Summary:
    As title says, an obj -> thrift-py3 struct converter, using reflection/metadata instead of serialization/deserialization.
    
    It's intended to be used to convert a thrift-py struct to thrift-py3 struct, but it can actually take any object that have compatible interfaces as input.
    
    Reviewed By: nanshu
    
    Differential Revision: D20714692
    
    fbshipit-source-id: 20ad2eb97450d5088c64a861cc2a00826a22a8a0

---
+++ thrift/lib/py3/converter.pyx
@@ -0,0 +1,56 @@
+# Copyright (c) Facebook, Inc. and its affiliates.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from typing import Any, Type
+
+from thrift.py3.reflection import inspect
+from thrift.py3.types import CompiledEnum, Container, MapSpec, Struct
+
+
+def to_py3_struct(cls, obj):
+    return _to_py3_struct(cls, obj)
+
+
+cdef object _to_py3_struct(object cls, object obj):
+    struct_spec = inspect(cls)
+    return cls(
+        **{
+            field_spec.name: _to_py3_field(
+                field_spec.type, getattr(obj, field_spec.name)
+            )
+            for field_spec in struct_spec.fields
+        }
+    )
+
+
+cdef object _to_py3_field(object cls, object obj):
+    if obj is None:
+        return None
+    if issubclass(cls, Struct):
+        return _to_py3_struct(cls, obj)
+    elif issubclass(cls, Container):
+        container_spec = inspect(cls)
+        if isinstance(container_spec, MapSpec):
+            return {
+                _to_py3_field(container_spec.key, k): _to_py3_field(
+                    container_spec.value, v
+                )
+                for k, v in obj.items()
+            }
+        else:
+            return [_to_py3_field(container_spec.value, elem) for elem in obj]
+    elif issubclass(cls, CompiledEnum):
+        return cls(obj)
+    else:
+        return obj
---
+++ thrift/lib/py3/test/converter.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python3
+# Copyright (c) Facebook, Inc. and its affiliates.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import unittest
+
+import convertible.ttypes as ttypes
+import convertible.types as types
+from thrift.py3.converter import to_py3_struct
+
+
+class ConverterTest(unittest.TestCase):
+    def test_simple(self) -> None:
+        simple = to_py3_struct(
+            types.Simple,
+            ttypes.Simple(
+                intField=42,
+                strField="simple",
+                intList=[1, 2, 3],
+                strSet={"hello", "world"},
+                strToIntMap={"one": 1, "two": 2},
+                color=ttypes.Color.GREEN,
+            ),
+        )
+        self.assertEqual(simple.intField, 42)
+        self.assertEqual(simple.strField, "simple")
+        self.assertEqual(simple.intList, [1, 2, 3])
+        self.assertEqual(simple.strSet, {"hello", "world"})
+        self.assertEqual(simple.strToIntMap, {"one": 1, "two": 2})
+        self.assertEqual(simple.color, types.Color.GREEN)
+
+    def test_nested(self) -> None:
+        nested = to_py3_struct(
+            types.Nested,
+            ttypes.Nested(
+                simpleField=ttypes.Simple(
+                    intField=42,
+                    strField="simple",
+                    intList=[1, 2, 3],
+                    strSet={"hello", "world"},
+                    strToIntMap={"one": 1, "two": 2},
+                    color=ttypes.Color.NONE,
+                ),
+                simpleList=[
+                    ttypes.Simple(
+                        intField=200,
+                        strField="face",
+                        intList=[4, 5, 6],
+                        strSet={"keep", "calm"},
+                        strToIntMap={"three": 3, "four": 4},
+                        color=ttypes.Color.RED,
+                    ),
+                    ttypes.Simple(
+                        intField=404,
+                        strField="b00k",
+                        intList=[7, 8, 9],
+                        strSet={"carry", "on"},
+                        strToIntMap={"five": 5, "six": 6},
+                        color=ttypes.Color.GREEN,
+                    ),
+                ],
+                colorToSimpleMap={
+                    ttypes.Color.BLUE: ttypes.Simple(
+                        intField=500,
+                        strField="internal",
+                        intList=[10],
+                        strSet={"server", "error"},
+                        strToIntMap={"seven": 7, "eight": 8, "nine": 9},
+                        color=ttypes.Color.BLUE,
+                    )
+                },
+            ),
+        )
+        self.assertEqual(nested.simpleField.intField, 42)
+        self.assertEqual(nested.simpleList[0].intList, [4, 5, 6])
+        self.assertEqual(nested.simpleList[1].strSet, {"carry", "on"})
+        self.assertEqual(
+            nested.colorToSimpleMap[types.Color.BLUE].color, types.Color.BLUE
+        )
