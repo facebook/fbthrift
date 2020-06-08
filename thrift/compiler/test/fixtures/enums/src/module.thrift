fca77b07ad3b8fd00af06008b26d18dd86d76ed3
Yuhan Hao <yuhanhao@fb.com>
2020-06-08 16:31:48 -0700

    add a tests for different ways enums are referred
    Summary: Add a test for different currently supported ways to refer enums in thrift IDL
    
    Reviewed By: yfeldblum
    
    Differential Revision: D21918220
    
    fbshipit-source-id: c47a4b20a2b29640f72ff855bc32d9ef163969b1

---
+++ thrift/compiler/test/fixtures/enums/src/module.thrift
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace java.swift test.fixtures.enums
+
+enum Metasyntactic {
+    FOO = 1;
+    BAR = 2;
+    BAZ = 3;
+    BAX = 4;
+}
+
+struct SomeStruct {
+    1: Metasyntactic reasonable = FOO;
+    2: Metasyntactic fine = 2;
+    // The following will cause thrift compiler to crush when generating java stubs
+    // 3: Metasyntactic questionable = -1;
+    4: set<i32> tags = [];
+}
