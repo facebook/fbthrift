1607c82190843cd0b1531fbe05d1aec334c8605d
Alfred Fuller <afuller@fb.com>
2020-07-31 10:39:16 -0700

    Define Core Conformance Test Types
    Summary: Defines: Any, Value, and Object
    
    Reviewed By: vitaut
    
    Differential Revision: D22710000
    
    fbshipit-source-id: 35276f3f57fa08e71e2cc7fd10637c56df10938d

---
+++ thrift/conformance/if/object.thrift
@@ -0,0 +1,53 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+include "thrift/conformance/if/any.thrift"
+
+namespace cpp2 apache.thrift.conformance
+
+// A dynamic struct/union/exception (e.g. a JSON Object).
+struct Object {
+  // The type of the object, if applicable.
+  1: string type;
+
+  // The members of the object.
+  2: map<string, Value> members;
+}
+
+// A dynamic value.
+union Value {
+  // Integers.
+  2: bool boolValue;
+  3: byte byteValue;
+  4: i16  i16Value;
+  5: i32  i32Value;
+  6: i64  i64Value;
+
+  // Floats.
+  7: float f32Value;
+  8: double f64Value;
+
+  // Containers of values.
+  9: list<Value> listValue;
+  10: map<Value, Value> mapValue;
+  11: set<Value> setValue;
+
+  // A dynamic object value.
+  12: Object objectValue;
+
+  // A static object value.
+  13: any.Any anyValue;
+}
