ba3bd336c5abdc61a1599ec5de40bacc081f81f4
Alfred Fuller <afuller@fb.com>
2020-08-04 13:48:16 -0700

    Add Conformance Service and TestSuite definitions
    Reviewed By: iahs
    
    Differential Revision: D22854522
    
    fbshipit-source-id: 2f00f63d380cb2b2f8c5313a4ba9fcbf2529207c

---
+++ thrift/conformance/if/test_suite.thrift
@@ -0,0 +1,54 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace cpp2 apache.thrift.conformance
+
+include "thrift/conformance/if/conformance.thrift"
+
+// A collection of test cases.
+struct TestSuit {
+  // The name of the suite.
+  1: string name;
+
+  // A description of the test suite, if useful.
+  2: optional string description;
+
+  // The test cases included in the suite.
+  3: list<TestCase> tests;
+}
+
+// A single test case.
+struct TestCase {
+  // The name of the test.
+  1: string name;
+
+  // A description of the test, if useful.
+  2: optional string description;
+
+  // The test case to run.
+  3: TestCaseUnion test;
+}
+
+// A union of all supported test case types.
+union TestCaseUnion {
+  1: RoundTripTestCase roundTrip;
+}
+
+// Tests if a value round trips correctly.
+struct RoundTripTestCase {
+  1: conformance.RoundTripRequest request;
+  2: conformance.RoundTripResponse expectedResponse;
+}
