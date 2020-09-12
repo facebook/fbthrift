77f7fbdf15dbee775551c06e0dead283e57202b8
Alfred Fuller <afuller@fb.com>
2020-09-11 19:41:09 -0700

    Add generator for roundTrip tests (concrete types x standard protocols)
    Reviewed By: iahs
    
    Differential Revision: D23309001
    
    fbshipit-source-id: ab4a941e1a466984453a32946fb349e11537eb9b

---
+++ thrift/conformance/data/TestGenerator.cpp
@@ -0,0 +1,85 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <thrift/conformance/data/TestGenerator.h>
+
+#include <fmt/core.h>
+
+#include <thrift/conformance/cpp2/Object.h>
+#include <thrift/conformance/data/ValueGenerator.h>
+
+namespace apache::thrift::conformance::data {
+
+namespace {
+
+template <typename TT>
+Test createRoundTripTest(
+    const AnyRegistry& registry,
+    const Protocol& protocol) {
+  Test test;
+  test.name_ref() = fmt::format("{}: {}", protocol.name(), TT::getName());
+  for (const auto& value : ValueGenerator<TT>::getInterestingValues()) {
+    RoundTripTestCase roundTrip;
+    roundTrip.request_ref()->value_ref() =
+        registry.store(asValueStruct<TT>(value.value), protocol);
+
+    auto& testCase = test.testCases_ref()->emplace_back();
+    testCase.name_ref() = value.name;
+    testCase.test_ref()->set_roundTrip(std::move(roundTrip));
+  }
+
+  return test;
+}
+
+void addRoundTripToSuite(
+    const AnyRegistry& registry,
+    const Protocol& protocol,
+    TestSuite& suite) {
+  suite.tests_ref()->emplace_back(
+      createRoundTripTest<type::bool_t>(registry, protocol));
+  suite.tests_ref()->emplace_back(
+      createRoundTripTest<type::byte_t>(registry, protocol));
+  suite.tests_ref()->emplace_back(
+      createRoundTripTest<type::i16_t>(registry, protocol));
+  suite.tests_ref()->emplace_back(
+      createRoundTripTest<type::i32_t>(registry, protocol));
+  suite.tests_ref()->emplace_back(
+      createRoundTripTest<type::float_t>(registry, protocol));
+  suite.tests_ref()->emplace_back(
+      createRoundTripTest<type::double_t>(registry, protocol));
+  suite.tests_ref()->emplace_back(
+      createRoundTripTest<type::string_t>(registry, protocol));
+  suite.tests_ref()->emplace_back(
+      createRoundTripTest<type::binary_t>(registry, protocol));
+}
+
+} // namespace
+
+TestSuite createRoundTripSuite(const AnyRegistry& registry) {
+  TestSuite suite;
+  suite.name_ref() = "Round Trip";
+  addRoundTripToSuite(
+      registry, getStandardProtocol<StandardProtocol::Binary>(), suite);
+  addRoundTripToSuite(
+      registry, getStandardProtocol<StandardProtocol::Compact>(), suite);
+  addRoundTripToSuite(
+      registry, getStandardProtocol<StandardProtocol::Json>(), suite);
+  addRoundTripToSuite(
+      registry, getStandardProtocol<StandardProtocol::SimpleJson>(), suite);
+  return suite;
+}
+
+} // namespace apache::thrift::conformance::data
---
+++ thrift/conformance/data/TestGeneratorTest.cpp
@@ -0,0 +1,60 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <thrift/conformance/data/TestGenerator.h>
+
+#include <gtest/gtest.h>
+#include <thrift/conformance/cpp2/AnyRegistry.h>
+#include <thrift/conformance/cpp2/AnyStructSerializer.h>
+#include <thrift/conformance/cpp2/Object.h>
+#include <thrift/conformance/if/gen-cpp2/object_types_custom_protocol.h>
+
+namespace apache::thrift::conformance::data {
+
+TEST(TestGeneratorTest, RoundTripSuite) {
+  AnyRegistry registry;
+  registry.registerType<Value>("fb.com/apache.thrift.conformance.Value");
+  registry.registerSerializer<Value>(
+      &getAnyStandardSerializer<Value, StandardProtocol::Binary>());
+  registry.registerSerializer<Value>(
+      &getAnyStandardSerializer<Value, StandardProtocol::Compact>());
+  registry.registerSerializer<Value>(
+      &getAnyStandardSerializer<Value, StandardProtocol::Json>());
+  registry.registerSerializer<Value>(
+      &getAnyStandardSerializer<Value, StandardProtocol::SimpleJson>());
+  auto suite = createRoundTripSuite(registry);
+
+  constexpr size_t kNumProtocols = 4;
+  constexpr size_t kNumTypes = 8;
+  EXPECT_EQ(*suite.name_ref(), "Round Trip");
+  ASSERT_EQ(suite.tests_ref()->size(), kNumProtocols * kNumTypes);
+
+  EXPECT_EQ(*suite.tests_ref()->at(0 * kNumTypes).name_ref(), "Binary: bool");
+  EXPECT_EQ(*suite.tests_ref()->at(1 * kNumTypes).name_ref(), "Compact: bool");
+  EXPECT_EQ(*suite.tests_ref()->at(2 * kNumTypes).name_ref(), "Json: bool");
+  EXPECT_EQ(
+      *suite.tests_ref()->at(3 * kNumTypes).name_ref(), "SimpleJson: bool");
+
+  const auto& test = suite.tests_ref()->at(1);
+  EXPECT_EQ(*test.name_ref(), "Binary: byte");
+
+  ASSERT_GT(test.testCases_ref()->size(), 0);
+  const auto& testCase = test.testCases_ref()->at(0);
+  EXPECT_EQ(*testCase.name_ref(), "zero");
+  EXPECT_EQ(testCase.test_ref()->getType(), TestCaseUnion::roundTrip);
+}
+
+} // namespace apache::thrift::conformance::data
