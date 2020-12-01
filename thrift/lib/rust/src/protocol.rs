0a70677d27389e86a662f00367e5d93e02f5851f
Andres Suarez <asuarez@fb.com>
2020-12-01 11:07:12 -0800

    Fix license header
    Reviewed By: mzlee
    
    Differential Revision: D25246234
    
    fbshipit-source-id: 6d59c63adcf128687534f7869b98a473a6a76eb7

--- thrift/lib/rust/src/protocol.rs
+++ thrift/lib/rust/src/protocol.rs
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 use crate::bufext::BufMutExt;
 use crate::errors::ProtocolError;
 use crate::framing::{Framing, FramingDecoded, FramingEncoded, FramingEncodedFinal};
