9bc1f39e826dbdc5c148ae573ee5fed6239be8b3
TJ Yin <ytj@fb.com>
2020-12-27 13:43:01 -0800

    add indirection wrapper to use cpp.indirection to extend existing thrift struct
    Summary: With `IndirectionWrapper`, we can easily extend thrift struct.
    
    Reviewed By: yfeldblum
    
    Differential Revision: D24948867
    
    fbshipit-source-id: 8fb43fab75841929b6ddd4f8e72d02d192ba8276

---
+++ thrift/lib/cpp2/IndirectionWrapper.h
@@ -0,0 +1,95 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#include <memory>
+#include <utility>
+
+#include <folly/Overload.h>
+#include <thrift/lib/cpp2/Thrift.h>
+
+namespace apache {
+namespace thrift {
+
+//  IndirectionWrapper
+//
+//  Owns a value of type T, which may be incomplete at instantiation time.
+//
+//  Provides access to the T and works with cpp.indirection.
+//
+//  Expected to be used as a base class. Enforced by marking the dtor protected.
+template <typename Derived, typename T>
+class IndirectionWrapper {
+ private:
+  std::unique_ptr<T> raw_{new T()};
+
+ protected:
+  ~IndirectionWrapper() = default;
+
+ public:
+  FBTHRIFT_CPP_DEFINE_MEMBER_INDIRECTION_FN(__fbthrift_data());
+
+  IndirectionWrapper() = default;
+
+  IndirectionWrapper(const IndirectionWrapper& other)
+      : raw_(new T(other.__fbthrift_data())) {}
+
+  IndirectionWrapper(IndirectionWrapper&& other) noexcept(false)
+      : raw_(new T(std::move(other.__fbthrift_data()))) {}
+
+  IndirectionWrapper& operator=(const IndirectionWrapper& other) noexcept(
+      std::is_nothrow_copy_assignable_v<T>) {
+    __fbthrift_data() = other.__fbthrift_data();
+    return *this;
+  }
+
+  IndirectionWrapper& operator=(IndirectionWrapper&& other) noexcept(
+      std::is_nothrow_move_assignable_v<T>) {
+    __fbthrift_data() = std::move(other.__fbthrift_data());
+    return *this;
+  }
+
+  friend bool operator==(const Derived& lhs, const Derived& rhs) {
+    return lhs.__fbthrift_data() == rhs.__fbthrift_data();
+  }
+
+  friend bool operator<(const Derived& lhs, const Derived& rhs) {
+    return lhs.__fbthrift_data() < rhs.__fbthrift_data();
+  }
+
+  void __clear() {
+    __fbthrift_data().__clear();
+  }
+
+  T& __fbthrift_data() & {
+    return *raw_;
+  }
+
+  const T& __fbthrift_data() const& {
+    return *raw_;
+  }
+
+  T&& __fbthrift_data() && {
+    return std::move(*raw_);
+  }
+
+  const T&& __fbthrift_data() const&& {
+    return std::move(*raw_);
+  }
+};
+} // namespace thrift
+} // namespace apache
