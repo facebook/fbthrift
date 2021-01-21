09f3c7aa65a94a0be442efa4bdb67877a000ccb9
Victor Zverovich <viz@fb.com>
2021-01-21 13:49:19 -0800

    Don't generate unnecessary includes in *_constants.cpp
    Summary:
    Most Thrift files don't define constants and the object files generated from `*_constants.cpp` end up being discarded by a linker. To save some compilation time make `*_constants.cpp` effectively empty (just one comment) when there are no constants. Most importantly this will eliminate includes.
    
    Another option is to not emit `_constants.cpp` at all but it is problematic because `genrule` requires outputs to exist.
    
    Reviewed By: iahs
    
    Differential Revision: D25985737
    
    fbshipit-source-id: 5e7d5a59e5510e46fbcbda788c7df9525b2db53b

--- thrift/compiler/test/fixtures/coroutines/gen-cpp2/module_constants.cpp
+++ thrift/compiler/test/fixtures/coroutines/gen-cpp2/module_constants.cpp
@@ -4,12 +4,3 @@
  * DO NOT EDIT UNLESS YOU ARE SURE THAT YOU KNOW WHAT YOU ARE DOING
  *  @generated
  */
-
-#include "thrift/compiler/test/fixtures/coroutines/gen-cpp2/module_constants.h"
-
-#include <thrift/lib/cpp2/gen/module_constants_cpp.h>
-
-
-namespace cpp2 {
-
-} // cpp2
--- thrift/compiler/test/fixtures/types/gen-cpp2/module_constants.cpp
+++ thrift/compiler/test/fixtures/types/gen-cpp2/module_constants.cpp
@@ -4,12 +4,3 @@
  * DO NOT EDIT UNLESS YOU ARE SURE THAT YOU KNOW WHAT YOU ARE DOING
  *  @generated
  */
-
-#include "thrift/compiler/test/fixtures/types/gen-cpp2/module_constants.h"
-
-#include <thrift/lib/cpp2/gen/module_constants_cpp.h>
-
-
-namespace apache { namespace thrift { namespace fixtures { namespace types {
-
-}}}} // apache::thrift::fixtures::types
