9b60cef0c44108999ddc7a2276d0d31df6d2ce90
Nanshu Chen <nanshu@fb.com>
2021-01-28 14:49:33 -0800

    remove TTrolliusServer
    Reviewed By: junzh0u
    
    Differential Revision: D26096431
    
    fbshipit-source-id: 28330f6464faa9ca3539139f5e75ae880b365954

--- thrift/lib/py/util/trollius.py
+++ thrift/lib/py/util/trollius.py
@@ -19,46 +19,6 @@ from __future__ import unicode_literals
 
 import trollius as asyncio
 
-from trollius import (From, Return, )
-from thrift.server.TTrolliusServer import ThriftClientProtocolFactory
-from thrift.util.Decorators import protocol_manager
-
-
-@asyncio.coroutine
-def async_protocol_manager(coro):
-    _, protocol = yield From(coro)
-    raise Return(protocol_manager(protocol))
-
-
-def create_client(client_klass,
-                  host=None,
-                  port=None,
-                  loop=None,
-                  timeouts=None,
-                  client_type=None):
-    """
-    create a Trollius thrift client and return a context manager for it
-    This is a coroutine
-    :param client_klass: thrift Client class
-    :param host: hostname/ip, None = loopback
-    :param port: port number
-    :param loop: Trollius event loop
-    :returns: a Context manager which provides the thrift client
-    """
-    if not loop:
-        loop = asyncio.get_event_loop()
-    coro = loop.create_connection(
-        ThriftClientProtocolFactory(
-            client_klass,
-            loop=loop,
-            timeouts=timeouts,
-            client_type=client_type,
-        ),
-        host=host,
-        port=port,
-    )
-    return async_protocol_manager(coro)
-
 
 def call_as_future(callable, loop, *args, **kwargs):
     """This is a copy of thrift.util.asyncio. So, let's consider unifying them.
