0db57c9f3547e1b08487045dfcd4910ec3a616ad
Yuhan Hao <yuhanhao@fb.com>
2021-03-31 07:26:52 -0700

    migrate away from using boost::make_recursive_variant
    Summary: Migrate away from using `boost::make_recursive_variant`. That facility has no parallel in `std::variant`. So this diff will make future migration to `std::variant` a bit easier.
    
    Reviewed By: yfeldblum
    
    Differential Revision: D27440134
    
    fbshipit-source-id: 18f664abd2deb4647ad8fc51c3d0a36b6d93ce2e

--- thrift/compiler/mustache/mstch.h
+++ thrift/compiler/mustache/mstch.h
@@ -127,18 +127,23 @@ class lambda_t {
   std::function<std::string(node_renderer<N> renderer, const std::string&)> fun;
 };
 
-} // namespace internal
-
-using node = boost::make_recursive_variant<
+template <typename Node>
+using node_base = boost::variant<
     std::nullptr_t,
     std::string,
     int,
     double,
     bool,
-    internal::lambda_t<boost::recursive_variant_>,
-    std::shared_ptr<internal::object_t<boost::recursive_variant_>>,
-    std::map<const std::string, boost::recursive_variant_>,
-    std::vector<boost::recursive_variant_>>::type;
+    internal::lambda_t<Node>,
+    std::shared_ptr<internal::object_t<Node>>,
+    std::map<const std::string, Node>,
+    std::vector<Node>>;
+
+} // namespace internal
+
+struct node : internal::node_base<node> {
+  using internal::node_base<node>::node_base;
+};
 using object = internal::object_t<node>;
 using lambda = internal::lambda_t<node>;
 using map = std::map<const std::string, node>;
