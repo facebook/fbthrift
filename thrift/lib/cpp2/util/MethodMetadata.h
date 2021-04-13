fdb91422519655f70939c47449575c72b3132b54
Yicheng Wang <yichengfb@fb.com>
2021-04-12 17:18:38 -0700

    MethodMetaData, a utility class that manage the storage of method meta
    Summary:
    We are planning to pass the idempotency information of a method through the transport.
    Similar to ManagedStringView, this new class will have two types of storage, both static and dynamic.
    
    Reviewed By: praihan
    
    Differential Revision: D27540039
    
    fbshipit-source-id: 7c212d69182603ae06087862a9ade494949a7c0d

---
+++ thrift/lib/cpp2/util/MethodMetadata.h
@@ -0,0 +1,131 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+#include <memory>
+#include <string>
+#include <string_view>
+#include <folly/Range.h>
+
+namespace apache::thrift {
+
+enum class FunctionQualifier {
+  NONE = 0,
+  ONE_WAY,
+  IDEMPOTENT,
+  READ_ONLY,
+};
+
+/*
+ * A move-only structure for storing thrift method metadata.
+ * Metadata : method name & function qualifier.
+ * When created from static data, the structure creates a view of the data
+ * When created from temporary data, the structure takes the ownership of
+ * the data by creating a dynamic storage for it.
+ */
+class MethodMetadata {
+ public:
+  struct Data {
+    Data(std::string name, FunctionQualifier qualifier)
+        : methodName(std::move(name)), functionQualifier(qualifier) {}
+    std::string methodName;
+    FunctionQualifier functionQualifier;
+  };
+
+  MethodMetadata() = delete;
+  MethodMetadata(const MethodMetadata&) = delete;
+  MethodMetadata& operator=(const MethodMetadata&) = delete;
+
+  MethodMetadata(MethodMetadata&& that) noexcept
+      : isOwning_(std::exchange(that.isOwning_, false)),
+        data_(std::exchange(that.data_, nullptr)) {}
+
+  MethodMetadata& operator=(MethodMetadata&& that) noexcept {
+    MethodMetadata tmp(std::move(that));
+    swap(*this, tmp);
+    return *this;
+  }
+
+  ~MethodMetadata() noexcept {
+    if (isOwning()) {
+      delete data_;
+    }
+  }
+
+  /* implicit */ MethodMetadata(std::string_view methodName)
+      : isOwning_(true),
+        data_(new Data(std::string(methodName), FunctionQualifier::NONE)) {}
+
+  /* implicit */ MethodMetadata(std::string&& methodName)
+      : isOwning_(true),
+        data_(new Data(std::move(methodName), FunctionQualifier::NONE)) {}
+
+  /* implicit */ MethodMetadata(const std::string& methodName)
+      : isOwning_(true), data_(new Data(methodName, FunctionQualifier::NONE)) {}
+
+  /* implicit */ MethodMetadata(const char* methodName)
+      : isOwning_(true), data_(new Data(methodName, FunctionQualifier::NONE)) {}
+
+  /* implicit */ MethodMetadata(folly::StringPiece methodName)
+      : isOwning_(true),
+        data_(new Data(std::string(methodName), FunctionQualifier::NONE)) {}
+
+  static MethodMetadata from_static(Data* mPtr) {
+    return MethodMetadata(mPtr, NonOwningTag{});
+  }
+
+  std::string name_str() const& { return data_->methodName; }
+
+  std::string name_str() && {
+    return isOwning() ? std::move(data_->methodName) : data_->methodName;
+  }
+
+  std::string_view name_view() const {
+    return std::string_view(data_->methodName);
+  }
+
+  FunctionQualifier qualifier() const { return data_->functionQualifier; }
+
+  bool isOwning() const { return isOwning_; }
+
+  MethodMetadata getCopy() const {
+    if (isOwning()) {
+      return MethodMetadata(data_, OwningTag{});
+    } else {
+      return MethodMetadata(data_, NonOwningTag{});
+    }
+  }
+
+ private:
+  struct NonOwningTag {};
+  struct OwningTag {};
+
+  MethodMetadata(Data* mPtr, OwningTag)
+      : isOwning_(true), data_(new Data(*mPtr)) {}
+
+  MethodMetadata(Data* mPtr, NonOwningTag) : isOwning_(false), data_(mPtr) {}
+
+  friend void swap(MethodMetadata& left, MethodMetadata& right) noexcept {
+    std::swap(left.isOwning_, right.isOwning_);
+    std::swap(left.data_, right.data_);
+  }
+
+  bool isOwning_{false};
+  Data* data_{nullptr};
+};
+
+} // namespace apache::thrift
