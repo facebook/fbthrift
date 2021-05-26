edb4836539f7b89aea6f3c01c36a60cedf80954e
Misha Shneerson <mshneer@fb.com>
2021-05-25 23:18:21 -0700

    Instrument SFQThreadManagerTest
    Summary:
    Test is flakey, sometimes failing in waiting on a baton and it is not clear
    what baton it is waiting on. Adding test asserts that would shows the issue if
    baton is not posted within 3 seconds.
    
    Reviewed By: praihan
    
    Differential Revision: D28699771
    
    fbshipit-source-id: 63b158e8f42aacaf171c77a07f5785dab500dc1a

--- thrift/lib/cpp/concurrency/test/SFQThreadManagerTest.cpp
+++ thrift/lib/cpp/concurrency/test/SFQThreadManagerTest.cpp
@@ -30,6 +30,7 @@
 #include <thrift/lib/cpp/concurrency/SFQThreadManager.h>
 
 using namespace apache::thrift::concurrency;
+using namespace std::literals::chrono_literals;
 using testing::_;
 using testing::AnyNumber;
 using testing::AtLeast;
@@ -82,28 +83,28 @@ TEST_F(SFQThreadManagerTest, FairnessPreemptTest) {
   ThreadManager::ExecutionScope es(PRIORITY::NORMAL);
   es.setTenantId(0);
   tm->getKeepAlive(es, source)->add([&]() {
-    addOrderBaton[0].wait();
+    ASSERT_TRUE(addOrderBaton[0].try_wait_for(3s));
     ++c0;
     c0Baton[0].post();
   });
 
   es.setTenantId(0);
   tm->getKeepAlive(es, source)->add([&]() {
-    addOrderBaton[1].wait();
+    ASSERT_TRUE(addOrderBaton[1].try_wait_for(3s));
     ++c0;
     c0Baton[1].post();
   });
 
   es.setTenantId(1);
   tm->getKeepAlive(es, source)->add([&]() {
-    addOrderBaton[2].wait();
+    ASSERT_TRUE(addOrderBaton[2].try_wait_for(3s));
     ++c1;
     c1Baton[0].post();
   });
 
   es.setTenantId(1);
   tm->getKeepAlive(es, source)->add([&]() {
-    addOrderBaton[3].wait();
+    ASSERT_TRUE(addOrderBaton[3].try_wait_for(3s));
     ++c1;
     c1Baton[1].post();
   });
@@ -114,20 +115,20 @@ TEST_F(SFQThreadManagerTest, FairnessPreemptTest) {
 
   // Tenant 0 was added first, so we expect this to execute.
   addOrderBaton[0].post();
-  c0Baton[0].wait();
+  ASSERT_TRUE(c0Baton[0].try_wait_for(3s));
   EXPECT_EQ(1, c0);
   EXPECT_EQ(0, c1);
 
   // Tenant 1 should be next even though it was added 3rd. Posting the 3rd
   // add-order baton would lock up here if it were unfair.
   addOrderBaton[2].post();
-  c1Baton[0].wait();
+  ASSERT_TRUE(c1Baton[0].try_wait_for(3s));
   EXPECT_EQ(1, c0);
   EXPECT_EQ(1, c1);
 
   // Tenant 0 will then be up next. It was the task added 2nd.
   addOrderBaton[1].post();
-  c0Baton[1].wait();
+  ASSERT_TRUE(c0Baton[1].try_wait_for(3s));
   EXPECT_EQ(2, c0);
   EXPECT_EQ(1, c1);
 
@@ -137,16 +138,16 @@ TEST_F(SFQThreadManagerTest, FairnessPreemptTest) {
   es = ThreadManager::ExecutionScope(PRIORITY::HIGH);
   es.setTenantId(123);
   tm->getKeepAlive(es, ThreadManager::Source::INTERNAL)->add([&]() {
-    hpribaton.wait();
+    ASSERT_TRUE(hpribaton.try_wait_for(3s));
     hpribatonOuter.post();
   });
   hpribaton.post();
-  hpribatonOuter.wait();
+  ASSERT_TRUE(hpribatonOuter.try_wait_for(3s));
 
   // Now we should be able to execute the tenant 1 task after the
   // source-preempted task.
   addOrderBaton[3].post();
-  c1Baton[1].wait();
+  ASSERT_TRUE(c1Baton[1].try_wait_for(3s));
   EXPECT_EQ(2, c0);
   EXPECT_EQ(2, c1);
 
