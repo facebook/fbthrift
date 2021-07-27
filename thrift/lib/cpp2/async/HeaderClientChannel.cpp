6325d62a7330a910dfdb4405aedebf27acc35993
Andrii Grynenko <andrii@fb.com>
2021-07-27 09:58:54 -0700

    Enable HeaderClientChannel upgrade to Rocket
    Summary: This diff enables the upgrade to rocket functionality inside HeaderClientChannel. Any Thrift channel created via HeaderClientChannel::newChannel will try to transparently upgrade to Rocket before sending the first request and if such upgrade was successful (if server supports it) Rocket transport will be used for all subsequent requests.
    
    Reviewed By: mshneer
    
    Differential Revision: D29706888
    
    fbshipit-source-id: 95a914d3c010191968e2e38f85d1e06915533f59

--- thrift/lib/cpp2/async/HeaderClientChannel.cpp
+++ thrift/lib/cpp2/async/HeaderClientChannel.cpp
@@ -32,7 +32,7 @@
 #include <thrift/lib/cpp2/server/Cpp2ConnContext.h>
 #include <thrift/lib/thrift/gen-cpp2/RocketUpgradeAsyncClient.h>
 
-THRIFT_FLAG_DEFINE_bool(raw_client_rocket_upgrade_enabled_v2, false);
+THRIFT_FLAG_DEFINE_bool(raw_client_rocket_upgrade_enabled_v2, true);
 THRIFT_FLAG_DEFINE_int64(raw_client_rocket_upgrade_timeout_ms, 500);
 
 using folly::IOBuf;
