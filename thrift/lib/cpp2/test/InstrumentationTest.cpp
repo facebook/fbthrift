ffb8bc76d692f2934383a779c0e32fa8c67c4448
CodemodService FBSourceClangFormatLinterBot <>
2021-08-06 04:21:40 -0700

    Daily `arc lint --take CLANGFORMAT`
    Reviewed By: zertosh
    
    Differential Revision: D30154797
    
    fbshipit-source-id: 80a38009969f9b56fed7f5728c39819a543e6272

--- thrift/lib/cpp2/test/InstrumentationTest.cpp
+++ thrift/lib/cpp2/test/InstrumentationTest.cpp
@@ -964,10 +964,10 @@ class RegistryTests : public testing::TestWithParam<std::tuple<size_t, bool>> {
    private:
     folly::observer::SimpleObservable<AdaptiveConcurrencyController::Config>
         oConfig{AdaptiveConcurrencyController::Config{}};
-  folly::observer::SimpleObservable<uint32_t> oMaxRequests{0u};
-  AdaptiveConcurrencyController controller_{
-      oConfig.getObserver(), oMaxRequests.getObserver()};
-  RequestStateMachine stateMachine_;
+    folly::observer::SimpleObservable<uint32_t> oMaxRequests{0u};
+    AdaptiveConcurrencyController controller_{
+        oConfig.getObserver(), oMaxRequests.getObserver()};
+    RequestStateMachine stateMachine_;
   };
 };
 
