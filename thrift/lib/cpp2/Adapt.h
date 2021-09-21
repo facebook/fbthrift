90898c27c76fa87bcfb34820edb8d15477f360e8
Alfred Fuller <afuller@fb.com>
2021-09-20 19:55:46 -0700

    Add generic equal and less implementations.
    Summary: These first try for Adapter::{op}, then Adapted::operator {op}, and then fall back on converting toThrift (which is actually performant if toThrift is ~trivial, for example by just returning a reference).
    
    Reviewed By: thedavekwon
    
    Differential Revision: D31032744
    
    fbshipit-source-id: fa238668644f533622f3e4aa4ddff907577191e0

--- thrift/lib/cpp2/Adapt.h
+++ thrift/lib/cpp2/Adapt.h
@@ -18,18 +18,107 @@
 
 #include <utility>
 
+#include <folly/Traits.h>
+
 namespace apache {
 namespace thrift {
 namespace adapt_detail {
 
-template <typename Adapter, typename AdaptedType, typename NativeType>
-void fromThrift(AdaptedType& adapted, NativeType&& native) {
-  adapted = Adapter::fromThrift(std::forward<NativeType>(native));
+// Identical to std::declval<const T&>
+template <typename T>
+const T& cr();
+
+template <typename Adapter, typename AdaptedT, typename NativeT>
+void fromThrift(AdaptedT& adapted, NativeT&& native) {
+  adapted = Adapter::fromThrift(std::forward<NativeT>(native));
 }
 
 // The type returned by the adapter for the given thrift type.
-template <typename Adapter, typename ThriftType>
-using adapted_t = decltype(Adapter::fromThrift(std::declval<ThriftType&&>()));
+template <typename Adapter, typename ThriftT>
+using adapted_t = decltype(Adapter::fromThrift(std::declval<ThriftT&&>()));
+
+// Equal op based on the thrift types.
+template <typename Adapter, typename AdaptedT>
+struct thrift_equal {
+  constexpr bool operator()(const AdaptedT& lhs, const AdaptedT& rhs) const {
+    return Adapter::toThrift(lhs) == Adapter::toThrift(rhs);
+  }
+};
+
+// Equal op based on the adapted types, with a fallback on thrift_equal.
+template <typename Adapter, typename AdaptedT, typename = void>
+struct adapted_equal : thrift_equal<Adapter, AdaptedT> {};
+template <typename Adapter, typename AdaptedT>
+struct adapted_equal<
+    Adapter,
+    AdaptedT,
+    folly::void_t<decltype(cr<AdaptedT>() == cr<AdaptedT>())>> {
+  constexpr bool operator()(const AdaptedT& lhs, const AdaptedT& rhs) const {
+    return lhs == rhs;
+  }
+};
+
+// Equal op based on the adapter, with a fallback on adapted_equal.
+template <typename Adapter, typename AdaptedT, typename = void>
+struct adapter_equal : adapted_equal<Adapter, AdaptedT> {};
+template <typename Adapter, typename AdaptedT>
+struct adapter_equal<
+    Adapter,
+    AdaptedT,
+    folly::void_t<decltype(Adapter::equal(cr<AdaptedT>(), cr<AdaptedT>()))>> {
+  constexpr bool operator()(const AdaptedT& lhs, const AdaptedT& rhs) const {
+    return Adapter::equal(lhs, rhs);
+  }
+};
+
+// Less op based on the thrift types.
+template <typename Adapter, typename AdaptedT>
+struct thrift_less {
+  constexpr bool operator()(const AdaptedT& lhs, const AdaptedT& rhs) const {
+    return Adapter::toThrift(lhs) < Adapter::toThrift(rhs);
+  }
+};
+
+// Less op based on the adapted types, with a fallback on thrift_less.
+template <typename Adapter, typename AdaptedT, typename = void>
+struct adapted_less : thrift_less<Adapter, AdaptedT> {};
+template <typename Adapter, typename AdaptedT>
+struct adapted_less<
+    Adapter,
+    AdaptedT,
+    folly::void_t<decltype(cr<AdaptedT>() < cr<AdaptedT>())>> {
+  constexpr bool operator()(const AdaptedT& lhs, const AdaptedT& rhs) const {
+    return lhs < rhs;
+  }
+};
+
+// Less op based on the adapter, with a fallback on adapted_less.
+template <typename Adapter, typename AdaptedT, typename = void>
+struct adapter_less : adapted_less<Adapter, AdaptedT> {};
+template <typename Adapter, typename AdaptedT>
+struct adapter_less<
+    Adapter,
+    AdaptedT,
+    folly::void_t<decltype(Adapter::less(cr<AdaptedT>(), cr<AdaptedT>()))>> {
+  constexpr bool operator()(const AdaptedT& lhs, const AdaptedT& rhs) const {
+    return Adapter::less(lhs, rhs);
+  }
+};
+
+template <typename Adapter, typename AdaptedT>
+constexpr bool equal(const AdaptedT& lhs, const AdaptedT& rhs) {
+  return adapter_equal<Adapter, AdaptedT>()(lhs, rhs);
+}
+
+template <typename Adapter, typename AdaptedT>
+constexpr bool not_equal(const AdaptedT& lhs, const AdaptedT& rhs) {
+  return !adapter_equal<Adapter, AdaptedT>()(lhs, rhs);
+}
+
+template <typename Adapter, typename AdaptedT>
+constexpr bool less(const AdaptedT& lhs, const AdaptedT& rhs) {
+  return adapter_less<Adapter, AdaptedT>()(lhs, rhs);
+}
 
 } // namespace adapt_detail
 } // namespace thrift
