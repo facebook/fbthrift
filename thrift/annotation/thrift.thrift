12c8eb07d3a207ce702d8d90dadde02f4421d67a
TJ Yin <ytj@fb.com>
2021-10-07 20:06:44 -0700

    add annotation for Compatibility lint warning
    Summary:
    For following struct
    
    ```
    thrift.RequiresBackwardCompatibility{field_name = true}
    struct Foo {
      1: i32 field1;
      2: i32 field2;
    }
    ```
    
    There will be lint warning if user rename the field.
    
    This is implementation of Compatibility annotation that's mentioned here https://fb.quip.com/a7P3AxsbEmPv#temp:C:NNZeec91e29130041cb869b91362
    
    Reviewed By: iahs
    
    Differential Revision: D31430654
    
    fbshipit-source-id: a550e30cb668a99677e0944e246bcd47a2e3b14c

---
+++ thrift/annotation/thrift.thrift
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+include "thrift/annotation/scope.thrift"
+
+namespace cpp2 facebook.thrift.annotation.thrift
+namespace py3 facebook.thrift.annotation.thrift
+namespace php facebook_thrift_annotation_thrift
+namespace java.swift com.facebook.thrift.annotation.thrift
+namespace java com.facebook.thrift.annotation.thrift
+namespace py.asyncio facebook_thrift_asyncio.annotation.thrift
+namespace go thrift.annotation.thrift
+namespace py thrift.annotation.thrift
+
+// Indicates changes that break compatibility
+@scope.Struct
+@scope.Union
+@scope.Exception
+struct RequiresBackwardCompatibility {
+  1: bool field_name = false;
+} (
+  thrift.uri = "facebook.com/thrift/annotation/thrift/RequiresBackwardCompatibility",
+)
