b5807b4158fd285273f17370ebab32d30af46e1a
Mingtao Yang <mingtao@fb.com>
2021-10-18 14:09:46 -0700

    Treat Fizz/KTLS as a TLS based transport
    Summary:
    Fizz/KTLS is the protocol string for fizz::AsyncKTLSSocket, which uses kTLS set
    up by a Fizz handshake.
    
    Reviewed By: anhuang
    
    Differential Revision: D31659992
    
    fbshipit-source-id: 6052282adcd2a2e82ec5db56009f6afa7acb75a2

--- thrift/lib/cpp2/server/LoggingEventHelper.cpp
+++ thrift/lib/cpp2/server/LoggingEventHelper.cpp
@@ -91,7 +91,8 @@ void logSetupConnectionEventsOnce(
       }
       if (auto transport = context.getTransport()) {
         const auto& protocol = context.getSecurityProtocol();
-        if (protocol == "TLS" || protocol == "Fizz" || protocol == "stopTLS") {
+        if (protocol == "TLS" || protocol == "Fizz" || protocol == "stopTLS" ||
+            protocol == "Fizz/KTLS") {
           if (!transport->getPeerCertificate()) {
             logTlsNoPeerCertEvent(context);
           }
