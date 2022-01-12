8f15e74d18ea8d042dd4aad5767271884510d141
Renato Lima <renatol@fb.com>
2022-01-12 12:18:47 -0800

    Fix leak for CRYPTO_zalloc/CRYPTO_malloc in UniversalNameTest.cpp
    Summary:
    Leak in UniversalNameTest.cpp due to usage of C CRYPTO_zalloc/malloc via external dep Openssl and not freeing resource. Since we know the solution, we are applying this small change.
    
    Solution: Add a call to free the MD ctx via EVP_MD_CTX_free.
    
    Reviewed By: Alfus
    
    Differential Revision: D33535770
    
    fbshipit-source-id: 9dade37b3b7bd29d0177719db749e01a6f9cb4fc

--- thrift/lib/cpp2/type/UniversalNameTest.cpp
+++ thrift/lib/cpp2/type/UniversalNameTest.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) Facebook, Inc. and its affiliates.
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -103,6 +103,7 @@ TEST(UniversalNameTest, Constants) {
   EXPECT_EQ(
       getUniversalHashSize(UniversalHashAlgorithm::Sha2_256),
       EVP_MD_CTX_size(ctx));
+  EVP_MD_CTX_free(ctx);
 }
 
 TEST(UniversalNameTest, ValidateUniversalName) {
