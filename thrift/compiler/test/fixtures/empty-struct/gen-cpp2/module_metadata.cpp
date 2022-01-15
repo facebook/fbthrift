3df88f0ac09513cc828ab194c49ab06434047b5d
Yedidya Feldblum <yfeldblum@fb.com>
2022-01-15 09:29:39 -0800

    suppress -Wignored-optimization-argument in generated cpp2 metadata
    Summary: Some of these generated functions can get so large that the compiler gives up optimizing them. That would be alright - these functions are so rarely called that their being optimized is not particularly important - except that the compiler also emits a warning about it and this warning may be treated as an error. Suppress this warning so that builds do not fail when these functions get sufficiently large.
    
    Reviewed By: Mizuchi, luciang
    
    Differential Revision: D33592075
    
    fbshipit-source-id: 5dfcf1383ae8722e1cdd53b813e32e0f05b57ae2

--- thrift/compiler/test/fixtures/empty-struct/gen-cpp2/module_metadata.cpp
+++ thrift/compiler/test/fixtures/empty-struct/gen-cpp2/module_metadata.cpp
@@ -7,6 +7,13 @@
 #include <thrift/lib/cpp2/gen/module_metadata_cpp.h>
 #include "thrift/compiler/test/fixtures/empty-struct/gen-cpp2/module_metadata.h"
 
+// some of these functions can be so large that the compiler gives up optimizing
+// them - and issues a warning which may be treated as an error!
+//
+// these functions are so rarely called that it is probably okay for them not to
+// be optimized in practice
+FOLLY_CLANG_DISABLE_WARNING("-Wignored-optimization-argument")
+
 namespace apache {
 namespace thrift {
 namespace detail {
