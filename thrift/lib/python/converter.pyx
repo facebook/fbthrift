83885f859764b3e04f56e2bcfba90e1f17afe2f8
Jun Zhou <junz@fb.com>
2022-05-19 10:50:40 -0700

    python: remove converter->metadata dependency
    Summary: it's unused left over from a previous iteration
    
    Reviewed By: nanshu
    
    Differential Revision: D36427670
    
    fbshipit-source-id: e61e956960056760e90c4659b569da5f1e6763c3

--- thrift/lib/python/converter.pyx
+++ thrift/lib/python/converter.pyx
@@ -12,7 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from thrift.python.metadata import gen_metadata
 from thrift.python.types cimport (
     StructOrUnion,
     Struct,
