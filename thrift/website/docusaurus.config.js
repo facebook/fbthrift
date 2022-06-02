5bc17375ee49b9e22115861dd0681d6638c8a64c
Victor Zverovich <viz@fb.com>
2022-06-01 17:59:19 -0700

    Add an introduction page to docs
    Summary: Add an introduction page to docs by migrating it from the wiki.
    
    Reviewed By: thedavekwon
    
    Differential Revision: D36834898
    
    fbshipit-source-id: 0ff4bbe9afdd938a440541d46dbee29181b7c51c

--- thrift/website/docusaurus.config.js
+++ thrift/website/docusaurus.config.js
@@ -65,7 +65,7 @@ const config = {
         items: [
           {
             type: 'doc',
-            docId: 'index',
+            docId: 'intro',
             position: 'left',
             label: 'Docs',
           },
