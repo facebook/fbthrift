10ad01c80e0481a7aa77c061269c400957108b8a
Hung Nguyen <hungmn@fb.com>
2022-08-08 12:57:59 -0700

    Ensure active requests not decremented to negative
    Summary: Since the max request comparison casts active requests counter from signed to unsigned, if the active requests become negative, their unsigned value is very large and would trigger overload protection. This diff makes sure that we won't decrement the counter below zero.
    
    Reviewed By: ycking21
    
    Differential Revision: D38459476
    
    fbshipit-source-id: dd9bb43fc9c1bd575d90242b73af2d3ec6d3326a

--- thrift/lib/cpp2/server/ServerConfigs.h
+++ thrift/lib/cpp2/server/ServerConfigs.h
@@ -150,7 +150,7 @@ class ServerConfigs {
   }
 
   void decActiveRequests() {
-    if (!isActiveRequestsTrackingDisabled()) {
+    if (!isActiveRequestsTrackingDisabled() && activeRequests_ > 0) {
       --activeRequests_;
     }
   }
