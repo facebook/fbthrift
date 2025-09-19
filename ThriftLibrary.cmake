facebook/fbthrift

commit 58038399cefc0c2256ce4ef5444dee37147cbf07

Fix thrift_generate
Summary: Update the `thrift_generate` CMake macro to handle the newly added `<module>_types_serialization.cpp` as well as previously missing `<module>_types_custom_protocol.h`.

Reviewed By: tanquer

Differential Revision: D80203985

fbshipit-source-id: 33086a07395aa5618f3ea38b35262c2d56b4e550

ThriftLibrary.cmake
@@ -223,14 +223,16 @@ macro(thrift_generate
     ${output_path}/gen-${language}/${source_file_name}_data.h
     ${output_path}/gen-${language}/${source_file_name}_metadata.h
     ${output_path}/gen-${language}/${source_file_name}_types.h
-    ${output_path}/gen-${language}/${source_file_name}_types.tcc
+    ${output_path}/gen-${language}/${source_file_name}_types.tcch
+    ${output_path}/gen-${language}/${source_file_name}_types_custom_protocol.h
   )
   set("${target_file_name}-${language}-SOURCES"
     ${output_path}/gen-${language}/${source_file_name}_constants.cpp
     ${output_path}/gen-${language}/${source_file_name}_data.cpp
     ${output_path}/gen-${language}/${source_file_name}_types.cpp
-    ${output_path}/gen-${language}/${source_file_name}_types_compact.cpp
     ${output_path}/gen-${language}/${source_file_name}_types_binary.cpp
+    ${output_path}/gen-${language}/${source_file_name}_types_compact.cpp
+    ${output_path}/gen-${language}/${source_file_name}_types_serialization.cpp
   )
   if("${options}" MATCHES "layouts")
     set("${target_file_name}-${language}-SOURCES"

commit 09fefcfd1433e058a27bdb9bb09cc248273dedde

Fix typo in ThriftLibrary.cmake
Was introduced in 58038399cefc0c2256ce4ef5444dee37147cbf07

ThriftLibrary.cmake
@@ -223,7 +223,7 @@ macro(thrift_generate
     ${output_path}/gen-${language}/${source_file_name}_data.h
     ${output_path}/gen-${language}/${source_file_name}_metadata.h
     ${output_path}/gen-${language}/${source_file_name}_types.h
-    ${output_path}/gen-${language}/${source_file_name}_types.tcch
+    ${output_path}/gen-${language}/${source_file_name}_types.tcc
     ${output_path}/gen-${language}/${source_file_name}_types_custom_protocol.h
   )
   set("${target_file_name}-${language}-SOURCES"