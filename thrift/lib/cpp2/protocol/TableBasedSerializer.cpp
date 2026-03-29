59a1f91394c0b9f9101f832aacc4865fcf2dd4df
TJ Yin <ytj@meta.com>
2026-03-29 01:29:28 -0700

    table-based serialization support (1): Add main functionalities
    Summary: This diff added basic table-based serialization support for JSON5: table-based serializer will invoke `writeMapBegin<Key, Map>(size)`, since JSON5 doesn't support `writeMapBegin(TType key, TType value, size)`, since `TType` doesn't have enough information for JSON5.
    
    Reviewed By: vitaut
    
    Differential Revision: D97831969
    
    fbshipit-source-id: b364bd8c481e3cf788594c48771958c763e75c72

--- thrift/lib/cpp2/protocol/TableBasedSerializer.cpp
+++ thrift/lib/cpp2/protocol/TableBasedSerializer.cpp
@@ -20,6 +20,8 @@
 #include <thrift/lib/cpp2/protocol/CompactProtocol.h>
 #include <thrift/lib/cpp2/protocol/JSONProtocol.h>
 #include <thrift/lib/cpp2/protocol/SimpleJSONProtocol.h>
+#include <thrift/lib/cpp2/protocol/detail/Json5ProtocolReader.h>
+#include <thrift/lib/cpp2/protocol/detail/Json5ProtocolWriter.h>
 
 namespace apache::thrift::detail {
 
@@ -99,6 +101,14 @@ template void read<JSONProtocolReader>(
     JSONProtocolReader* iprot, const StructInfo& structInfo, void* object);
 template size_t write<JSONProtocolWriter>(
     JSONProtocolWriter* iprot,
+    const StructInfo& structInfo,
+    const void* object);
+template void read<json5::detail::Json5ProtocolReader>(
+    json5::detail::Json5ProtocolReader* iprot,
+    const StructInfo& structInfo,
+    void* object);
+template size_t write<json5::detail::Json5ProtocolWriter>(
+    json5::detail::Json5ProtocolWriter* iprot,
     const StructInfo& structInfo,
     const void* object);
 
