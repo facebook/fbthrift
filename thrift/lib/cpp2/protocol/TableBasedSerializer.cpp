315381e37ecebcf8b28dfe3e2ee986453d42ca7a
TJ Yin <ytj@meta.com>
2026-04-06 15:07:27 -0700

    table-based serialization support (1): Add main functionalities (redo)
    Reviewed By: vitaut
    
    Differential Revision: D99046822
    
    fbshipit-source-id: c71ceaa104e0547e0dbd3dea6aa2ded94f0124bf

--- thrift/lib/cpp2/protocol/TableBasedSerializer.cpp
+++ thrift/lib/cpp2/protocol/TableBasedSerializer.cpp
@@ -21,6 +21,12 @@
 #include <thrift/lib/cpp2/protocol/JSONProtocol.h>
 #include <thrift/lib/cpp2/protocol/SimpleJSONProtocol.h>
 
+// Disable Json5Protocol on mobile due to build size regression.
+#if !FOLLY_MOBILE
+#include <thrift/lib/cpp2/protocol/detail/Json5ProtocolReader.h>
+#include <thrift/lib/cpp2/protocol/detail/Json5ProtocolWriter.h>
+#endif
+
 namespace apache::thrift::detail {
 
 #define THRIFT_DEFINE_PRIMITIVE_TYPE_TO_INFO(                          \
@@ -101,5 +107,15 @@ template size_t write<JSONProtocolWriter>(
     JSONProtocolWriter* iprot,
     const StructInfo& structInfo,
     const void* object);
+#if !FOLLY_MOBILE
+template void read<json5::detail::Json5ProtocolReader>(
+    json5::detail::Json5ProtocolReader* iprot,
+    const StructInfo& structInfo,
+    void* object);
+template size_t write<json5::detail::Json5ProtocolWriter>(
+    json5::detail::Json5ProtocolWriter* iprot,
+    const StructInfo& structInfo,
+    const void* object);
+#endif
 
 } // namespace apache::thrift::detail
